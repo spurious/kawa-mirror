eeb78729a8a078160fe736b9af61b8da098da3db
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
1996-07-17 06:06:16 +0000

    * kawa/standard/callcc.java: Implement limited call/cc.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@52 5e0a886f-7f45-49c5-bc19-40643649e37f

---
+++ kawa/standard/callcc.java
@@ -0,0 +1,32 @@
+package kawa.standard;
+import kawa.lang.*;
+
+/**
+ * Implement the Scheme standard function "call-with-current-continuation".
+ * This is a restricted version, that only works for escape-like applications.
+ * @author Per Bothner
+ */
+
+public class callcc extends Procedure1
+{
+  public callcc()
+  {
+    super("call-with-current-continuation");
+  }
+
+  public Object apply1 (Object arg1)
+      throws WrongArguments, WrongType, GenericError, UnboundSymbol
+  {
+    Procedure proc = (Procedure) arg1;
+    try
+      {
+	proc = (Procedure) arg1;
+      }
+    catch (ClassCastException ex)
+      {
+	throw new GenericError ("argument to call/cc is not procedure");
+      }
+    return Continuation.callcc (proc);
+  }
+
+}

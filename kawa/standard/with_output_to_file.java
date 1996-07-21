101b503a09620eb4323a4d4b829b13bdcd9616a1
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
1996-07-21 04:46:23 +0000

    Missing file.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@112 5e0a886f-7f45-49c5-bc19-40643649e37f

---
+++ kawa/standard/with_output_to_file.java
@@ -0,0 +1,39 @@
+package kawa.standard;
+import kawa.lang.*;
+             
+public class with_output_to_file extends Procedure2
+{
+  public with_output_to_file ()
+  {
+    super("with_output-to-file");
+  }
+
+  public Object apply2 (Object string, Object proc)
+       throws WrongArguments, WrongType, GenericError, UnboundSymbol
+  {
+    String fname = ((StringBuffer)string).toString();
+
+    try {
+      java.io.OutputStream is = new java.io.FileOutputStream(fname);
+      OutPort port = new OutPort(is, fname);
+      Object result;
+      OutPort save_port = OutPort.outDefault ();
+      try
+	{
+	  OutPort.setOutDefault (port);
+	  result = ((Procedure)proc).apply0 ();
+	}
+      finally
+	{
+	  OutPort.setOutDefault (save_port);
+	  port.close ();
+	}
+      return result;
+    } catch (java.io.FileNotFoundException e) {
+       throw new GenericError ("file not found: " + fname);
+    }
+    catch (java.io.IOException e) {
+      throw new GenericError ("caught I/O exception: " + e);
+    }
+  }
+}

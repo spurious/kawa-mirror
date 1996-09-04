b11b2f76e342d2acd306af6984a82f00d65b681a
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
1996-09-04 21:48:38 +0000

    * kawa/lang/ReadError.java:  Include line number info in error message.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@137 5e0a886f-7f45-49c5-bc19-40643649e37f

--- kawa/lang/ReadError.java
+++ kawa/lang/ReadError.java
@@ -6,8 +6,36 @@ package kawa.lang;
 
 public class ReadError extends SyntaxError
 {
-  ReadError (InPort inport, String message)
+  InPort inport;
+
+  public ReadError (InPort inport, String message)
   {
-    super (message);
+    super (construct_message (inport, message));
+    this.inport = inport;
+  }
+
+  public InPort getPort ()
+  {
+    return inport;
+  }
+
+  static private String construct_message (InPort port, String message)
+  {
+    StringBuffer buffer = new StringBuffer ();
+    String name = port.getName ();
+    if (name == null)
+      name = "<unknown>";
+    buffer.append (name);
+    buffer.append (':');
+    buffer.append (port.getLineNumber ());
+    int column = port.getColumnNumber ();
+    if (column >= 0)
+      {
+	buffer.append (':');
+	buffer.append (column);
+      }
+    buffer.append (": ");
+    buffer.append (message);
+    return buffer.toString ();
   }
 }

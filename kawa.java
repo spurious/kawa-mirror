bb20f620d1255bce7b967ddeea6accbe0fa9677d
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
1996-09-13 15:36:48 +0000

    * kawa.java:  Re-write to do options processing. 	* kawa.texi:  New documentation file. 	* texinfo.tex:  New support file, needed for kawa.texi. 	* Makefile.{am,in}:  Update for kawa.texi.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@187 5e0a886f-7f45-49c5-bc19-40643649e37f

--- kawa.java
+++ kawa.java
@@ -1,39 +1,84 @@
 import kawa.lang.*;
 import kawa.standard.*;
-
+import kawa.Shell;
 import java.io.*;
 
-class kawa {
+class kawa
+{
+  static void bad_option (String str)
+  {
+    System.err.println ("bad option " + str);
+    System.exit (-1);
+  }
 
-   public static void main(java.lang.String args[]) {
+  public static Vector commandLineArguments;
 
-      boolean usingSystem = true;
-      InputStream stream = null;
-      if (args.length>0) {
-         try {
-            stream = new FileInputStream(args[0]);
-            usingSystem = false;
-         } catch (FileNotFoundException e) {
-            System.out.println("Cannot open file "+args[0]);
-            System.exit(1);
-         }
-      } else {
-         stream = System.in;
-      }
-      kawa.standard.StandardInterpreter interpreter =
-         new kawa.standard.StandardInterpreter(
-            InPort.inDefault (),
+  public static void setArgs (String[] args, int arg_start)
+  {
+    Object[] array = new Object[args.length - arg_start];
+    for (int i = arg_start;  i < args.length;  i++)
+      array[i - arg_start] = new StringBuffer (args[i]);
+    commandLineArguments = new Vector (array);
+    Interpreter.define_global (Symbol.make ("command-line-arguments"),
+			       commandLineArguments);
+  }
+
+  public static void main(String args[])
+  {
+    kawa.standard.StandardInterpreter interpreter =
+      new kawa.standard.StandardInterpreter(
+	    InPort.inDefault (),
 	    OutPort.outDefault (),
 	    OutPort.errDefault ()
          );
 
-      kawa.Shell shell = new kawa.Shell(
-         interpreter,
-         usingSystem,  // Display prompt only if input is from System.in
-         usingSystem   // Display result only if input is from System.in
-      );
+    int iArg = 0;
+    boolean something_done = false;
+    for ( ;  iArg < args.length;  iArg++)
+      {
+	String arg = args[iArg];
+	if (arg.equals ("-c") || arg.equals ("-e"))
+	  {
+	    iArg++;
+	    if (iArg == args.length)
+	      bad_option (arg);
+	    setArgs (args, iArg+1);
+	    Shell.runString (args[iArg], interpreter, false);
+	    something_done = true;
+	  }
+	else if (arg.equals ("-f"))
+	  {
+	    iArg++;
+	    if (iArg == args.length)
+	      bad_option (arg);
+	    setArgs (args, iArg+1);
+	    Shell.runFile (args[iArg], interpreter, false);
+	    something_done = true;
+	  }
+	else if (arg.equals ("-s") || arg.equals ("--"))
+	  {
+	    iArg++;
+	    setArgs (args, iArg);
+	    Shell.run (InPort.inDefault (), interpreter, true, true);
+	    return;
+	  }
+	else if (arg.length () > 0 && arg.charAt(0) == '-')
+	  bad_option (arg);
+	else
+	  break;
+      }
+    if (something_done)
+      return;
 
-      shell.run();
-
+    if (iArg < args.length)
+      {
+	setArgs (args, iArg+1);
+	Shell.runFile (args[iArg], interpreter, false);
+      }
+    else
+      {
+	setArgs (args, iArg);
+	Shell.run (InPort.inDefault (), interpreter, true, true);
+      }
    }
 }

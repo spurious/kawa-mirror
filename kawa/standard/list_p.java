d4be4bc48edd61a7c49cc71eb745740af72a4b84
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
1996-11-05 00:16:48 +0000

    Remove redundant name qualification (which J++ complains about). 	Remove constructors.  Generally remove verbosity.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@266 5e0a886f-7f45-49c5-bc19-40643649e37f

--- kawa/standard/list_p.java
+++ kawa/standard/list_p.java
@@ -1,13 +1,10 @@
 package kawa.standard;
 import kawa.lang.*;
 
+/** Implement the standard Scheme function "list?". */
+
 public class list_p extends Procedure1
 {
-  public kawa.standard.list_p()
-  {
-    super("list?");
-  }
-
   public Object apply1 (Object arg1)
   {
     if (List.list_length (arg1) >= 0)

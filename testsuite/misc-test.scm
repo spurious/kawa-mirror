b48cfc841bb3ad067ca53ed06b32acd5c9da6186
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
1997-03-18 22:20:14 +0000

    * misc-test.scm:  New tests.         * Makefile.am, Makefile.in:  Update.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@388 5e0a886f-7f45-49c5-bc19-40643649e37f

---
+++ testsuite/misc-test.scm
@@ -0,0 +1,22 @@
+(TEST-INIT "Miscellaneous")
+
+;;; DSSSL spec example 11
+(test '(3 4 5 6) (lambda x x) 3 4 5 6)
+(test '(5 6) (lambda (x y #!rest z) z) 3 4 5 6)
+(test '(3 4 5 i: 6 j: 1)
+      (lambda ( x y #!optional z #!rest r #!key i (j 1))
+	(list x y z i: i j: j))
+      3 4 5 i: 6 i: 7)
+
+(test #t keyword? foo:)
+(test #t keyword? 'foo:)
+(test #f keyword? 'foo\:)
+
+;;; DSSSL spec example 44
+(test "Argentina" keyword->string \Argentina:)
+
+;;; DSSSL spec example 45
+(test foobar: string->keyword "foobar")
+
+(define-unit ft 12in)
+(test 18in + 6in 1ft)

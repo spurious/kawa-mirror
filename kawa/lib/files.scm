ba3e18f0621a5302dca42b1acacac658a6721f08
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
1997-05-27 03:37:35 +0000

    * files.scm:  New file.         * Makefile.{am,in}:  Update for new file.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@445 5e0a886f-7f45-49c5-bc19-40643649e37f

---
+++ kawa/lib/files.scm
@@ -0,0 +1,40 @@
+(define (file-exists? filename)
+  ((primitive-virtual-method "java.io.File" "exists" "boolean" ())
+   (->pathname filename)))
+
+(define (file-directory? filename)
+  ((primitive-virtual-method "java.io.File" "isDirectory" "boolean" ())
+   (->pathname filename)))
+
+(define (file-readable? filename)
+  ((primitive-virtual-method "java.io.File" "canRead" "boolean" ())
+   (->pathname filename)))
+
+(define (file-writable? filename)
+  ((primitive-virtual-method "java.io.File" "canWrite" "boolean" ())
+   (->pathname filename)))
+
+;(define (file-modification-time filename)
+;  ((primitive-virtual-method "java.io.File" "lastModified" "long" ())
+;   (->pathname filename)))
+
+(define (delete-file filename)
+  ((primitive-virtual-method "java.io.File" "delete" "boolean" ())
+   (->pathname filename)))
+
+(define (rename-file oldname newname)
+  ((primitive-virtual-method "java.io.File" "renameTo" "boolean"
+			     ("java.io.File"))
+   (->pathname oldname) (->pathname newname)))
+
+(define (create-directory dirname)
+  ((primitive-virtual-method "java.io.File" "mkdir" "boolean" ())
+   (->pathname dirname)))
+
+; Taken from MIT Scheme
+(define (->pathname filename)
+  ((primitive-constructor "java.io.File" ("String"))
+    filename))
+  
+; From scsh
+;(define (directory-files [dir [dotfiles?]]) ...)

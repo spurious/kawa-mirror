b3c80684b42a6c247df185467617d0a733f4972d
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
1997-12-15 00:57:10 +0000

    * syntax.scm (dynamic-wind):  New procedure. 	(fluid-let):  New syntax.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@513 5e0a886f-7f45-49c5-bc19-40643649e37f

--- kawa/lib/syntax.scm
+++ kawa/lib/syntax.scm
@@ -19,3 +19,32 @@
 (define-syntax unless (syntax-rules ()
 				  ((when cond exp ...)
 				   (if (not cond) (begin exp ...)))))
+
+(define (dynamic-wind before thunk after)
+  (before)
+  (try-finally
+   (thunk)
+   (after)))
+
+(define-syntax fluid-let
+  (syntax-rules ()
+		((fluid-let ((variable init) ...) . exprs)
+		 (let* ((old-env ((primitive-static-method
+				   "kawa.lang.Environment" "getCurrent"
+				   "kawa.lang.Environment" ())))
+			(new-env ((primitive-constructor
+				   "kawa.lang.Environment"
+				   ("kawa.lang.Environment")) old-env)))
+		   (begin
+		     ((primitive-virtual-method "kawa.lang.Environment"
+						"define" "kawa.lang.Binding"
+						("String" <object>))
+		      new-env 'variable init) ...)
+		   ((primitive-static-method
+		     "kawa.lang.Environment" "setCurrent"
+		     "void" ("kawa.lang.Environment")) new-env)
+		   (try-finally
+		    (begin . exprs)
+		    ((primitive-static-method
+		      "kawa.lang.Environment" "setCurrent"
+		      "void" ("kawa.lang.Environment")) old-env))))))

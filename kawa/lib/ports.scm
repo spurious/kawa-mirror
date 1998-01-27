f006402ff3c6ff0e9d1c10c8807a8f39da40b59e
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
1998-01-27 19:47:02 +0000

    * port.scm, syntax.scm:  Use <TYPENAME> in place of "TYPENAME". 	* syntax.scm (catch, error):  New procedures. 	* Makefile.am (SECONDARY_OBJS):  Update for new procedures.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@591 5e0a886f-7f45-49c5-bc19-40643649e37f

--- kawa/lib/ports.scm
+++ kawa/lib/ports.scm
@@ -1,34 +1,34 @@
 (define (force-output #!optional (port (current-output-port)))
-  ((primitive-virtual-method "java.io.Writer" "flush" "void" ())
+  ((primitive-virtual-method <java.io.Writer> "flush" <void> ())
    port))
 
 (define (newline #!optional (port (current-output-port)))
-  ((primitive-virtual-method "kawa.lang.OutPort" "println"
-			     "void" ())
+  ((primitive-virtual-method <kawa.lang.OutPort> "println"
+			     <void> ())
    port))
 
 (define (eof-object? obj)
   (eq? obj #!eof))
 
 (define (input-port-read-state port)
-  ((primitive-virtual-method "kawa.lang.InPort" "getReadState" <char> ())
+  ((primitive-virtual-method <kawa.lang.InPort> "getReadState" <char> ())
    port))
 
 (define (input-port-line-number port)
   (+ 1
-     ((primitive-virtual-method "kawa.lang.LineBufferedReader" "getLineNumber"
-				"int" ())
+     ((primitive-virtual-method <kawa.lang.LineBufferedReader> "getLineNumber"
+				<int> ())
       port)))
 
 (define (set-input-port-line-number! port num)
-  ((primitive-virtual-method "kawa.lang.LineBufferedReader" "setLineNumber"
-			     "void" ("int"))
+  ((primitive-virtual-method <kawa.lang.LineBufferedReader> "setLineNumber"
+			     <void> (<int>))
    port (- num 1)))
 
 (define (input-port-column-number port)
   ((+ 1
-      ((primitive-virtual-method "kawa.lang.LineBufferedReader"
-				 "getColumnNumber" "int" ())
+      ((primitive-virtual-method <kawa.lang.LineBufferedReader>
+				 "getColumnNumber" <int> ())
        port))))
 
 (define (default-prompter port)
@@ -42,18 +42,19 @@
 		       "|# "))))
 
 (define (input-port-prompter port)
-  ((primitive-virtual-method "kawa.lang.TtyInPort" "getPrompter"
-			       "kawa.lang.Procedure" ())
+  ((primitive-virtual-method <kawa.lang.TtyInPort> "getPrompter"
+			       <kawa.lang.Procedure> ())
    port))
 
 (define (set-input-port-prompter! port prompter)
-  ((primitive-virtual-method "kawa.lang.TtyInPort" "setPrompter"
-			       <void> ("kawa.lang.Procedure"))
+  ((primitive-virtual-method <kawa.lang.TtyInPort> "setPrompter"
+			       <void> (<kawa.lang.Procedure>))
    port prompter))
 
 (define (transcript-on filename)
-  ((primitive-static-method "kawa.lang.OutPort" "setLogFile" <void> ("String"))
+  ((primitive-static-method <kawa.lang.OutPort> "setLogFile" <void>
+			    (<java.lang.String>))
    filename))
 
 (define (transcript-off)
-  ((primitive-static-method "kawa.lang.OutPort" "closeLogFile" <void> ())))
+  ((primitive-static-method <kawa.lang.OutPort> "closeLogFile" <void> ())))
--- kawa/lib/syntax.scm
+++ kawa/lib/syntax.scm
@@ -4,12 +4,12 @@
 		 (%defmacro (quote name) (lambda lambda-list form ...)))))
 
 (define (%defmacro name expander)
-  ((primitive-constructor "kawa.lang.DefMacro"
+  ((primitive-constructor <kawa.lang.DefMacro>
 			  (<symbol> <function>))
    name expander))
 
 (define (gentemp)
-  ((primitive-static-method "kawa.lang.Symbol" "gentemp"
+  ((primitive-static-method <kawa.lang.Symbol> "gentemp"
 			    <symbol> ())))
 
 (define-syntax when (syntax-rules ()
@@ -25,6 +25,24 @@
   (try-finally
    (thunk)
    (after)))
+
+(define (catch key thunk handler)
+  (try-catch (thunk)
+	     (ex <kawa.lang.NamedException>
+		 ((primitive-virtual-method
+		   <kawa.lang.NamedException> "applyHandler"
+		   <object> (<object> <function>))
+		  ex key handler))))
+
+;;; The one-argument case is a standard DSSSL procedure.
+;;; The multi-argument extension matches Guile.
+(define (error msg . args)
+  (set! msg (call-with-output-string (lambda (port) (display msg port))))
+  (set! args (map
+	      (lambda (arg)
+		(call-with-output-string (lambda (port) (write arg port))))
+	      args))
+  (apply throw 'misc-error msg args))
 
 (define-syntax fluid-let
   (syntax-rules ()

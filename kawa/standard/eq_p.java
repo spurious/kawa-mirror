43f1306441879320f33679074809f1bc13e7aa1c
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
1998-02-03 23:47:48 +0000

    Mostly re-formatting.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@613 5e0a886f-7f45-49c5-bc19-40643649e37f

--- kawa/standard/eq_p.java
+++ kawa/standard/eq_p.java
@@ -1,15 +1,15 @@
 package kawa.standard;
-import kawa.lang.Procedure2;
+import kawa.lang.*;
 
 /** Implement the standard Scheme function "eq?". */
 
-public class eq_p extends kawa.lang.Procedure2 {
-   public Object apply2(Object arg1, Object arg2) 
-   {
-      if (arg1==arg2) {
-         return kawa.lang.Interpreter.trueObject;
-      } else {
-         return kawa.lang.Interpreter.falseObject;
-      }
-   }
+public class eq_p extends Procedure2 {
+
+  public Object apply2(Object arg1, Object arg2) 
+  {
+    if (arg1==arg2)
+      return Boolean.TRUE;
+    else
+      return Boolean.FALSE;
+  }
 }

6f6a85e6d44a8ceb7459611f37d8e03ffff15fde
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
1998-03-11 06:05:09 +0000

    * kawa/lang/PrimArraySet.java (compile):  Convert value.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@696 5e0a886f-7f45-49c5-bc19-40643649e37f

--- kawa/lang/PrimArraySet.java
+++ kawa/lang/PrimArraySet.java
@@ -21,7 +21,7 @@ public class PrimArraySet extends Procedure3 implements Inlineable
   {
     exp.args[0].compile(comp, 0, new ArrayType(element_type));
     exp.args[1].compile(comp, 0, Type.int_type);
-    exp.args[2].compile(comp, 0);
+    exp.args[2].compile(comp, 0, element_type);
     comp.getCode().emitArrayStore(element_type);
     if ((flags & Expression.IGNORED) == 0)
       comp.compileConstant (Interpreter.voidObject);

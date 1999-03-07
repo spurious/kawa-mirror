e0a96a9687a03b411c82735bb8fab1932d7fb641
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
1999-03-07 17:01:56 +0000

    �  	* CallStack.java:  New class. 	* CpsProcedure.java:  New class (extends ProcedureN). 	* CallFrame.java:  New class (extends CpsProcedure). 	* Makefile.am (java_JAVA), Makefile.in:  Add new source files. 	* Procedure.java (noArgs):  New static field. 	(apply(CallStack)):  New overloading method. 	* ProcedureN.java (apply0):  Use Procedure.noArgs. 	* Values.java (empty):  Change to use Procedure.noArgs.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@1117 5e0a886f-7f45-49c5-bc19-40643649e37f

---
+++ gnu/mapping/CallFrame.java
@@ -0,0 +1,37 @@
+package gnu.mapping;
+
+/** A procedure activation frame (when compiled with explicit stacks). */
+
+public abstract class CallFrame extends CpsProcedure implements Cloneable
+{
+  public Object[] args;
+  public Procedure proc;
+  protected Procedure caller;
+  protected int saved_pc;
+
+  protected int numArgs;
+
+  public int numArgs() { return numArgs; }
+
+  public void apply (CallStack stack)
+  {
+    Object[] args = stack.args;
+    Procedure.checkArgCount(this, args.length);
+    try
+      {
+	CallFrame frame = (CallFrame) clone();
+	frame.args = args;
+	frame.caller = stack.proc;
+	frame.saved_pc = stack.pc;
+	frame.proc = this;
+	stack.proc = frame;
+	stack.pc = saved_pc;
+      }
+    catch (java.lang.CloneNotSupportedException ex)
+      {
+	throw new InternalError(ex.toString());
+      }
+  }
+
+  public abstract void step (CallStack stack);
+}

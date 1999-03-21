e95a7c9e675d108917b12bdef3b64e5787b08407
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
1999-03-21 16:35:59 +0000

    * Binding.java (constraint):  New field. Re-write to use Constraints. 	Add methods to implement JDK1.2 java.util.Map.Entry interface. 	* Environment.java:  Misc changes for Constraint model. 	* NameMap.java (get):  Rename to getChecked. 	(get(Object)):  New, for compatibility with JDK1.2 collections.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@1158 5e0a886f-7f45-49c5-bc19-40643649e37f

--- gnu/mapping/Binding.java
+++ gnu/mapping/Binding.java
@@ -1,20 +1,38 @@
 package gnu.mapping;
 
-/* A Binding is a Location in an Environment object. */
+/** A Binding is a Location in an Environment object. */
 
-public class Binding extends Location
+public final class Binding extends Location
+    // implements java.util.Map.Entry
 {
   /** The current value of the binding. */
   Object value;
 
+  Constraint constraint;
+
   public final Object get ()
   {
-    return value;
+    return constraint.get(this);
+  }
+
+  public final Procedure getProcedure ()
+  {
+    return constraint.getProcedure(this);
   }
 
   public final void set (Object value)
   {
-    this.value = value;
+    constraint.set(this, value);
+  }
+
+  public final void setConstraint (Constraint constraint)
+  {
+    this.constraint = constraint;
+  }
+
+  public boolean isBound ()
+  {
+    return ! (constraint instanceof UnboundConstraint);  // FIXME
   }
 
   // The compiler emits calls to this method.
@@ -22,12 +40,10 @@ public class Binding extends Location
   {
     Binding binding = new Binding();
     binding.value = init;
-    binding.name = name;
+    binding.setName(name);
+    binding.constraint = new TrivialConstraint(null);
     return binding;
   }
-
-  /** The (interned) name of the binding. */
-  String name;
 
   /** Used to chain multiple Bindings in the same hash bucket.
    * Note that there can be multiple Bindings with the same name;
@@ -37,4 +53,61 @@ public class Binding extends Location
   /** The "time" the binding was created.
    * If the binding is newer than the current thread, it does not count. */
   int time_stamp;
+
+  public void print(java.io.PrintWriter ps)
+  {
+    ps.print ("#<binding ");
+    String name = getName();
+    if (name != null)
+      ps.print(name);
+    if (isBound())
+      {
+	ps.print(" -> ");
+	SFormat.print(get(), ps);
+      }
+    else
+      ps.print("(unbound)");
+    ps.print ('>');
+  }
+
+  // Methods that implement java.util.Map.Entry:
+
+  public final Object getKey ()
+  {
+    return getName();
+  }
+
+  public final Object getValue ()
+  {
+    return constraint.get(this);
+  }
+
+  public final Object setValue (Object value)
+  {
+    Object old = constraint.get(this);
+    constraint.set(this, value);
+    return old;
+  }
+
+  public boolean equals (Object o)
+  {
+    if (! (o instanceof Binding))
+      return false;
+    Binding e2 = (Binding) o;
+    String e1Key = getName();
+    String e2Key = e2.getName();
+    // This is quite following the Collections spec, but we assume keys
+    // are interned, or if they are not, that they are seen as unequal.
+    if (e1Key != e2Key)
+      return false;
+    Object e1Value = constraint.get(this);
+    Object e2Value = e2.constraint.get(e2);
+    return e1Value == null ? e2Value == null : e1Value.equals(e2Value);
+  }
+
+  public int hashCode ()
+  {
+    Object value = constraint.get(this);
+    return getName().hashCode() ^ (value == null ? 0 : value.hashCode());
+  }
 }

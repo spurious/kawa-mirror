eb52b8fd790940ba613b51acd107d9757b14bdc0
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
1999-03-21 16:45:34 +0000

    � 	* kawa/standard/Scheme.java (voidType):  New static Type field. 	(getNamedType):  USe voidType. 	(initScheme):  Define define-alias and emacs:read. 	* kawa/standard/ScmEnv.java (getType):  New helper method. 	(ScmEnvConstraint):  New helper class. 	(<init>):  Set unboundConstraint to new ScmEnvConstraint. 	* kawa/standard/call_with_values.java (apply(CallStack)):  New method. 	* kawa/standard/location.java (rewriteArg):  New static method. 	(rewrite):  Handle macros and both global and local variables.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@1174 5e0a886f-7f45-49c5-bc19-40643649e37f

--- kawa/standard/call_with_values.java
+++ kawa/standard/call_with_values.java
@@ -13,5 +13,23 @@ public class call_with_values extends Procedure2
     else
       return cons_proc.apply1 (values);
   }
+
+  public void apply (CallStack stack)
+  {
+    Procedure.checkArgCount(this, 2);
+    Object[] args = stack.args;
+    Object values = ((Procedure) args[0]).apply0 ();
+    Procedure consumer = (Procedure) args[1];
+    if (values instanceof Values)
+      args = ((Values) values).values();
+    else
+      {
+	args = new Object[1];
+	args[0] = values;
+      }
+    stack.args = args;
+    consumer.apply(stack);
+  }
 }
+
 
--- kawa/standard/location.java
+++ kawa/standard/location.java
@@ -2,6 +2,7 @@ package kawa.standard;
 import kawa.lang.*;
 import gnu.mapping.*;
 import gnu.expr.*;
+import gnu.bytecode.Access;
 
 /**
  * The Syntax transformer that re-writes the Kawa "location" primitive.
@@ -12,6 +13,35 @@ public class location extends Syntax implements Printable
 {
   static private Pattern pattern = new ListPat (2, 2);
 
+  /** Assuming obj is in an lvalue context, re-write it. */
+  public static Expression rewriteArg (Object obj, Translator tr)
+  {
+    while (obj instanceof Pair)
+      {
+	Pair pair = (Pair) obj;
+	Object proc = pair.car;
+	Object args = pair.cdr;
+	Syntax syntax = tr.check_if_Syntax (proc);
+	if (syntax == null)
+	  {
+	    int nargs = List.length(args);
+	    Expression[] xargs = new Expression[nargs];
+	    for (int i = 0; i < nargs; i++)
+	      {
+		pair = (Pair) args;
+		xargs[i] = tr.rewrite(pair.car);
+		args = pair.cdr;
+	      }
+	    return new ApplyExp(tr.rewrite(proc), xargs);
+	  }
+	if (syntax instanceof Macro)
+	  obj = ((Macro) syntax).expand(pair.cdr, tr);
+	else
+	  return syntax.rewrite(pair.cdr, tr);
+      }
+    return tr.rewrite(obj);
+  }
+
   public Expression rewrite (Object obj, Translator tr)
   {
     if (! (obj instanceof Pair))
@@ -19,15 +49,34 @@ public class location extends Syntax implements Printable
     Pair pair = (Pair) obj;
     if (pair.cdr != List.Empty)
       return tr.syntaxError ("extra arguments to location");
-    Expression arg = tr.rewrite(pair.car);
+    //    Expression arg = tr.rewrite(pair.car);
+    Expression arg = rewriteArg(pair.car, tr);
+    return location.rewrite(arg, tr);
+  }
+
+  public static Expression rewrite (Expression arg, Translator tr)
+  {
     if (arg instanceof ReferenceExp)
       {
 	ReferenceExp rexp = (ReferenceExp) arg;
 	rexp.setDontDereference(true);
 	Declaration binding = rexp.getBinding();
 	if (binding != null)
-	  binding.setIndirectBinding(true);
-	return rexp;
+	  {
+	    binding.setIndirectBinding(true);
+	    return rexp;
+	  }
+	else
+	  {
+	    gnu.bytecode.Method meth = Compilation.typeEnvironment
+	      .addMethod("getCurrentBinding", Compilation.string1Arg,
+			 Compilation.typeBinding,
+			 Access.PUBLIC|Access.STATIC);
+	    Expression proc = new QuoteExp(new PrimProcedure(meth));
+	    Expression[] args = new Expression[1];
+	    args[0] = new QuoteExp(rexp.getName());
+	    return new ApplyExp(proc, args);
+	  }
       }
     if (arg instanceof ApplyExp)
       {

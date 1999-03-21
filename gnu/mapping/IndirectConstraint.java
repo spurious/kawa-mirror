576d913473d71b1c1ad179c92c25e953281b1451
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
1999-03-21 16:35:16 +0000

    i 	* Constraint.java:  New abstract class. 	* TrivialConstraint.java:  New Constraint sub-class. 	* UnboundConstraint.java:  New Constraint sub-class. 	* IndirectConstraint.java:  New Constraint sub-class. 	* Makefile.am, Makefile.in (java_JAVA):  Add new classes.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@1156 5e0a886f-7f45-49c5-bc19-40643649e37f

---
+++ gnu/mapping/IndirectConstraint.java
@@ -0,0 +1,29 @@
+package gnu.mapping;
+
+public class IndirectConstraint extends Constraint
+{
+  public Object get (Binding binding)
+  {
+    return ((Procedure) binding.value).apply0();
+  }
+
+  public void set (Binding binding, Object value)
+  {
+    ((Procedure) (HasSetter) binding.value).set0(value);
+  }
+
+  public static void define (String name, Procedure location)
+  {
+    define(name, location, Environment.getCurrent());
+  }
+
+  public static void define (String name, Procedure location, Environment env)
+  {
+    Binding binding = env.getBinding(name);
+    synchronized (binding)
+      {
+	binding.value = location;
+	binding.constraint = new IndirectConstraint();
+      }
+  }
+}

68b19ec2eb07fa6cd6ab2e24d017d52728b889bb
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
1999-09-14 16:15:30 +0000

    * Window.java (next, previous):  Remove fields.  Instead use 	awt Component hierarchy. 	(getNextWindow, getOtherWindow, getContainedWindow, getFirstWindow,  	getLastWindow, getNextWindowInFrame):  New methods. 	(deleteNoValidate, deleteOtherWindows):  New methods. 	(split):  Moved in code from Frame.addWindow. 	* Frame.java (getFirstWindow, getLastWindow, getSelectedFrame, 	getSelectedWindow, otherWindow):  New methods. 	(first):  Remove field. 	(addWindow):  Remove. 	(delete, isLive):  New methods.
    	* Window.java (selected):  Remove static field.
    	(getSelected):  New method - gets window selected in selected Frame.
    	(setSelected):  Also make buffer current.
    	(warp):  Remove one overloaded method.
    
    
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@1443 5e0a886f-7f45-49c5-bc19-40643649e37f

--- gnu/jemacs/buffer/Frame.java
+++ gnu/jemacs/buffer/Frame.java
@@ -3,7 +3,9 @@ import javax.swing.*;
 
 public class Frame extends javax.swing.JFrame
 {
-  public Window first;
+  static Frame selectedFrame;
+  Window selectedWindow;
+
   JPanel contents;
 
   static int counter;
@@ -18,72 +20,65 @@ public class Frame extends javax.swing.JFrame
   {
     super(defaultName());
     setSize(400, 300);
-  }
-
-  /** Make a new window on BUFFER and link it after BEFORE.
-   * If BEFORE is null (untested), make the new window be first. */
-  public Window
-  addWindow (Buffer buffer, Window before, boolean horizontal)
-  {
-    Window window = new Window(buffer);
-    Frame frame = before.frame;
-    window.modeline = new Modeline(window);
-    if (frame.first == null)
-      {
-	window.next = window;
-	window.previous = window;
-	frame.first = window;
-      }
-    else
-      {
-	if (before == null)
-	  {
-	    before = frame.first.previous;
-	    frame.first = window;
-	  }
-	before.next = window;
-	window.previous = before;
-	window.next = before.next;
-	before.next = window;
-      }
-    window.frame = frame;
-
-    // We will re-use the old Window's panel as a parent for both
-    // the old and new Windows.
-    JPanel panel = before.getPanel();
-    if (before.modeline != null)
-      panel.remove(before.modeline);
-    if (before.scrollPane != null)
-      panel.remove(before.scrollPane);
-    else
-      panel.remove(before);
-
-    // New add both windows (old and new) to the panel.
-    // Both windows have to be re-wrapped.
-    int axis = horizontal ? BoxLayout.X_AXIS : BoxLayout.Y_AXIS;
-    panel.setLayout(new BoxLayout(panel, axis));
-    panel.add(before.wrap());
-    panel.add(window.wrap());
-    panel.validate();
-    return window;
-  }
-
-  /** Add a new window as the last window. */
-  public Window addWindow (Buffer buffer)
-  {
-    return addWindow(buffer, first == null ? null : first.previous, false);
+    if (selectedFrame == null)
+      selectedFrame = this;
+    setTitle("JEmacs");
   }
 
   public Frame (Buffer buffer)
   {
-    Window first = new Window(buffer, true);
-    first.frame = this;
-    contents = first.wrap();
+    Window win = new Window(buffer, true);
+    win.frame = this;
+    contents = win.wrap();
     getContentPane().add(contents);
     setSize(600, 400);
     setVisible(true);
-    if (Window.selected == null)
-      Window.setSelected(first);
+    Window.setSelected(win);
+    setTitle("JEmacs");
+  }
+
+  public void delete()
+  {
+    for (Window cur = getFirstWindow(); cur != null; )
+      {
+        Window next = cur.getNextWindow(true);
+        cur.deleteNoValidate();
+        cur = next;
+      }
+    contents = null;
+    dispose();
+    if (this == selectedFrame)
+      selectedFrame = null;
+  }
+
+  public boolean isLive()
+  {
+    return contents != null;
+  }
+
+  public Window getFirstWindow()
+  {
+    return Window.getFirstWindow(this);
+  }
+
+  public Window getLastWindow()
+  {
+    return Window.getLastWindow(this);
+  }
+
+  public static Frame getSelectedFrame()
+  {
+    return selectedFrame;
+  }
+
+  public Window getSelectedWindow()
+  {
+    return selectedWindow;
+  }
+
+  public Window otherWindow(int count)
+  {
+    return selectedWindow.getNextWindowInFrame(count);;
   }
 
   public String ask(String prompt)
--- gnu/jemacs/buffer/Window.java
+++ gnu/jemacs/buffer/Window.java
@@ -1,22 +1,18 @@
 package gnu.jemacs.buffer;
 import java.awt.event.FocusEvent;
 import javax.swing.*;
+import javax.swing.text.*;
 import java.awt.*;
 
 public class Window extends javax.swing.JTextPane
   implements java.awt.event.FocusListener
 {
-  static Window selected;
-  
   Modeline modeline;
   Frame frame;
   Buffer buffer;
   /** The panel that contains this window and the modeline. */
   JPanel panel;
   JScrollPane scrollPane;
-
-  Window next;
-  Window previous;
 
   /** Create new Window.
    * @param buffer the Buffer containing the data.
@@ -26,48 +22,40 @@ public class Window extends javax.swing.JTextPane
     this(buffer, true);
   }
 
+  /** Create new Window.
+   * @param buffer the Buffer containing the data.
+   * @param wantModeline true if we should create a mode line
+   */
   public Window(Buffer buffer, boolean wantModeline)
   {
     super(buffer.document);
-    this.buffer = buffer;
-    setKeymap(buffer.keymap);
+    if (wantModeline)
+      modeline = new Modeline(this, buffer.modelineDocument);
+    setBuffer(buffer);
     addFocusListener(this);
-    if (wantModeline)
-      modeline = new Modeline(this);
   }
 
-  /** Create new Window.
-   * @param buffer the Buffer containing the data.
-   * @param panel the parent panel (which should be empty)
-   *  that will contain this Window's JScrollPane and the optional modeline.
-   * @param wantModeline true if we should create a mode line
-   */
-  /*
-  public Window(Buffer buffer, JPanel panel, boolean wantModeline)
+  public Dimension getPreferredScrollableViewportSize()
   {
-    this(buffer);
-    if (wantModeline)
-    wrap(panel, wantModeline);
+    Dimension size = panel.getSize();
+    if (modeline != null)
+      size = new Dimension(size.width,
+                       size.height - modeline.getPreferredSize().height);
+    return size;
   }
-  */
 
-  public void wrap(JPanel panel)
+  /** Warp this (and optional modeline) inside a ScrollPane in a new JPanel. */
+  public JPanel wrap()
   {
-    panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+    BorderLayout layout = new BorderLayout();
+    JPanel panel = new JPanel(layout);
     scrollPane = new JScrollPane(this,
                                  JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
                                  JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-    panel.add(scrollPane);
+    panel.add(scrollPane, BorderLayout.CENTER);
     if (modeline != null)
-      panel.add(modeline);
+      panel.add(modeline, BorderLayout.SOUTH);
     this.panel = panel;
-  }
-
-  /** Warp this (and optional modeline) inside a new JPanel. */
-  public JPanel wrap()
-  {
-    JPanel panel = new JPanel();
-    wrap(panel);
     return panel;
   }
 
@@ -75,18 +63,6 @@ public class Window extends javax.swing.JTextPane
   public JPanel getPanel()
   {
     return panel;
-  }
-
-  public static Window getSelected()
-  {
-    return selected;
-  }
-
-  public static void setSelected(Window window)
-  {
-    if (selected != null)
-      selected.unselect();
-    window.select();
   }
 
   public static Window getWindow(java.awt.event.ActionEvent event)
@@ -101,17 +77,34 @@ public class Window extends javax.swing.JTextPane
       }
   }
 
-  public void select()
+  public static Window getSelected()
   {
-    selected = this;
-    buffer.curPosition = getCaret();
+    return Frame.selectedFrame == null ? null
+      : Frame.selectedFrame.selectedWindow;
   }
 
-  public void unselect()
+  public void setSelected()
+  {
+    Window selected = getSelected();
+    if (selected != null)
+      selected.unselect();
+    frame.selectedWindow = this;
+    Frame.selectedFrame = frame;
+    buffer.curPosition = getCaret();
+    Buffer.setCurrent(buffer);
+  }
+
+  public static void setSelected(Window window)
+  {
+    window.setSelected();
+    window.requestFocus();
+  }
+
+  void unselect()
   {
     buffer.point = buffer.curPosition.getDot();
     buffer.curPosition = null;
-    selected = null;
+    // ?? selected = null;
   }
 
   public Frame getFrame()
@@ -127,11 +120,15 @@ public class Window extends javax.swing.JTextPane
   public void setBuffer (Buffer buffer)
   {
     this.buffer = buffer;
+    System.err.println("<setBuffer buf:"+buffer+" dot:"+buffer.getDot());
     setDocument(buffer.document);
     setKeymap(buffer.keymap);
-    modeline.setDocument(buffer.modelineDocument);
-    buffer.curPosition = getCaret();
-    buffer.curPosition.setDot(buffer.point);
+    if (modeline != null)
+      modeline.setDocument(buffer.modelineDocument);
+    Caret caret = getCaret();
+    caret.setDot(buffer.getDot());
+    buffer.curPosition = caret;
+    System.err.println(">setBuffer buf:"+buffer+" dot:"+buffer.getDot());
   }
 
   /** Returns the "Emacs value" (1-origin) of point. */
@@ -147,46 +144,224 @@ public class Window extends javax.swing.JTextPane
 
   public Window split (int lines, boolean horizontal)
   {
-    return frame.addWindow(buffer, this, horizontal);
+    return split(buffer, lines, horizontal);
+  }
+
+  /** Split this window into two.
+   * Display Var>buffer</var> in the new window.
+   * @return the new Window.
+   */
+  public Window split (Buffer buffer, int lines, boolean horizontal)
+  {
+    Window window = new Window(buffer);
+    Frame frame = this.frame;
+    window.modeline = new Modeline(window, buffer.modelineDocument);
+    window.frame = frame;
+
+    JPanel panel = this.getPanel();
+    java.awt.Dimension oldSize = panel.getSize();
+    java.awt.Container oldParent = panel.getParent();
+    oldParent.remove(panel);
+    JSplitPane split
+      = new JSplitPane(horizontal ? JSplitPane.HORIZONTAL_SPLIT
+                       : JSplitPane.VERTICAL_SPLIT,
+                       panel, window.wrap());
+    split.setDividerSize(2);
+    // FIXME - use lines.
+    split.setDividerLocation((horizontal ? oldSize.width : oldSize.height) / 2);
+    oldParent.add(split);
+    oldParent.validate();
+    if (this == Window.getSelected())
+      this.requestFocus();
+
+    return window;
+  }
+
+  /** Return the next/previous Window in the cyclic order of windows.
+   * Returns null if this is the last/first window in this Frame. */
+  public Window getNextWindow(boolean forwards)
+  {
+    Container prev = this;
+    for (;;)
+      {
+        Container parent = prev.getParent();
+        if (parent == null)
+          return null;
+        if (parent instanceof JSplitPane)
+          {
+            JSplitPane split =(JSplitPane) parent;
+            if (prev == split.getLeftComponent())
+              {
+                if (forwards)
+                  return getFirstWindow((Container) split.getRightComponent());
+              }
+            else // prev == split.getRightComponent():
+              {
+                if (!forwards)
+                  return getLastWindow((Container) split.getLeftComponent());
+              }
+          }
+        prev = parent;
+      }
+  }
+
+  /** Return the next/previous Window in the cyclic order of windows.
+   * Returns first/last if this is the last/first window in this Frame. */
+  public Window getOtherWindow(boolean forwards)
+  {
+    Window win = getNextWindow(forwards);
+    if (win == null)
+      win = forwards ? frame.getFirstWindow() : frame.getLastWindow();
+    return win;
+  }
+
+  public static Window getContainedWindow(Container cont, boolean last)
+  {
+    for (;;)
+      {
+        if (cont instanceof Window)
+          return (Window) cont;
+        if (cont instanceof JScrollPane)
+          cont = (Container) ((JScrollPane) cont).getViewport().getView();
+        else if (cont instanceof JFrame)
+          cont = ((JFrame) cont).getContentPane();
+        else if (cont instanceof JSplitPane)
+          {
+            JSplitPane split = (JSplitPane) cont;
+            cont = (Container)
+              (last ? split.getRightComponent() : split.getLeftComponent());
+          }
+        else
+          {
+            int count = cont.getComponentCount();
+            if (count == 0)
+              return null;
+            cont = (Container) cont.getComponent(last ? (count - 1) : 0);
+          }
+      }
+  }
+
+  public static Window getFirstWindow(Container cont)
+  {
+    return getContainedWindow(cont, false);
+  }
+
+  public static Window getLastWindow(Container cont)
+  {
+    return getContainedWindow(cont, true);
+  }
+
+  public Window getNextWindowInFrame(int count)
+  {
+    Window win = this;
+    if (count > 0)
+      {
+        while (--count >= 0)
+          win = win.getOtherWindow(true);
+      }
+    else
+      {
+        while (++count <= 0)
+          win = win.getOtherWindow(false);
+      }
+    return win;
   }
 
   public void delete()
   {
-    if (frame.first == this)
-      frame.first = next;
+    Frame frame = this.frame;
+    deleteNoValidate();
+    if (frame.getFirstWindow() == null)
+      frame.delete();
     else
-      previous.next = next;
-    if (next != null)
-      next.previous = previous;
-    next = null;
-    previous = null;
+      frame.validate();
+  }
+
+  void deleteNoValidate()
+  {
+    if (frame.selectedWindow == this)
+      {
+        Window next = getNextWindowInFrame(1);
+        if (frame == Frame.selectedFrame)
+          setSelected(next);
+        else
+          frame.selectedWindow = next;
+      }
     if (modeline != null)
       panel.remove(modeline);
     panel.remove(scrollPane);
-    // FIXME rmeove parent panel if now singleton.
-    Container parent = panel.getParent();
+
+    // Mow remove the Panel from its parent.
+    Container oldParent = panel.getParent();
+    if (oldParent instanceof JSplitPane)
+      {
+        JSplitPane split = (JSplitPane) oldParent;
+        Component other;
+        if (panel == split.getLeftComponent())
+          other = split.getRightComponent();
+        else
+          other = split.getLeftComponent();
+        split.remove(this);
+        split.remove(other);
+        // In the JSplitPane's parent, replace the JSplitPane by just other 
+        Container splitParent = split.getParent();
+        if (splitParent instanceof JSplitPane)
+          {
+            JSplitPane outerSplit = (JSplitPane) splitParent;
+            if (split == outerSplit.getLeftComponent())
+              outerSplit.setLeftComponent(other);
+            else
+              outerSplit.setRightComponent(other);
+          }
+        else
+          {
+            splitParent.remove(split);
+            splitParent.add(other);
+          }
+      }
+    else
+      oldParent.remove(panel);
+
+    panel = null;
+    scrollPane = null;
+    frame = null;
+  }
+
+  public void deleteOtherWindows()
+  {
+    for (Window cur = frame.getFirstWindow(); cur != null; )
+      {
+        Window next = cur.getNextWindow(true);
+        if (cur != this)
+          cur.deleteNoValidate();
+        cur = next;
+      }
+    frame.validate();
   }
 
   public void focusGained(FocusEvent e)
   {
-    setSelected(this);
-    // System.err.println("focus gained for "+this+": "+e);
+    setSelected();
   }
 
   public void focusLost(FocusEvent e)
   {
-    //System.err.println("focus lost for "+this+": "+e);
   }
 
   public String toString()
   {
     StringBuffer sbuf = new StringBuffer(100);
-    sbuf.append("#<window on \"");
-    sbuf.append(buffer.getName());
-    sbuf.append("\" 0x");
+    sbuf.append("#<window on ");
+    if (buffer == null)
+      sbuf.append("no buffer");
+    else
+      {
+        sbuf.append('\"');
+        sbuf.append(buffer.getName());
+        sbuf.append('\"');
+      }
+    sbuf.append(" 0x");
     sbuf.append(Integer.toHexString(System.identityHashCode(this)));
-    sbuf.append(" size: ");
-    sbuf.append(getSize());
     sbuf.append('>');
     return sbuf.toString();
   }

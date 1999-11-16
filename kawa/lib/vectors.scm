a58adbfdb45eba539573d7d1838c723f4997e411
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
1999-11-16 01:26:13 +0000

    Ch vectors.scm:  Use invoke.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@1492 5e0a886f-7f45-49c5-bc19-40643649e37f

--- kawa/lib/vectors.scm
+++ kawa/lib/vectors.scm
@@ -2,24 +2,18 @@
   (instance? x <vector>))
 
 (define (vector-length (x <vector>))
-  ((primitive-virtual-method <vector> "length" <int> ())
-   x))
+  (invoke x 'length))
 
 (define (vector-ref
          (vector <vector>)
          (k <int>))
-  ((primitive-virtual-method <vector> "elementAt"
-			     <object> (<int>))
-   vector k))
+  (invoke vector 'elementAt k))
 
 (define (vector-set!
          (vector <vector>)
          (k <int>)
          obj)
-  ((primitive-virtual-method <vector> "setElementAt"
-			     <void> (<object> <int>))
-   vector obj k))
+  (invoke vector 'setElementAt obj k))
 
 (define (list->vector (x <list>))
-  ((primitive-virtual-method <list> "toVector" <vector> ())
-   x))
+  (invoke x 'toVector))

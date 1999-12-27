d351998e0834e731729005fe66662f0dc2802775
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
1999-12-27 21:00:10 +0000

    * files.scm (*temp-file-number*):  Define using (new) define-private.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@1556 5e0a886f-7f45-49c5-bc19-40643649e37f

--- kawa/lib/files.scm
+++ kawa/lib/files.scm
@@ -66,7 +66,7 @@
 ; From scsh
 ;(define (directory-files [dir [dotfiles?]]) ...)
 
-(define *temp-file-number* 1)
+(define-private *temp-file-number* 1)
 ; From MzLib.  Scsh has (create-temp-file [prefix]).
 ; This is not safe from race conditions!
 ; Fix this using new Java2 File.createTempFile.  FIXME.

e4aa9f57ebd42dfad83c607117c2e19e74a07b23
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
2000-02-13 05:15:10 +0000

    * scribble.scm:  New "applet" testcase. 	* scribble.html:  Web page that uses scribble applet. 	* Makefile.am (scribble.class, check-scribble):  New rules.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@1641 5e0a886f-7f45-49c5-bc19-40643649e37f

---
+++ testsuite/scribble.scm
@@ -0,0 +1,27 @@
+;; This is the scribble applet in Flanagan's "Java Examples in a Nutshell"
+;; (O'Reilly, 1997), transcribed into Kawa-Scheme.
+
+(define-private last-x 0)
+(define-private last-y 0)
+
+(define (init) <void>
+  (let ((applet :: <java.applet.Applet> (this)))
+    (invoke applet 'addMouseListener
+	    (object (<java.awt.event.MouseAdapter>)
+		    ((mousePressed (e :: <java.awt.event.MouseEvent>)) <void>
+		     (set! last-x (invoke e 'getX))
+		     (set! last-y (invoke e 'getY)))))
+    (invoke applet 'addMouseMotionListener
+	    (object (<java.awt.event.MouseMotionAdapter>)
+		    ((mouseDragged (e :: <java.awt.event.MouseEvent>)) <void>
+		     (let ((g :: <java.awt.Graphics>
+			      (invoke applet 'getGraphics))
+			   (x :: <int> (invoke e 'getX))
+			   (y :: <int> (invoke e 'getY)))
+		       (invoke g 'drawLine last-x last-y x y)
+		       (set! last-x x)
+		       (set! last-y y)))))))
+
+(define (start) <void> (format #t "called start.~%~!"))
+(define (stop) <void> (format #t "called stop.~%~!"))
+(define (destroy) <void> (format #t "called destroy.~%~!"))

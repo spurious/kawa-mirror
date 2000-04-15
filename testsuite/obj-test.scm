c7600332129e69edf86307eae5b2c6f81190a557
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
2000-04-15 05:57:01 +0000

    * obj-test.scm: Added 4 tests for record with names that need mangling.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@1756 5e0a886f-7f45-49c5-bc19-40643649e37f

--- testsuite/obj-test.scm
+++ testsuite/obj-test.scm
@@ -1,4 +1,4 @@
-(test-init "Objects" 29)
+(test-init "Objects" 33)
 
 ;; Force procedure to be applied without being inlined:
 (define-syntax force-eval
@@ -27,6 +27,17 @@
 (test '(re im) record-type-field-names complex)
 
 (test 20 'set! (begin (set! (z 'im) 15) (+ (z 're) (z 'im))))
+
+;; Check name mangling and demangling of records.
+(define funny-record (make-record-type 'lispy-name->foo!? '(a! b-c)))
+(test "lispy-name->foo!?" record-type-name funny-record)
+(test '(a! b-c) record-type-field-names funny-record)
+(define make-funny-record1 (record-constructor funny-record))
+(define make-funny-record2 (record-constructor funny-record '(b-c a!)))
+(define lt1 (make-funny-record1 10 12))
+(test 10 'accessor21 ((record-accessor funny-record 'a!) lt1))
+((record-modifier funny-record 'b-c) lt1 14)
+(test (make-funny-record2 14 10) 'funny-record lt1)
 
 (test '(10 \10 20 \20) 'object-with-field-1
        (let*

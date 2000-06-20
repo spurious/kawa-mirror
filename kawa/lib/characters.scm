090daeb0d72cb9f35420144037aff3abb9022931
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
2000-06-20 03:35:42 +0000

    * characters.scm (char=?, char<?, char>?, char<=?, char>=?, 	char-ci=?, char-ci<?, char-ci>?, char-ci<=?, char-ci>=?): 	New procedures.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@1904 5e0a886f-7f45-49c5-bc19-40643649e37f

--- kawa/lib/characters.scm
+++ kawa/lib/characters.scm
@@ -1,5 +1,5 @@
 (define (char? x)
-  (instance? x <gnu.kawa.util.Char>))
+  (instance? x <character>))
 
 (define (char-alphabetic? (char <char>))
   (invoke-static <java.lang.Character> "isLetter" char))
@@ -16,16 +16,47 @@
 (define (char-lower-case? (char <char>))
   (invoke-static <java.lang.Character> "isLowerCase" char))
 
-(define (char->integer (char <char>))
-  ((primitive-virtual-method <gnu.kawa.util.Char> "intValue"
-			     <int> ())
-   char))
+(define (char->integer (char <character>))
+  (invoke char 'intValue))
 
 (define (integer->char (n <int>))
-  (invoke-static <gnu.kawa.util.Char> "make" n))
+  (invoke-static <character> 'make n))
 
 (define (char-upcase (char <char>))
   (invoke-static <java.lang.Character> "toUpperCase" char))
 
 (define (char-downcase (char <char>))
   (invoke-static <java.lang.Character> "toLowerCase" char))
+
+(define (char=? (c1 :: <character>) (c2 :: <character>))
+  (invoke-static <character> '$Eq c1 c2))
+(define (char<? (c1 :: <character>) (c2 :: <character>))
+  (invoke-static <character> '$Ls c1 c2))
+(define (char>? (c1 :: <character>) (c2 :: <character>))
+  (invoke-static <character> '$Gr c1 c2))
+(define (char<=? (c1 :: <character>) (c2 :: <character>))
+  (invoke-static <character> '$Ls$Eq c1 c2))
+(define (char>=? (c1 :: <character>) (c2 :: <character>))
+  (invoke-static <character> '$Gr$Eq c1 c2))
+
+(define (char-ci=? (c1 :: <character>) (c2 :: <character>))
+  (invoke-static <character> '$Eq
+                 (invoke-static <java.lang.Character> 'toUpperCase c1)
+                 (invoke-static <java.lang.Character> 'toUpperCase c2)))
+(define (char-ci<? (c1 :: <character>) (c2 :: <character>))
+  (invoke-static <character> '$Ls
+                 (invoke-static <java.lang.Character> 'toUpperCase c1)
+                 (invoke-static <java.lang.Character> 'toUpperCase c2)))
+(define (char-ci>? (c1 :: <character>) (c2 :: <character>))
+  (invoke-static <character> '$Gr
+                 (invoke-static <java.lang.Character> 'toUpperCase c1)
+                 (invoke-static <java.lang.Character> 'toUpperCase c2)))
+(define (char-ci<=? (c1 :: <character>) (c2 :: <character>))
+  (invoke-static <character> '$Ls$Eq
+                 (invoke-static <java.lang.Character> 'toUpperCase c1)
+                 (invoke-static <java.lang.Character> 'toUpperCase c2)))
+(define (char-ci>=? (c1 :: <character>) (c2 :: <character>))
+  (invoke-static <character> '$Gr$Eq
+                 (invoke-static <java.lang.Character> 'toUpperCase c1)
+                 (invoke-static <java.lang.Character> 'toUpperCase c2)))
+

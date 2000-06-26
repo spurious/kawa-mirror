f45365d0f8837c269476909dd77cb52663653b1f
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
2000-06-26 03:43:00 +0000

    New directory. 	* lang-test.lisp:  New test set. 	* Makefile.am:  New.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@1918 5e0a886f-7f45-49c5-bc19-40643649e37f

---
+++ gnu/commonlisp/testsuite/lang-test.lisp
@@ -0,0 +1,19 @@
+(test-init "Common Lisp tests" 4)
+
+(setq y 100)
+(defun foo1 (x)
+  (lambda (y)
+    (/ x y)))
+(defvar foo12 (foo1 12))
+(test 4 'lexical-test-1 (apply foo12 '(3)))
+
+;(test t 'eq-test-1 (eq t 't))
+
+(test t 'equal-test-1 (equal "tt" "tt"))
+(test nil 'equal-test-1 (equal "tt" "tt "))
+
+(test "The octal value of 18 is 22,
+   and the hex value is 12."
+      'format-test-1
+      (format "The octal value of ~d is ~o,
+   and the hex value is ~x." 18 18 18))

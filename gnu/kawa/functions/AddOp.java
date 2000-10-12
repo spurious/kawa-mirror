6bd245d75a2426387a33d9db47bb68543c52a67f
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
2000-10-12 22:33:03 +0000

    * MakeProcedure.java:  New class. 	* Makefile.am:  Update accordingly.
    	* AddOp.java:  Implement gnu.expr.CanInline.
    	(inline):  New method.
    	(pairwise):  New static help method.
    
    
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@1978 5e0a886f-7f45-49c5-bc19-40643649e37f

--- gnu/kawa/functions/AddOp.java
+++ gnu/kawa/functions/AddOp.java
@@ -11,7 +11,7 @@ import gnu.bytecode.PrimType;
  * @author Per Bothner
  */
 
-public class AddOp extends ProcedureN
+public class AddOp extends ProcedureN implements CanInline
 {
   int plusOrMinus = 1;
 
@@ -83,5 +83,120 @@ public class AddOp extends ProcedureN
   public Object applyN (Object[] args)
   {
     return applyN(plusOrMinus, args);
+  }
+
+  /** Convert (PROC A B C) to (PROC (PROC A B) C) etc. */
+
+  public static Expression pairwise(Procedure proc,
+                                    Expression rproc, Expression[] args)
+  {
+    int len = args.length;
+    Expression prev = args[0];
+    for (int i = 1;  i < len;  i++)
+      {
+        Expression[] args2 = new Expression[2];
+        args2[0] = prev;
+        args2[1] = args[i];
+        ApplyExp next = new ApplyExp(rproc, args2);
+        if (proc instanceof CanInline)
+          prev = ((CanInline) proc).inline(next);
+        else
+          prev = next;
+      }
+    return prev;
+  }
+
+  public Expression inline (ApplyExp exp)
+  {
+    Expression folded = ApplyExp.inlineIfConstant(this, exp);
+    if (folded != exp)
+      return folded;
+    Expression[] args = exp.getArgs();
+    if (args.length > 2)
+      return pairwise(this, exp.getFunction(), args);
+    if (args.length == 1 && plusOrMinus < 0)
+      {
+        Type type0 = args[0].getType();
+        if (type0 instanceof PrimType)
+          {
+            char sig0 = type0.getSignature().charAt(0);
+            Type type = null;
+            int opcode = 0;
+            if (sig0 == 'V' || sig0 == 'Z' || sig0 == 'C')
+              {
+                // error
+              }
+            else if (sig0 == 'D')
+              {
+                opcode = 119 /* dneg */;
+                type = Type.double_type;
+              }
+            else if (sig0 == 'F')
+              {
+                opcode = 118 /* fneg */;
+                type = Type.float_type;
+              }
+            else if (sig0 == 'J')
+              {
+                opcode = 117 /* lneg */;
+                type = Type.long_type;
+              }
+            else
+              {
+                opcode = 116 /* ineg */;
+                type = Type.int_type;
+              }
+            if (type != null)
+              {
+                PrimProcedure prim
+                  = PrimProcedure.makeBuiltinBinary(opcode, type);
+                return new ApplyExp(prim, args);
+              }
+          }
+      }
+    if (args.length == 2)
+      {
+        Type type0 = args[0].getType();
+        Type type1 = args[1].getType();
+        if (type0 instanceof PrimType && type1 instanceof PrimType)
+          {
+            char sig0 = type0.getSignature().charAt(0);
+            char sig1 = type1.getSignature().charAt(0);
+            Type type = null;
+            int opcode = 0;
+            if (sig0 == 'V' || sig0 == 'Z' || sig0 == 'C'
+                || sig1 == 'V' || sig1 == 'Z' || sig1 == 'C')
+              {
+                // error
+              }
+            else if (sig0 == 'D' || sig1 == 'D')
+              {
+                opcode = plusOrMinus > 0 ? 99 /* dadd */ : 103 /* dsub */;
+                type = Type.double_type;
+              }
+            else if (sig0 == 'F' || sig1 == 'F')
+              {
+                opcode = plusOrMinus > 0 ? 98 /* fadd */ : 102 /* fsub */;
+                type = Type.float_type;
+              }
+            else if (sig0 == 'J' || sig1 == 'J')
+              {
+                opcode = plusOrMinus > 0 ? 97 /* ladd */ : 101 /* lsub */;
+                type = Type.long_type;
+              }
+            else
+              {
+                opcode = plusOrMinus > 0 ? 96 /* iadd */ : 100 /* isub */;
+                type = Type.int_type;
+              }
+            if (type != null)
+              {
+                PrimProcedure prim
+                  = PrimProcedure.makeBuiltinBinary(opcode, type);
+                return new ApplyExp(prim, args);
+              }
+          }
+      }
+    return exp;
   }
 }
---
+++ gnu/kawa/functions/MakeProcedure.java
@@ -0,0 +1,80 @@
+package gnu.kawa.functions;
+import gnu.mapping.*;
+import gnu.expr.*;
+
+public class MakeProcedure extends ProcedureN implements CanInline
+{
+  public static final MakeProcedure makeProcedure = new MakeProcedure();
+  static { makeProcedure.setName("make-procedure"); }
+
+  public static GenericProc makeProcedure$V (Object[] args)
+  {
+    return GenericProc.make(args);
+  }
+
+  public Object applyN(Object[] args)
+  {
+    return GenericProc.make(args);
+  }
+
+  public Expression inline (ApplyExp exp)
+  {
+    Expression[] args = exp.getArgs();
+    int alen = args.length;
+    Expression method = null;
+    int countMethods = 0;
+    String name = null;
+    for (int i = 0;  i < alen;  i++)
+      {
+	Expression arg = args[i];
+        Object key;
+	if (arg instanceof QuoteExp
+            && (key = ((QuoteExp) arg).getValue()) instanceof Keyword)
+	  {
+	    String keyword = ((Keyword) key).getName();
+	    Expression next = args[++i];
+	    if (keyword == "name")
+              {
+                if (next instanceof QuoteExp)
+                  name = ((QuoteExp) next).getValue().toString();
+              }
+	    else if (keyword == "method")
+              {
+                countMethods++;
+                method = next;
+              }
+	    else
+	      ; // result.setProperty(keyword, value);
+	  }
+	else
+          {
+            countMethods++;
+            method = arg;
+          }
+      }
+    if (countMethods == 1 && method instanceof LambdaExp)
+      {
+        LambdaExp proc = (LambdaExp) method;
+        System.err.println("inline makeProc count:"+countMethods+" name:"+name);
+        for (int i = 0;  i < alen;  i++)
+          {
+            Expression arg = args[i];
+            Object key;
+            if (arg instanceof QuoteExp
+                && (key = ((QuoteExp) arg).getValue()) instanceof Keyword)
+              {
+                String keyword = ((Keyword) key).getName();
+                Expression next = args[++i];
+                if (keyword == "name")
+                  proc.setName(name);
+                else if (keyword == "method")
+                  ;
+                else
+                  proc.setProperty(keyword, next);
+              }
+          }
+        return method;
+      }
+    return exp;
+  }
+}

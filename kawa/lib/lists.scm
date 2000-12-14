af5981162ef447084c098fd14a83744b183a3ed5
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
2000-12-14 04:14:02 +0000

    * lists.scm (reverse, reverse!):  New procedures.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@2021 5e0a886f-7f45-49c5-bc19-40643649e37f

--- kawa/lib/lists.scm
+++ kawa/lib/lists.scm
@@ -31,3 +31,13 @@
 
 (define (length list :: <list>) :: <int>
   (invoke-static <list> 'length list))
+
+(define (reverse (list :: <list>)) :: <list>
+  (let loop ((arg list) (result '()))
+    (if (null? arg) result
+	(let ((pair :: <pair> arg))
+	  (loop (cdr pair) (cons (car pair) result))))))
+
+;; Not in R5RS, but is in Guile (with extra mystery argument).
+(define (reverse! (list :: <list>)) :: <list>
+  (invoke-static <list> 'reverseInPlace list))

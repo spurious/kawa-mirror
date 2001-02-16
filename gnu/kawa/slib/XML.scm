22490dea3b7df6ad11901d6a21e424e31f8f1b49
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
2001-02-16 21:09:42 +0000

    * configure.in:  New --enable-xml flag. 	(XML_SELECTED):  New substitution macro. 	* gnu/kawa/Makefile.am (SUBDIR):  Add optional xml directory. 	* kawa/standard/Scheme.java (getNamedType):  Add <document>.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@2031 5e0a886f-7f45-49c5-bc19-40643649e37f

--- gnu/kawa/slib/XML.scm
+++ gnu/kawa/slib/XML.scm
@@ -1,8 +1,10 @@
+;;; Parse an XML file (specified by a URL or url string), giving a <document>.
 (define (parse-xml-from-url url) :: <document>
   (if (not (instance? url <java.net.URL>))
       (set! url (make <java.net.URL> (invoke url 'toString))))
   (invoke-static <gnu.kawa.xml.ParsedXMLToConsumer> 'parse url))
 
+;;; Print a Consumable (such as a <document>) to a port, in XML syntax.
 (define (print-as-xml (data :: <gnu.kawa.util.Consumable>)
 		      #!optional (out (current-output-port)))
   (invoke data 'consume (make <gnu.kawa.xml.XMLPrinter> out)))

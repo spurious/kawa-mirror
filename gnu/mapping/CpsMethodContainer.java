492d2b079de749b3ff2f842f4bfd4d359561aebb
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
2001-02-28 06:05:10 +0000

    * CpsMethodContainer.java:  New interface. 	* CpsMethodProc.java:    New class, extends CpsProcedure. 	* Makefile.am:  Update accordingly.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@2033 5e0a886f-7f45-49c5-bc19-40643649e37f

---
+++ gnu/mapping/CpsMethodContainer.java
@@ -0,0 +1,12 @@
+// Copyright (c) 2001  Per M.A. Bothner.
+// This is free software;  for terms and warranty disclaimer see ./COPYING.
+
+package gnu.mapping;
+
+/** An object that may have methods that provide the "action" needed
+ * by an CpsMethodProc. */
+
+public interface CpsMethodContainer
+{
+  public Object apply(CpsMethodProc proc, CallContext context);
+}
---
+++ gnu/mapping/CpsMethodProc.java
@@ -0,0 +1,27 @@
+// Copyright (c) 2001  Per M.A. Bothner.
+// This is free software;  for terms and warranty disclaimer see ./COPYING.
+
+package gnu.mapping;
+
+public class CpsMethodProc extends CpsProcedure
+{
+  CpsMethodContainer module;
+  public final int selector;
+  private int numArgs;
+
+  public CpsMethodProc(CpsMethodContainer module, int selector,
+			String name, int numArgs)
+  {
+    this.module = module;
+    this.selector = selector;
+    this.numArgs = numArgs;
+    setName(name);
+  }
+
+  public int numArgs() { return numArgs; }
+
+  public void apply (CallContext context)
+  {
+    module.apply(this, context);
+  }
+}

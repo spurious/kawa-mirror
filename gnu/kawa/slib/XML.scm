1f09bab050a790f676ed484f0722b9034e7d9209
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
2001-04-10 03:30:45 +0000

    * Makefile.am (CLEANFILES):  Removed redundant macro.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@2105 5e0a886f-7f45-49c5-bc19-40643649e37f

--- gnu/kawa/slib/XML.scm
+++ gnu/kawa/slib/XML.scm
@@ -4,6 +4,29 @@
       (set! url (make <java.net.URL> (invoke url 'toString))))
   (invoke-static <gnu.kawa.xml.ParsedXMLToConsumer> 'parse url))
 
+#|
+(define (parse-nsxml-from-url url) :: <document>
+  (if (not (instance? url <java.net.URL>))
+      (set! url (make <java.net.URL> (invoke url 'toString))))
+  (let* ((doc :: <document> (make <document>))
+	 (parser :: <gnu.kawa.xml.XMLParser>
+		 (make <gnu.kawa.xml.XMLParser> url
+		       (make <gnu.kawa.xml.ParsedXMLToConsumer>
+			 (make <gnu.kawa.xml.NamespaceResolver> doc)))))
+    (invoke parser 'parse)
+    doc))
+
+(define (print-nsxml-from-url url #!optional (out (current-output-port)))
+  (if (not (instance? url <java.net.URL>))
+      (set! url (make <java.net.URL> (invoke url 'toString))))
+  (let ((parser :: <gnu.kawa.xml.XMLParser>
+		(make <gnu.kawa.xml.XMLParser> url
+		      (make <gnu.kawa.xml.ParsedXMLToConsumer>
+			(make <gnu.kawa.xml.NamespaceResolver>
+			  (make <gnu.kawa.xml.XMLPrinter> out))))))
+    (invoke parser 'parse)))
+|#
+
 ;;; Print a Consumable (such as a <document>) to a port, in XML syntax.
 (define (print-as-xml (data :: <gnu.lists.Consumable>)
 		      #!optional (out (current-output-port)))

6be657ff5ea3cb6735f042f348a249f8885b80a1
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
2001-08-18 01:14:53 +0000

    * ValuesMap.java:  New procedure, extends CpsProcedure. 	* Makefile.am:  Update accordingly.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@2308 5e0a886f-7f45-49c5-bc19-40643649e37f

---
+++ gnu/kawa/functions/ValuesMap.java
@@ -0,0 +1,53 @@
+// Copyright (c) 2001  Per M.A. Bothner and Brainfood Inc.
+// This is free software;  for terms and warranty disclaimer see ./COPYING.
+
+package gnu.kawa.functions;
+import gnu.lists.*;
+import gnu.mapping.*;
+
+/** Map a unary function over a value sequence, yielding a new sequence.
+ * Used to implement XQuery's 'for' form.
+ */
+
+public class ValuesMap extends CpsProcedure
+{
+  public static final ValuesMap valuesMap = new ValuesMap();
+
+  public int numArgs() { return 0x2002; }
+
+  public void apply (CallContext ctx)
+  {
+    Procedure proc = (Procedure) ctx.getNextArg();
+    Consumer out = ctx.consumer;
+    Object val = ctx.getNextArg();
+    Procedure.checkArgCount(proc, 1);
+    if (val instanceof Values)
+      {
+	int ipos = 0;
+	Values values = (Values) val;
+	System.err.print("values-map:");values.dump();
+	for (;;)
+	  {
+	    Object v = values.getNext(ipos, null);
+	    System.err.println("map val: ipos:"+ipos+"->"+v);
+	    if (v == Sequence.eofValue)
+	      break;
+	    ctx.setArgs(v);
+	    ctx.proc = proc;
+	    ctx.run();
+	    ipos = values.nextDataIndex(ipos >> 1);
+	    /*
+	    if (ipos < 0)
+	      break;
+	    */
+	    ipos = ipos << 1;
+	  }
+      }
+    else
+      {
+	ctx.setArgs(val);
+	ctx.proc = proc;
+	ctx.run();
+      }
+  }
+}

4d5114670e7ab34f4f5f7abc807be1b295d88efb
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
2001-11-01 21:46:45 +0000

    * tab.xml.in:  Add some attributes so we can test for that. 	* TestMisc.java:  New class with many small tests. 	* test1.xql, expected1.xml:  Removed - subsumed by TestMisc.java. 	* Makefile.am: Update accordingly.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@2454 5e0a886f-7f45-49c5-bc19-40643649e37f

---
+++ gnu/xquery/testsuite/TestMisc.java
@@ -0,0 +1,164 @@
+import gnu.expr.*;
+import kawa.*;
+import gnu.mapping.*;
+import gnu.xquery.lang.*;
+import gnu.text.*;
+
+public class TestMisc
+{
+  static { XQuery.registerEnvironment(); }
+  static Interpreter interp = Interpreter.getInterpreter();
+  static Environment env = Environment.getCurrent();
+
+  static int expectedPasses = 0;
+  static int unexpectedPasses = 0;
+  static int expectedFailures = 0;
+  static int unexpectedFailures = 0;
+  static boolean verbose = false;
+  static String failureExpectedNext = null;
+
+  public static void main(String[] args)
+  {
+    evalTest("3.4+1", "4.4");
+    evalTest("3.4+1 ,4*2.5", "4.4 10.0");
+    evalTest("3<5", "true");
+    evalTest("let $x:=3+4 return $x", "7");
+    evalTest("let $x:=3+4 return <a>{$x}</a>", "<a>7</a>");
+
+    evalTest("for $y in (4,5,2+4) return <b>{10+$y}</b>",
+	     "<b>14</b><b>15</b><b>16</b>");
+
+    evalTest("(3,4,5)[3]", "5");
+    evalTest("1,((2,3)[false()]),5", "1 5");
+    evalTest("1,((2,3)[true()]),5", "1 2 3 5");
+    evalTest("(for $y in (5,4) return <b>{10+$y}</b>)[2]", "<b>14</b>");
+
+    evalTest("document(\"tab.xml\")/result",
+	     "<result>\n" +
+	     "<row>\n" +
+	     "<fld1>a1</fld1>\n" +
+	     "<fld2 align=\"right\">12</fld2>\n" +
+	     "</row>\n" +
+	     "<row>\n" +
+	     "<fld1 align=\"left\">b1</fld1>\n" +
+	     "<fld2 align=\"right\">22</fld2>\n" +
+	     "</row>\n" +
+	     "</result>");
+    evalTest("document(\"tab.xml\")/result/row/fld2",
+	     "<fld2 align=\"right\">12</fld2><fld2 align=\"right\">22</fld2>");
+    evalTest("document(\"tab.xml\")/result/row[fld2]",
+	     "<row>\n" +
+	     "<fld1>a1</fld1>\n" +
+	     "<fld2 align=\"right\">12</fld2>\n" +
+	     "</row><row>\n" +
+	     "<fld1 align=\"left\">b1</fld1>\n" +
+	     "<fld2 align=\"right\">22</fld2>\n" +
+	     "</row>");
+    evalTest("document(\"tab.xml\")/result/row/*",
+	     "<fld1>a1</fld1><fld2 align=\"right\">12</fld2><fld1 align=\"left\">b1</fld1><fld2 align=\"right\">22</fld2>");
+    evalTest("document(\"tab.xml\")/result/row[2]",
+	     "<row>\n" +
+	     "<fld1 align=\"left\">b1</fld1>\n" +
+	     "<fld2 align=\"right\">22</fld2>\n" +
+	     "</row>");
+    evalTest("document(\"tab.xml\")/result/row/*[2]",
+	     "<fld2 align=\"right\">12</fld2><fld2 align=\"right\">22</fld2>");
+
+    failureExpectedNext = "";
+    evalTest("(document(\"tab.xml\")/result/row/*)[2]",
+	     "<fld2 align=\"right\">12</fld2>");
+
+    evalTest("document(\"tab.xml\")/result/row/(fld2,fld1)",
+	     "<fld2 align=\"right\">12</fld2><fld1>a1</fld1><fld2 align=\"right\">22</fld2><fld1 align=\"left\">b1</fld1>");
+
+    evalTest("let $x:=12,\n" +
+	     "    $y:=<a>{$x+$x}</a>\n" +
+	     "  return <b atr1='11' atr2=\"{$x}\">{($y,99,$y)}</b>",
+	     "<b atr1=\"11\" atr2=\"12\"><a>24</a>99<a>24</a></b>");
+
+    System.out.println("# of expected passes      " + expectedPasses);
+    if (expectedFailures > 0)
+      System.out.println("# of expected failures    " + expectedFailures);
+    if (unexpectedPasses > 0)
+      System.out.println("# of unexpected passes    " + unexpectedPasses);
+    if (unexpectedFailures > 0)
+      System.out.println("# of unexpected failures  " + unexpectedFailures);
+  }
+
+  public static void evalTest(String expr, String expected)
+  {
+    String result;
+    try
+      {
+	result = eval(expr);
+      }
+    catch (Throwable ex)
+      {
+	if (verbose)
+	  ex.printStackTrace(System.out);
+	result = "*** caught " + ex.getClass().getName() + " ***";
+      }
+    boolean failureExpected = failureExpectedNext != null;
+    if (expected.equals(result))
+      {
+	if (failureExpected)
+	  unexpectedPasses++;
+	else
+	  expectedPasses++;
+	if (verbose || failureExpected)
+	  System.out.println((failureExpected ? "XPASS: \"" : "PASS: \"")
+			     + expr + "\" evaluated to \"" + result + "\"");
+      }
+    else
+      {
+	if (failureExpectedNext != null)
+	  expectedFailures++;
+	else
+	  unexpectedFailures++;
+	if (verbose || ! failureExpected)
+	  System.out.println((failureExpected ? "XFAIL: \"" : "FAIL: \"")
+			     + expr + "\" evaluated to \"" + result
+			     + "\" but expected \"" + expected + "\"");
+      }
+    failureExpectedNext = null;
+  }
+
+  public static String eval(String expr)
+    throws Throwable
+  {
+    CharArrayOutPort out = new CharArrayOutPort();
+    InPort in = new CharArrayInPort(expr);
+    OutPort err = OutPort.errDefault();
+    SourceMessages messages = new SourceMessages();
+    Lexer lexer = interp.getLexer(in, messages);
+    CallContext ctx = new CallContext();
+    ctx.consumer = interp.getOutputConsumer(out);
+
+    ModuleExp mod = interp.parse(env, lexer);
+    if (mod == null)
+      return "*** end-of-file ***";
+    mod.setName("atInteractiveLevel");  // FIXME
+    if (lexer.checkErrors(err, 20))
+      return "*** syntax error ***";
+
+    int ch;
+    for (;;)
+      {
+	ch = in.read();
+	if (ch < 0 || ch == '\r' || ch == '\n')
+	  break;
+	if (ch != ' ' && ch != '\t')
+	  {
+	    in.unread();
+	    break;
+	  }
+      }
+
+    mod.evalModule(env, ctx);
+    ctx.runUntilDone();
+    if (ch >= 0)
+      return "*** junk at end of input ***";
+
+    return new String(out.toCharArray());
+  }
+}

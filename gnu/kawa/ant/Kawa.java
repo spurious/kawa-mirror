2640418ea2c48e5570369e367f0328e9b4f71d08
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
2001-11-19 06:24:31 +0000

    New directory - Ant support from Jim White.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@2515 5e0a886f-7f45-49c5-bc19-40643649e37f

---
+++ gnu/kawa/ant/Kawa.java
@@ -0,0 +1,24 @@
+/*
+ * Kawa.java
+ *
+ * Created on November 17, 2001, 12:44 PM
+ */
+
+package gnu.kawa.ant;
+
+import org.apache.tools.ant.taskdefs.MatchingTask;
+
+/**
+ *
+ * @author  jim
+ * @version 
+ */
+public class Kawa extends MatchingTask
+{
+
+    /** Creates new Kawa */
+    public Kawa() 
+    {
+    }
+
+}

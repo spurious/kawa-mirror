2e6a79fa44e63129f88be8a469c9559bbf870080
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
2002-01-19 19:07:39 +0000

    * Write.java:  Moved from gnu.kawa.functions. 	* Makefile.am:  Update accordingly.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@2612 5e0a886f-7f45-49c5-bc19-40643649e37f

---
+++ gnu/kawa/xml/WriteTo.java
@@ -0,0 +1,28 @@
+// Copyright (c) 2001  Per M.A. Bothner and Brainfood Inc.
+// This is free software;  for terms and warranty disclaimer see ./COPYING.
+
+package gnu.kawa.xml;
+import gnu.lists.*;
+import gnu.mapping.*;
+import gnu.xml.*;
+
+/** Write a value to a named file. */
+
+public class WriteTo extends Procedure2 // FIXME: implements Inlineable
+{
+  public static final WriteTo writeTo = new WriteTo();
+
+  public static void writeTo(Object value, String fileName) throws Throwable
+  {
+    OutPort out = new OutPort(new java.io.FileWriter(fileName), fileName);
+    XMLPrinter consumer = new XMLPrinter(out);
+    Values.writeValues(value, consumer);
+    out.close();
+  }
+
+  public Object apply2 (Object value, Object fileName) throws Throwable
+  {
+    writeTo(value, fileName.toString());
+    return Values.empty;
+  }
+}

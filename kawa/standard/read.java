a37f6ed06fd6e7693a1f37a0e29ceaac13be2a49
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
2002-01-30 06:42:24 +0000

    * read.java:  Since apply1 can now throw Throwable, don't need to 	wrap exception.  However, so call SyntaxException's new setHeader. 	Don't call ScmRead's statuc readObject, which creates a new ScmRead.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@2662 5e0a886f-7f45-49c5-bc19-40643649e37f

--- kawa/standard/read.java
+++ kawa/standard/read.java
@@ -1,37 +1,33 @@
 package kawa.standard;
 import gnu.kawa.lispexpr.ScmRead;
 import gnu.mapping.*;
+import gnu.text.*;
 
-public class read extends Procedure0or1 {
+public class read extends Procedure0or1
+{
   public final Object apply0 ()
+    throws Throwable
   {
     return apply1 (InPort.inDefault());
   }
 
   public final Object apply1 (Object arg1)
+    throws Throwable
   {
     if (! (arg1 instanceof InPort))
       throw new WrongType (this.name(), 0, "input port");
+    ScmRead lexer = new ScmRead((InPort)arg1);
     try
       {
-	ScmRead lexer = new ScmRead((InPort)arg1);
-	Object result = lexer.readObject((InPort)arg1);
-	gnu.text.SourceError errors = lexer.getErrors();
-	if (errors != null)
-	  {
-	    lexer.checkErrors(null, 0);
-	    throw new RuntimeException("syntax error in read: "
-				       + errors.toString());
-	  }
+	Object result = lexer.readObject();
+	if (lexer.seenErrors())
+	  throw new SyntaxException(lexer.getMessages());
 	return result;
       }
-    catch (gnu.text.SyntaxException e)
+    catch (SyntaxException ex)
       {
-	throw new RuntimeException("syntax error in read: " + e.toString ());
-      }
-    catch (java.io.IOException e)
-      {
-	throw new WrappedException(e);
+	ex.setHeader("syntax error in read:");
+	throw ex;
       }
   }
 }

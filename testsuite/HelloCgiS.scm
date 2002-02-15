63bc680e3406cbf78f0d25517ad4af830603d3a1
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
2002-02-15 07:03:23 +0000

    * HelloCgiK.krl, HelloCgiK.out:  New cgi-servlet/KRL test. 	* HelloCgiS.out, HelloCgiS.scm:  New cgi-servlet/Scheme test. 	* Makefile.am:  Add rules for new tests.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@2726 5e0a886f-7f45-49c5-bc19-40643649e37f

---
+++ testsuite/HelloCgiS.scm
@@ -0,0 +1,14 @@
+(require 'http)
+(unescaped-data "<?xml version='1.0'?>
+")
+(make-element 'body
+	      (make-element 'p
+			    "The request URI was: " (request-uri))
+	      (make-element 'p
+			    (let ((query (request-query-string)))
+			      (if query
+				  (values-append "The query string was: "
+						 query)
+				  "There was no query string."))))
+#\newline
+

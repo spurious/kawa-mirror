85ae55ee77d893db8cb7775460b86380655f4ccb
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
2002-03-02 21:28:41 +0000

    * HTTP.scm:  Moved from gnu/kawa/servlet. 	* Makefile.am:  Compile HTTP.scm - but only if --enable-servlet. Also Changed to new automake - see top-level ChangeLog.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@2758 5e0a886f-7f45-49c5-bc19-40643649e37f

---
+++ gnu/kawa/slib/HTTP.scm
@@ -0,0 +1,29 @@
+(define (response-header key value)
+  ((static-field <gnu.kawa.xml.MakeResponseHeader> 'makeResponseHeader)
+   key value))
+
+(define (response-content-type type)
+  (response-header '|Content-Type| type))
+
+(define (get-request) :: <javax.servlet.http.HttpServletRequest>
+  ((static-field <gnu.kawa.servlet.GetRequest> 'getRequest)))
+
+(define (request-method) :: <String>
+  (invoke (get-request) 'getMethod))
+
+(define (request-uri) :: <String>
+  (invoke (get-request) 'getRequestURI))
+
+(define (request-url) :: <String>
+  (invoke (get-request) 'getRequestURL))
+
+(define (request-path-info) :: <String>
+  (invoke (get-request) 'getPathInfo))
+
+(define (request-path-translated) :: <String>
+  (invoke (get-request) 'getPathTranslated))
+
+(define (request-query-string)
+  (let ((query (invoke (get-request) 'getQueryString)))
+    (if (eq? query #!null) #f query)))
+

a0ee8023d1fe9c8d132aa0370e8b20b4795bcdeb
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
2002-06-26 17:32:52 +0000

    * XMLParser.java:  Share buffer with LineBufferedReader. 	(fill):  New method, to synchronize and read from LineBufferedReader. 	(error, getName, getLineNumber, getColumnNjmber):  New methods.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@2916 5e0a886f-7f45-49c5-bc19-40643649e37f

--- gnu/kawa/xml/XMLParser.java
+++ gnu/kawa/xml/XMLParser.java
@@ -14,16 +14,89 @@ public class XMLParser extends XMLParserChar
   public XMLParser(LineBufferedReader reader, Consumer out, SourceMessages messages)
     throws java.io.IOException
   {
-    super(reader, new ParsedXMLToConsumer(out));
-
+    super(null, 0, 0, new ParsedXMLToConsumer(out));
+    in = reader;
     this.messages = messages;
   }
 
   public XMLParser(URL url, Consumer out, SourceMessages messages)
     throws java.io.IOException
   {
-    super(url, new ParsedXMLToConsumer(out));
-
+    super(null, 0, 0, new ParsedXMLToConsumer(out));
+    LineBufferedReader lreader
+      = new LineBufferedReader(url.openConnection().getInputStream());
+    in = lreader;
+    lreader.setName(url.toString());
     this.messages = messages;
+  }
+
+  public int fill(char[] buffer,  int start, int pos)
+  {
+    LineBufferedReader reader = (LineBufferedReader) in;
+    int saved = pos - start;
+    try
+      {
+	if (saved > 0)
+	  {
+	    int skipped = reader.skip(start - reader.pos);
+	    reader.mark(saved + 1);
+	    skipped = reader.skip(saved);
+	  }
+	else
+	  {
+	    reader.skip(pos - reader.pos);
+	  }
+	int x = reader.read();
+	if (x <= 0)
+	  return -1;
+	if (saved > 0)
+	  {
+	    reader.reset();
+	    reader.skip(saved);
+	  }
+	else
+	  reader.unread_quick();
+      }
+    catch (java.io.IOException ex)
+      {
+	throw new RuntimeException(ex.getMessage());
+      }
+    this.pos = reader.pos;
+    this.buffer = reader.buffer;
+    return reader.limit - reader.pos;
+  }
+
+  public void error(char severity, String message)
+  {
+    LineBufferedReader reader = (LineBufferedReader) in;
+    try
+      {
+	reader.skip(pos - reader.pos);
+      }
+    catch (Exception ex)
+      {
+      }
+    String filename = reader.getName();
+    if (filename != null && filename.startsWith("file:"))
+      filename = filename.substring(5);
+    int line = reader.getLineNumber();
+    int column = reader.getColumnNumber();
+    messages.error(severity, filename, line + 1, column >= 0 ? column + 1 : 0,
+		   message);
+  }
+
+  public String getName()
+  {
+    return ((LineBufferedReader) in).getName();
+  }
+
+  public int getLineNumber()
+  {
+    return ((LineBufferedReader) in).getLineNumber();
+  }
+
+  public int getColumnNumber()
+  {
+    return ((LineBufferedReader) in).getColumnNumber();
   }
 }

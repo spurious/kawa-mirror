a732259fccc37f212d43bc76896246d95fcc7cb0
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
2002-11-23 20:09:51 +0000

    * StaticFieldConstraint.java:  Moved from kawa.standard. 	* Makefile.am (java_sources):  Add new file.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@3087 5e0a886f-7f45-49c5-bc19-40643649e37f

---
+++ gnu/kawa/reflect/StaticFieldConstraint.java
@@ -0,0 +1,21 @@
+package gnu.kawa.reflect;
+import gnu.kawa.reflect.ClassMemberConstraint;
+import gnu.bytecode.ClassType;
+import gnu.mapping.*;
+
+public class StaticFieldConstraint extends ClassMemberConstraint
+{
+  public StaticFieldConstraint(String cname, String fname)
+  {
+    super(ClassType.make(cname), fname);
+  }
+
+
+  public static void define(Environment environ, String name,
+                            String cname, String fname)
+  {
+    Symbol symbol = environ.getSymbol(name);
+    setConstraint(symbol, new StaticFieldConstraint(cname, fname));
+    //    symbol.value = null;
+  }
+}

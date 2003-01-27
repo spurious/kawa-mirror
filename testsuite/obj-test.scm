8a65c4e5e986f283665489031ad372b9be5ab45e
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
2003-01-27 03:14:30 +0000

    Remove spurious ')'.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@3141 5e0a886f-7f45-49c5-bc19-40643649e37f

--- testsuite/obj-test.scm
+++ testsuite/obj-test.scm
@@ -36,7 +36,7 @@
 (define make-funny-record2 (record-constructor funny-record '(b-c a!)))
 (define lt1 (make-funny-record1 10 12))
 (test 10 'accessor21 ((record-accessor funny-record 'a!) lt1))
-((record-modifier funny-record 'b-c) lt1 (+ 2 (lt1 'b-c))))
+((record-modifier funny-record 'b-c) lt1 (+ 2 (lt1 'b-c)))
 (set! (lt1 'a!) 9)
 (test (make-funny-record2 14 9) 'funny-record lt1)
 

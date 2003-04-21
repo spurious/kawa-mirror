16a2119fed8d4f585def8c88053199fad78d2456
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
2003-04-21 07:04:27 +0000

    * ArrayClassLoader.java:  Rewrite to use a Hashtable.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@3272 5e0a886f-7f45-49c5-bc19-40643649e37f

--- gnu/bytecode/ArrayClassLoader.java
+++ gnu/bytecode/ArrayClassLoader.java
@@ -1,4 +1,5 @@
 package gnu.bytecode;
+import java.util.Hashtable;
 
 /** Load classes from a set of byte arrays.
  * @author	Per Bothner
@@ -6,17 +7,7 @@ package gnu.bytecode;
 
 public class ArrayClassLoader extends ClassLoader
 {
-  /** The raw byte arrays from which we will load the classes.
-   * Each element is suitable for defineClass. */
-  byte[][] classBytes;
-
-  /** Number of classes managed by this loader. */
-  int size;
-
-  /** Classes that we have already loaded. */
-  Class[] loadedClasses;
-
-  String[] classNames;
+  Hashtable map = new Hashtable(100);
 
   public ArrayClassLoader ()
   {
@@ -26,67 +17,24 @@ public class ArrayClassLoader extends ClassLoader
     By convention, the classes we manage are named "lambda"+<INTEGER>. */
   public ArrayClassLoader (byte[][] classBytes)
   {
-    this.classBytes = classBytes;
-    size = classBytes.length;
-    loadedClasses = new Class [size];
-    classNames = new String [size];
-    for (int i = 0;  i < size;  i++)
-      classNames[i] = "lambda" + i;
+    for (int i = classBytes.length;  --i >= 0; )
+      addClass("lambda" + i, classBytes[i]);
   }
 
   public ArrayClassLoader (String[] classNames, byte[][] classBytes)
   {
-    this.classBytes = classBytes;
-    size = classBytes.length;
-    loadedClasses = new Class [size];
-    this.classNames = classNames;
-  }
-
-  private void reserve(int count)
-  {
-    if (count <= 0)
-      return;
-    int newLength = size < count ? size + count : 2 * size;
-    if (loadedClasses == null)
-      loadedClasses = new Class[newLength];
-    else if (size + count > loadedClasses.length)
-      {
-        Class[] loadedClassesNew = new Class[newLength];
-        System.arraycopy(loadedClasses, 0, loadedClassesNew, 0, size);
-        loadedClasses = loadedClassesNew;
-      }
-    if (classBytes == null)
-      classBytes = new byte[newLength][];
-    else if (size + count > classBytes.length)
-      {
-        byte[][] classBytesNew = new byte[newLength][];
-        System.arraycopy(classBytes, 0, classBytesNew, 0, size);
-        classBytes = classBytesNew;
-      }
-    if (classNames == null)
-      classNames = new String[newLength];
-    else if (size + count > classNames.length)
-      {
-        String[] classNamesNew = new String[newLength];
-        System.arraycopy(classNames, 0, classNamesNew, 0, size);
-        classNames = classNamesNew;
-      }
+    for (int i = classBytes.length;  --i >= 0; )
+      addClass(classNames[i], classBytes[i]);
   }
 
   public void addClass(Class clas)
   {
-    reserve(1);
-    classNames[size] = clas.getName();
-    loadedClasses[size] = clas;
-    size++;
+    map.put(clas.getName(), clas);
   }
 
   public void addClass(String name, byte[] bytes)
   {
-    reserve(1);
-    classNames[size] = name == null ? ("lambda"+size) : name;
-    classBytes[size] = bytes;
-    size++;
+    map.put(name, bytes);
   }
 
   public void addClass (ClassType ctype)
@@ -101,29 +49,30 @@ public class ArrayClassLoader extends ClassLoader
   public Class loadClass (String name, boolean resolve)
        throws ClassNotFoundException
   {
+    Object r = map.get(name);
     Class clas;
-    for (int index = 0; ; index++)
+    if (r == null)
       {
-	if (index >= size)
+	clas = Class.forName(name);
+      }
+    else if (r instanceof byte[])
+      {
+	synchronized (this)
 	  {
-	    clas = Class.forName(name);
-	    break;
-	  }
-	else if (name.equals (classNames[index]))
-	  { 
-	    clas = loadedClasses[index];
-	    if (clas == null)
+	    r = map.get(name);
+	    if (r instanceof byte[])
 	      {
-		byte[] bytes = classBytes[index];
+		byte[] bytes = (byte[]) r;
 		clas = defineClass (name, bytes, 0, bytes.length);
-		loadedClasses[index] = clas;
-		classBytes[index] = null;  // To help garbage collector.
+		map.put(name, clas);
 	      }
-            break;
+	    else
+	      clas = (Class) r;
 	  }
       }
-
-    if (resolve)
+    else
+      clas = (Class) r;
+    if (resolve && clas != null)
       resolveClass (clas);
     return clas;
   }

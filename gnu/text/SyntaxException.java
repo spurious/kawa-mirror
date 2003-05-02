f71c86983c643d3e13ea0232f86d210855af2bc2
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
2003-05-02 15:36:32 +0000

    * SyntaxException.java (SyntaxException):  New constructor. 	(maxToPrint):  New field. 	(getMessage):  New method.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@3302 5e0a886f-7f45-49c5-bc19-40643649e37f

--- gnu/text/SyntaxException.java
+++ gnu/text/SyntaxException.java
@@ -12,6 +12,12 @@ public class SyntaxException extends Exception
     this.messages = messages;
   }
 
+  public SyntaxException(String header, SourceMessages messages)
+  {
+    this.header = header;
+    this.messages = messages;
+  }
+
   public final String getHeader() { return header; }
   public final void setHeader(String header) { this.header = header; }
 
@@ -27,5 +33,22 @@ public class SyntaxException extends Exception
   public void clear()
   {
     messages.clear();
+  }
+
+  public int maxToPrint = 10;
+
+  public String getMessage ()
+  {
+    StringBuffer buffer = new StringBuffer ();
+    if (header != null)
+      buffer.append(header);
+    int max = maxToPrint;
+    for (SourceError err = messages.firstError;
+	 err != null && --max >= 0;  err = err.next)
+      {
+	buffer.append('\n');
+	buffer.append(err);
+      }
+    return buffer.toString();
   }
 }

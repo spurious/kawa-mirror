2891bea213ecd22d87b654c5cf21469ea4cccf91
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
2003-05-13 15:38:56 +0000

    * OccurrenceType.java:  New class, extends Type. 	* Makefile.am (java_sources):  Add new file.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@3334 5e0a886f-7f45-49c5-bc19-40643649e37f

---
+++ gnu/kawa/reflect/OccurrenceType.java
@@ -0,0 +1,137 @@
+// Copyright (c) 2003  Per M.A. Bothner.
+// This is free software;  for terms and warranty disclaimer see ./COPYING.
+
+package gnu.kawa.reflect;
+import gnu.bytecode.*;
+import gnu.mapping.Values;
+import gnu.lists.*;
+import java.io.*;
+import gnu.expr.*;
+
+/** A type that matches some number of repetitions of a basetype. */
+
+public class OccurrenceType extends Type
+  implements Externalizable, TypeValue
+{
+  Type base;
+  int minOccurs;
+  int maxOccurs;
+
+  public OccurrenceType (Type base, int minOccurs, int maxOccurs)
+  {
+    this.base = base;
+    this.minOccurs = minOccurs;
+    this.maxOccurs = maxOccurs;
+  }
+
+  public Type getImplementationType()
+  {
+    return Type.pointer_type;
+  }
+
+  public int compare(Type other)
+  {
+    throw new Error("compare not implemented");
+  }
+
+  public Object coerceFromObject (Object obj)
+  {
+    if (! isInstance(obj))
+      throw new ClassCastException();
+    return obj;
+  }
+
+  public boolean isInstance (Object obj)
+  {
+    if (obj instanceof Values)
+      {
+	Values vals = (Values) obj;
+	int pos = vals.startPos();
+	int n = 0;
+	if (base instanceof ElementPredicate)
+	  {
+	    ElementPredicate pred = (ElementPredicate) base;
+	    for (;;)
+	      {
+		boolean matches;
+		matches = pred.isInstancePos(vals, pos);
+		pos = vals.nextPos(pos);
+		if (pos == 0)
+		  {
+		    return n >= minOccurs
+		      && (maxOccurs < 0 || n <= maxOccurs);
+		  }
+		if (! matches)
+		  return false;
+		n++;
+	      }
+	  }
+	else
+	  {
+
+	    for (;;)
+	      {
+		pos = vals.nextPos(pos);
+		if (pos == 0)
+		  {
+		    return n >= minOccurs
+		      && (maxOccurs < 0 || n <= maxOccurs);
+		  }
+		Object value = vals.getPosPrevious(pos);
+		if (! base.isInstance(value))
+		  return false;
+		n++;
+	      }
+	  }
+      }
+    else
+      {
+	if (minOccurs > 1 || maxOccurs == 0)
+	  return false;
+	return base.isInstance(obj);
+      }
+  }
+
+  public void emitTestIf(Variable incoming, Declaration decl,
+			 Compilation comp)
+  {
+    CodeAttr code = comp.getCode();
+    if (incoming != null)
+      code.emitLoad(incoming);
+    if (decl != null)
+      {
+	code.emitDup();
+	decl.compileStore(comp);
+      }
+    comp.compileConstant(this);
+    code.emitSwap();
+    code.emitInvokeVirtual(isInstanceMethod);
+    code.emitIfIntNotZero();
+  }
+
+  public void emitIsInstance(Variable incoming,
+			     Compilation comp, Target target)
+  {
+    gnu.kawa.reflect.InstanceOf.emitIsInstance(this, incoming, comp, target);
+  }
+
+  public void writeExternal(ObjectOutput out) throws IOException
+  {
+    out.writeObject(base);
+    out.writeInt(minOccurs);
+    out.writeInt(maxOccurs);
+  }
+
+  public void readExternal(ObjectInput in)
+    throws IOException, ClassNotFoundException
+  {
+    base = (Type) in.readObject();
+    minOccurs = in.readInt();
+    maxOccurs = in.readInt();
+  }
+
+  public static final ClassType typeOccurrenceType
+    = ClassType.make("gnu.kawa.reflect.OccurrenceType");
+  static final Method isInstanceMethod
+    = typeOccurrenceType.getDeclaredMethod("isInstance", 1);
+}

8b90c0bf6fc596d8e8bb1c932e27770cd1ccde56
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
2003-07-16 14:43:40 +0000

    * Arrays.java (effectiveIndex):  Minor optimization - I hope. 	(shareArray):  Be careful to not call getEffectiveIndex with invalid 	indexes.  Fixes Savannah [bug #4310] share-array edge case.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@3442 5e0a886f-7f45-49c5-bc19-40643649e37f

--- gnu/kawa/functions/Arrays.java
+++ gnu/kawa/functions/Arrays.java
@@ -1,4 +1,4 @@
-// Copyright (c) 2002  Per M.A. Bothner and Brainfood Inc.
+// Copyright (c) 2002, 2003  Per M.A. Bothner and Brainfood Inc.
 // This is free software;  for terms and warranty disclaimer see ./COPYING.
 
 package gnu.kawa.functions;
@@ -74,13 +74,13 @@ public class Arrays
     if (mapval instanceof Values)
       {
 	Values mapvals = (Values) mapval;
-	int nvals = mapvals.size();
-	SeqPosition ivals = mapvals.getIterator();
-	for (int j = 0;  j < nvals;  j++)
-	  work[j] = ((Number) ivals.next()).intValue();
+	for (int i = 0, j = 0;  (i = mapvals.nextPos(i)) != 0;  j++)
+	  {
+	    work[j] = ((Number) mapvals.getPosPrevious(i)).intValue();
+	  }
       }
     else
-      work[0] = ((Number) proc.applyN(args)).intValue();
+      work[0] = ((Number) mapval).intValue();
     return array.getEffectiveIndex(work);
   }
 
@@ -92,30 +92,42 @@ public class Arrays
     Object[] args = new Object[rank];
     int[] dimensions = new int[rank];
     int[] lowBounds = new int[rank]; // null - FIXME
+    boolean empty = false;
     for (int i = rank;  --i >= 0; )
       {
 	Object low = shape.getRowMajor(2*i);
 	args[i] = low;
-	lowBounds[i] = ((Number) low).intValue();
-      }
-    int arank = array.rank();
-    int[] work = new int[arank];
-    int offset0 = effectiveIndex (array, proc, args, work);
-    int[] offsets = new int[rank];
-    for (int i = rank;  --i >= 0; )
-      {
+	int lo = ((Number) low).intValue();
+	lowBounds[i] = lo;
 	int hi = ((Number) shape.getRowMajor(2*i+1)).intValue();
-	int lo = lowBounds[i];
 	int size = hi - lo;
 	dimensions[i] = size;
-	if (size == 0)
-	  offsets[i] = 0;
-	else
+	if (size <= 0)
+	  empty = true;
+      }
+    int arank = array.rank();
+    int[] offsets = new int[rank];
+    int offset0;
+    if (empty)
+      offset0 = 0;
+    else
+      {
+	int[] work = new int[arank];
+	offset0 = effectiveIndex (array, proc, args, work);
+	for (int i = rank;  --i >= 0; )
 	  {
-	    Object low = args[i];
-	    args[i] = IntNum.make(lo + 1);
-	    offsets[i] = effectiveIndex (array, proc, args, work) - offset0;
-	    args[i] = low;
+	    int size = dimensions[i];
+	    int lo = lowBounds[i];
+	    if (size <= 1)
+	      offsets[i] = 0;
+	    else
+	      {
+		Object low = args[i];
+		args[i] = IntNum.make(lo + 1);
+		offsets[i] = (effectiveIndex (array, proc, args, work)
+			      - offset0);
+		args[i] = low;
+	      }
 	  }
       }
     return array.transpose(lowBounds, dimensions, offset0, offsets);

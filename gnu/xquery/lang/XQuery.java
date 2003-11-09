dcc4c0adb445e09bf7f7b30884a5ff27c61607d4
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
2003-11-09 22:32:53 +0000

    * XQuery.java (PARSE_WITH_FOCUS):  New flag. 	(parse):  Check for PARSE_WITH_FOCUS. 	(applyWithFocus, applyWithFocus$X, evalToFocusProc, evalWithFocus, 	evalWithFocus$X):  New methods.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@3707 5e0a886f-7f45-49c5-bc19-40643649e37f

--- gnu/xquery/lang/XQuery.java
+++ gnu/xquery/lang/XQuery.java
@@ -14,8 +14,12 @@ import gnu.xml.*;
 import gnu.kawa.reflect.ClassMethods;
 import gnu.text.Lexer;
 import gnu.text.SourceMessages;
+import java.io.Reader;
 import java.util.Vector;
 import gnu.kawa.functions.ConstantFunction0;
+import gnu.math.IntNum;
+
+/** The XQuery language. */
 
 public class XQuery extends Interpreter
 {
@@ -58,6 +62,9 @@ public class XQuery extends Interpreter
     return parser;
   }
 
+  /** Special parser flag used by <code>evalToFocusProc</code>. */
+  public static final int PARSE_WITH_FOCUS = 0x10000;
+
   public Compilation parse(Lexer lexer, int options)
     throws java.io.IOException, gnu.text.SyntaxException
   {
@@ -74,6 +81,19 @@ public class XQuery extends Interpreter
 	if (sexp == null)
 	  return null;
 	mexp.body = sexp;
+      }
+    else if ((options & PARSE_WITH_FOCUS) != 0)
+      {
+	LambdaExp lexp = new LambdaExp(3);
+	Declaration dotDecl = lexp.addDeclaration(XQParser.DOT_VARNAME);
+	dotDecl.setFlag(Declaration.IS_SINGLE_VALUE);
+	dotDecl.noteValue (null);  // Does not have a known value.
+	lexp.addDeclaration(XQParser.POSITION_VARNAME, Type.int_type);
+	lexp.addDeclaration(XQParser.LAST_VARNAME, Type.int_type);
+	tr.push(lexp);
+	lexp.body = ((XQParser) lexer).parse(tr);
+	tr.pop(lexp);
+	mexp.body = lexp;
       }
     else
       {
@@ -133,6 +153,279 @@ public class XQuery extends Interpreter
   /** Environment of pre-defined non-standard Qexo/Kawa functions. */
   public static Environment extensionsEnvEnv
     = Environment.getInstance(KAWA_FUNCTION_NAMESPACE);
+
+  /** Call a procedure with a given focus (context).
+   * @param proc a 3-operand <code>Procedure</code> as returned by
+   *   <code>evalToFocusProc</code>
+   * @param item the context item, passed as the first argument to <code>proc</code>
+   * @param position the context position, passed as the second argument
+   * @param size the context size, passed as the second argument
+   * @param out where to send the result of <code>proc</code>
+   */
+  public void applyWithFocus (Procedure proc,
+			      Object item, int position, int size,
+			      Consumer out)
+    throws Throwable
+  {
+    CallContext ctx = CallContext.getInstance();
+    ctx.setArgs(item, IntNum.make(position),IntNum.make(size));
+    ctx.proc = proc;
+    Consumer save = ctx.consumer;
+    try
+      {
+	ctx.consumer = out;
+	ctx.runUntilDone();
+      }
+    finally
+      {
+	ctx.consumer = save;
+      }
+  }
+
+  /** Call a procedure with a given focus (context).
+   * @param proc a 3-operand <code>Procedure</code> as returned by
+   *   <code>evalToFocusProc</code>
+   * @param item the context item, passed as the first argument to <code>proc</code>
+   * @param position the context position, passed as the second argument
+   * @param size the context size, passed as the second argument
+   * @return the result of applying <code>proc</code>
+   */
+  public Object applyWithFocus (Procedure proc,
+				Object item, int position, int size)
+    throws Throwable
+  {
+    CallContext ctx = CallContext.getInstance();
+    int oldIndex = ctx.startFromContext();
+    try
+      {
+	ctx.setArgs(item, IntNum.make(position),IntNum.make(size));
+	ctx.proc = proc;
+	return ctx.getFromContext(oldIndex);
+      }
+    catch (Throwable ex)
+      { 
+	ctx.cleanupFromContext(oldIndex);
+	throw ex;
+      }
+  }
+
+  /** Call a procedure with each item in a sequence as the context item.
+   * @param proc a 3-operand <code>Procedure</code> as returned by
+   *   <code>evalToFocusProc</code>
+   * @param values a sequence.  The <code>proc</code> is called once for each
+   *   item, with the item as the first argument, a 1-based index as the
+   *   second argument, and the sequence size as the third argument.
+   * @param out where to send the result of <code>proc</code>
+   */
+  public void applyWithFocus (Procedure proc, Object values, Consumer out)
+    throws Throwable
+  {
+    CallContext ctx = CallContext.getInstance();
+    Consumer save = ctx.consumer;
+    try
+      {
+	ctx.consumer = out;
+	applyWithFocus$X(proc, values, ctx);
+      }
+    finally
+      {
+	ctx.consumer = save;
+      }
+  }
+
+  /** Call a procedure with each item in a sequence as the context item.
+   * @param proc a 3-operand <code>Procedure</code> as returned by
+   *   <code>evalToFocusProc</code>
+   * @param values a sequence.  The <code>proc</code> is called once for each
+   *   item, with the item as the first argument, a 1-based index as the
+   *   second argument, and the sequence size as the third argument.
+   * @return the result of applying <code>proc</code>
+   */
+  public Object applyWithFocus (Procedure proc, Object values)
+    throws Throwable
+  {
+    CallContext ctx = CallContext.getInstance();
+    int oldIndex = ctx.startFromContext();
+    try
+      {
+	applyWithFocus$X(proc, values, ctx);
+	return ctx.getFromContext(oldIndex);
+      }
+    catch (Throwable ex)
+      { 
+	ctx.cleanupFromContext(oldIndex);
+	throw ex;
+      }
+  }
+
+  /** Call a procedure with each item in a sequence as the context item.
+   * @param proc a 3-operand <code>Procedure</code> as returned by
+   *   <code>evalToFocusProc</code>
+   * @param values a sequence.  The <code>proc</code> is called once for each
+   *   item, with the item as the first argument, a 1-based index as the
+   *   second argument, and the sequence size as the third argument.
+   * @param ctx the <code>CallContext</code>.  The <code>$X</code> in the
+   *   method name tells Kawa that this argument is implicit when invoked
+   *   from XQuery.
+   */
+  public void applyWithFocus$X (Procedure proc, Object values, CallContext ctx)
+    throws Throwable
+  {
+    if (values instanceof Values)
+      {
+	Values v = (Values) values;
+	int count = v.size();
+	if (count == 0)
+	  return;
+	int ipos = 0;
+	IntNum size = IntNum.make(count);
+	for (int i = 1;  ;  i++)
+	  {
+	    ctx.setArgs(v.getPosNext(ipos), IntNum.make(i), size);
+	    ctx.proc = proc;
+	    ctx.runUntilDone();
+	    if (i == count)
+	      break;
+            ipos = v.nextPos(ipos);
+	  }
+      }
+    else
+      {
+	IntNum one = IntNum.one();
+	ctx.setArgs(values, one, one);
+	ctx.proc = proc;
+	ctx.runUntilDone();
+      }
+  }
+
+  /** Parse an XQuery expression that is the body of a procedure.
+   * Helper method used by <code>evalWithFocus</code> methods.
+   * @param expr an XQuery expression (query) to evaluate
+   * @return a 3-operand Procedure whose arguments become
+   * the context item, position, and size.
+   */
+  public Procedure evalToFocusProc (String expr)
+    throws Throwable
+  {
+    SourceMessages messages = new SourceMessages();
+    Procedure proc = evalToFocusProc(new CharArrayInPort(expr), messages);
+    if (messages.seenErrors())
+      throw new RuntimeException("invalid syntax in eval form:\n"
+				 + messages.toString(20));
+    return proc;
+  }
+
+  /** Parse an XQuery expression from a <code>Reader</code> that is the body of a procedure.
+   * Helper method used by <code>evalWithFocus</code> methods.
+   * @param in where we read the expression from
+   * @param messages where to write syntax errors
+   * @return a 3-operand Procedure whose arguments become
+   * the context item, position, and size.
+   */
+  public Procedure evalToFocusProc (Reader in, SourceMessages messages)
+    throws Throwable
+  {
+    InPort port = in instanceof InPort ? (InPort) in : new InPort(in);
+    Compilation comp = parse(port, messages, PARSE_WITH_FOCUS);
+    CallContext ctx = CallContext.getInstance();
+    int oldIndex = ctx.startFromContext();
+    try
+      {
+	ModuleExp.evalModule(environ, ctx, comp);
+	return (Procedure) ctx.getFromContext(oldIndex);
+      }
+    catch (Throwable ex)
+      { 
+	ctx.cleanupFromContext(oldIndex);
+	throw ex;
+      }
+  }
+
+  /** Evaluate an expression with each item in a sequence as the context item.
+   * @param in where we read the expression from
+   * @param messages where to write syntax errors
+   * @param values becomes the context sequence while
+   *   evaluating <code>expr</code>.
+   * @param out where to send the result of the expression
+   */
+  public void evalWithFocus (Reader in, SourceMessages messages,
+			     Object values, Consumer out)
+    throws Throwable
+  {
+    applyWithFocus(evalToFocusProc(in, messages), values, out);
+  }
+
+  /** Evaluate an expression with each item in a sequence as the context item.
+   * @param expr an XQuery expression (query) to evaluate
+   * @param values becomes the context sequence while
+   *   evaluating the expression
+   * @return the result of evaluating the expression
+   */
+  public Object evalWithFocus (String expr, Object values)
+    throws Throwable
+  {
+    return applyWithFocus(evalToFocusProc(expr), values);
+  }
+
+  /** Evaluate an expression with a given focus (context).
+   * @param expr an XQuery expression (query) to evaluate
+   * @param item becomes the context item while evaluating <code>expr</code>.
+   * @param position becomes the context position 
+   * @param size becomes the context size
+   * @return the result of evaluating <code>expr</code>
+   */
+  public Object evalWithFocus (String expr,
+			       Object item, int position, int size)
+    throws Throwable
+  {
+   return applyWithFocus(evalToFocusProc(expr), item, position, size);
+  }
+
+  /** Evaluate an expression with a given focus (context).
+   * @param in where we read the expression from
+   * @param messages where to write syntax errors
+   * @param item becomes the context item while evaluating the expression
+   * @param position becomes the context position 
+   * @param size becomes the context size
+   * @param out where to send the result of the expression
+   */
+  public void evalWithFocus (Reader in, SourceMessages messages,
+			     Object item, int position, int size,
+			     Consumer out)
+    throws Throwable
+  {
+    applyWithFocus(evalToFocusProc(in, messages), item, position, size, out);
+  }
+
+  /** Evaluate an expression with a given focus (context).
+   * Similar to <code>evalWithFocus(String, Object, Consumer)</code>.
+   * The "$X" in the method name tells the Kawa compiler that the CallContext
+   * argument is implicit, so it can be invoked from XQuery code thus:
+   * <code>XQuery:evalWithFocus($xquery, "expr", $sequence)</code>
+   */
+  public void evalWithFocus$X (String expr,
+			       Object values,
+			       CallContext ctx)
+    throws Throwable
+  {
+    applyWithFocus$X(evalToFocusProc(expr), values, ctx);
+  }
+
+  /** Evaluate an expression with a given focus (context).
+   * Similar to <code>evalWithFocus(String, Object, int, int, Consumer)</code>.
+   * The "$X" in the method name tells the Kawa compiler that the CallContext
+   * argument is implicit, so it can be invoked from XQuery code thus:
+   * <code>XQuery:evalWithFocus($xquery, "expr", $item, $pos, $size)</code>
+   */
+  public void evalWithFocus$X (String expr,
+			       Object item, int position, int size,
+			       CallContext ctx)
+    throws Throwable
+  {
+    Procedure proc = evalToFocusProc(expr);
+    ctx.setArgs(item, IntNum.make(position), IntNum.make(size));
+    ctx.proc = proc;
+  }
 
   public XQuery()
   {

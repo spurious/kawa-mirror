73dcc2acdbfe1620f870e53de0b0d2ecaee0dc72
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
2004-02-16 04:42:28 +0000

    * DefineNamespace.java (rewrite):  Move handling to scanForDefinitions.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@3818 5e0a886f-7f45-49c5-bc19-40643649e37f

--- gnu/kawa/xml/DefineNamespace.java
+++ gnu/kawa/xml/DefineNamespace.java
@@ -10,77 +10,35 @@ public class DefineNamespace extends Syntax
   public boolean scanForDefinitions (Pair st, java.util.Vector forms,
                                      ScopeExp defs, Translator tr)
   {
-    if (! (st.cdr instanceof Pair))
-      return super.scanForDefinitions(st, forms, defs, tr);
-    Pair p = (Pair) st.cdr;
-    Object name = p.car;
-    Pair namePair = p;
-    String sym = null;
-    if (name instanceof String)
+    Pair p1, p2;
+    if (! (st.cdr instanceof Pair)
+        || ! ((p1 = (Pair) st.cdr).car instanceof String)
+	|| ! (p1.cdr instanceof Pair)
+	|| (p2 = (Pair) p1.cdr).cdr != LList.Empty)
       {
-	sym = (Interpreter.NAMESPACE_PREFIX + name).intern();
-	Declaration decl = defs.getDefine(sym, 'w', tr);
-	// This gives us a ConstantValue attribute when compiled.
-	decl.setType(gnu.bytecode.Type.tostring_type);
-	tr.push(decl);
-	decl.setFlag(Declaration.IS_CONSTANT);
-	p = tr.makePair(p, decl, p.cdr);
-	st = tr.makePair(st, this, p);
-        if (defs instanceof ModuleExp)
-	  decl.setCanRead(true);
-	Translator.setLine(decl, namePair);
+	tr.error('e', "invalid syntax for define-alias");
+	return false;
       }
-    forms.addElement (st);
+    String name = (Interpreter.NAMESPACE_PREFIX + p1.car).intern();
+    Declaration decl = defs.getDefine(name, 'w', tr);
+    // This gives us a ConstantValue attribute when compiled.
+    decl.setType(gnu.bytecode.Type.tostring_type);
+    tr.push(decl);
+    decl.setFlag(Declaration.IS_CONSTANT);
+    if (defs instanceof ModuleExp)
+      decl.setCanRead(true);
+    Translator.setLine(decl, p1);
+    Expression value = tr.rewrite_car (p2, false);
+    SetExp sexp = new SetExp (name, value);
+    sexp.setDefining (true);
+    sexp.binding = decl;
+    decl.noteValue(value);
+    forms.addElement (sexp);
     return true;
   }
 
   public Expression rewriteForm (Pair form, Translator tr)
   {
-    Object obj = form.cdr;
-    String name = null;
-    Expression value = null;
-    Declaration decl = null;
-
-    if (obj instanceof Pair)
-      {
-	Pair p1 = (Pair) obj;
-	if (p1.car instanceof String && p1.cdr instanceof Pair)
-	  {
-	    Pair p2 = (Pair) p1.cdr;
-	    if (p2.cdr == LList.Empty)
-	      {
-		name = (String) p1.car;
-		value = tr.rewrite (p2.car);
-	      }
-	  }
-	else if (p1.car instanceof Declaration && p1.cdr instanceof Pair)
-	  {
-	    decl = (Declaration) p1.car;
-	    Pair p2 = (Pair) p1.cdr;
-	    Pair p3;
-	    if ("::" == p2.car && p2.cdr instanceof Pair
-		&& (p3 = (Pair) p2.cdr).cdr instanceof Pair)
-	      {
-		decl.setType(tr.exp2Type(p3));
-		decl.setFlag(Declaration.TYPE_SPECIFIED);
-		p2 = (Pair) p3.cdr;
-	      }
-	    if (p2.cdr == LList.Empty)
-	      {
-                name = decl.getName();
-		value = tr.rewrite (p2.car);
-	      }
-	  }
-      }
-    if (name == null)
-      return tr.syntaxError ("invalid syntax for "+getName());
-    SetExp sexp = new SetExp (name, value);
-    sexp.setDefining (true);
-    if (decl != null)
-      {
-	sexp.binding = decl;
-	decl.noteValue(value);
-      }
-    return sexp;
+    return tr.syntaxError ("define-namespace is only allowed in a <body>");
   }
 }

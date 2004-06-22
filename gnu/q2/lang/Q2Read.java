50c2f56cf4480502640a95925968aa81495c9f77
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
2004-06-22 20:00:49 +0000

    * Q2Read.java:  Remove BRL/KRL model of starting out quoted. 	(inLiteral):  Remove method.
    	* Q2Read.java (nesting):  Moved field to Lexer.
    	(skipIndentation, singleLine, readCommand):  New methods.
    	(Q2ReaderParens):  New class.
    
    
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@3931 5e0a886f-7f45-49c5-bc19-40643649e37f

--- gnu/q2/lang/Q2Read.java
+++ gnu/q2/lang/Q2Read.java
@@ -1,22 +1,17 @@
 package gnu.q2.lang;
 import gnu.kawa.lispexpr.*;
-import gnu.text.SyntaxException;
+import gnu.expr.QuoteExp;
+import gnu.text.*;
 import gnu.mapping.*;
-import gnu.text.SourceMessages;
 import gnu.lists.*;
+import java.util.Stack;
+import gnu.expr.Keyword;
+import gnu.xquery.util.MakeAttribute;
 
 /** A class to read Scheme forms (S-expressions). */
 
 public class Q2Read extends LispReader
 {
-  int nesting;
-
-  /** True if in literal text (even if nested inside an escaped expression). */
-  public boolean inLiteral ()
-  {
-    return ((InPort) port).readState == ']';
-  }
-
   void init()
   {
     initialColonIsKeyword = false;
@@ -36,6 +31,141 @@ public class Q2Read extends LispReader
   }
   
   protected ReadTable getReadTable () { return q2ReadTable; }
+
+  int skipIndentation ()
+      throws java.io.IOException, SyntaxException
+  {
+    int numTabs = 0, numSpaces = 0;
+    int ch = port.read();
+    while (ch == '\t')
+      {
+	numTabs++;
+	ch = port.read();
+      }
+    while (ch == ' ')
+      {
+	numSpaces++;
+	ch = port.read();
+      }
+    if (ch < 0)
+      return -1;
+    port.unread();
+    return (numTabs << 16) + numSpaces;
+  }
+
+  boolean singleLine()
+  {
+    return interactive && nesting == 0;
+  }
+
+  public Object readCommand ()
+      throws java.io.IOException, SyntaxException
+  {
+    return readCommand(false);
+  }
+
+  public Object readCommand (boolean forceList)
+      throws java.io.IOException, SyntaxException
+  {
+    int line = port.getLineNumber();
+    int startColumn = port.getColumnNumber();
+    int lastColumn = startColumn;
+    Object obj = LList.Empty;
+    PairWithPosition pair = null, last = null;
+    for (;;)
+      {
+	int ch = read();
+	if (ch < 0)
+	  break;
+	if (ch == ' ' || ch == '\t')
+	  continue;
+	unread();
+	if (ch == ')')
+	  break;
+	line = port.getLineNumber();
+	int column = port.getColumnNumber();
+	while (ch == '\r' || ch == '\n')
+	  {
+	    if (singleLine())
+	      return obj;
+	    ch = read();
+	    skipIndentation(); // skipHorSpace.
+	    column = port.getColumnNumber();
+	    ch = peek();
+	    if (column <= startColumn)
+	      break;
+	  }
+	if (column <= startColumn && last != null)
+	  break;
+	Object next;
+	if (column == lastColumn && last != null)
+	  next = readCommand();
+	else if (column < lastColumn && last != null)
+	  {
+	    PairWithPosition p = pair;
+	    for (;;)
+	      {
+		Object n = p.cdr;
+		if (n == LList.Empty)
+		  break;
+		PairWithPosition np = (PairWithPosition) n;
+		int pColumn = np.getColumn()-1;
+		if (pColumn >= column)
+		  {
+		    if (pColumn > column)
+		      error('e', "some tokens on previous line indented more than current line");
+		    n = np.cdr;
+		    if (n != LList.Empty)
+		      {
+			if (((PairWithPosition) n).getColumn()-1==column)
+			  {
+			    p = (PairWithPosition) n;
+			    continue;
+			  }
+			last = (PairWithPosition)
+			  makePair(np, port.getLineNumber(), column);
+			p.cdr = last;
+		      }
+		    break;
+		  }
+		p = np;
+	      }
+	    next = readCommand();
+	  }
+	else
+	  next = readObject();
+	if (next == Sequence.eofValue)
+	  break;
+	lastColumn = column;
+	String filename = port.getName();
+	PairWithPosition cur = PairWithPosition.make(next, LList.Empty,
+						     filename, line+1, column+1);
+	if (last == null)
+	  {
+	    pair = cur;
+	    obj = cur;
+	  }
+	else if (last.car instanceof Keyword)
+	  {
+	    Object name = new QuoteExp(((Keyword) last.car).getName());
+	    last.car
+	      = new PairWithPosition(last, MakeAttribute.makeAttribute,
+				     new PairWithPosition(last, name, cur));
+	    continue;
+	  }
+	else
+	  last.cdr = cur;
+	last = cur;
+      }
+    if (! forceList)
+      {
+	if (obj == last)
+	  obj = last.car;
+	else if (last == null)
+	  obj = QuoteExp.voidExp;
+      }
+    return obj;
+  }
 
   public static Object readObject(InPort port)
       throws java.io.IOException, SyntaxException
@@ -59,5 +189,30 @@ public class Q2Read extends LispReader
   static
   {
     q2ReadTable = ReadTable.getInitial();
+    q2ReadTable.set('(', new Q2ReaderParens());
   }
+}
+
+class Q2ReaderParens extends ReaderDispatchMisc
+{
+  public Object read (Lexer in, int ch, int count)
+    throws java.io.IOException, SyntaxException
+  {
+    Q2Read reader = (Q2Read) in;
+    char saveReadState = reader.pushNesting('(');
+    try
+      {
+	Object result = reader.readCommand(true);
+
+	LineBufferedReader port = reader.getPort();
+	if (port.read() != ')')
+	  reader.error("missing ')'");
+	return result;
+      }
+    finally
+      {
+	reader.popNesting(saveReadState);
+      }
+  }
+
 }

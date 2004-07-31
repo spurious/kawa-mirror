ebf32ab712850da9333c04d71e8bebaf7009ff8a
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
2004-07-31 19:05:52 +0000

    * kawa/repl.java:  Update copyright year printed by --version.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@3967 5e0a886f-7f45-49c5-bc19-40643649e37f

--- kawa/repl.java
+++ kawa/repl.java
@@ -583,7 +583,7 @@ public class repl extends Procedure0or1
 	    System.out.print("Kawa ");
 	    System.out.print(Version.getVersion());
 	    System.out.println();
-	    System.out.println("Copyright (C) 2002 Per Bothner");
+	    System.out.println("Copyright (C) 2004 Per Bothner");
 	    something_done = true;
 	  }
 	else if (arg.length () > 0 && arg.charAt(0) == '-')

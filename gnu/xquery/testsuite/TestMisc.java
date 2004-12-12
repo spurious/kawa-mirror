3545bd301544973ad5631063a5953bc98c38da35
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
2004-12-12 02:07:08 +0000

    * TestMisc.java: Update 'instanceof' instance of'. 	* mod1.xql: Update declaration syntax. 	* mod2.xql: Likewise. 	* usemod.xql: Likewise.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@4187 5e0a886f-7f45-49c5-bc19-40643649e37f

--- gnu/xquery/testsuite/TestMisc.java
+++ gnu/xquery/testsuite/TestMisc.java
@@ -317,32 +317,32 @@ public class TestMisc
 	     + "for $book in $books/book return <p>{string($book/@id)}</p>",
 	     "<p>book1</p><p>book2</p>");
 
-    evalTest("for $n in children(<a>xx<b/>yy</a>) return $n instanceof node()",
+    evalTest("for $n in children(<a>xx<b/>yy</a>) return $n instance of node()",
 	     "true true true");
-    evalTest("for $n in children(<a>xx<b/>yy</a>) return $n instanceof text ( )",
+    evalTest("for $n in children(<a>xx<b/>yy</a>) return $n instance of text ( )",
 	     "true false true");
-    evalTest("for $n in children(<a>xx<b/>yy</a>) return $n instanceof element(a,*)",
+    evalTest("for $n in children(<a>xx<b/>yy</a>) return $n instance of element(a,*)",
 	     "false false false");
-    evalTest("for $n in <a>xx<b/>yy</a>/node() return $n instanceof element(b,*)",
+    evalTest("for $n in <a>xx<b/>yy</a>/node() return $n instance of element(b,*)",
 	     "false true false");
-    // FIXME: evalTest("<a>xx<b/>yy</a>/node() instanceof node()", "false");
-    evalTest("<a>xx<b/>yy</a>/node() instanceof node()?", "false");
-    evalTest("<a>xx<b/>yy</a>/node() instanceof node()+", "true");
-    evalTest("<a>xx<b/>yy</a>/node() instanceof node()*", "true");
-    evalTest("<a>xx<b/>yy</a>/node() instanceof item()+", "true");
-    evalTest("(3,4,5) instanceof item()+", "true");
-    evalTest("('a','b') instanceof string+", "true");
-    evalTest("(2,3) instanceof string?", "false");
-    evalTest("(2,3) instanceof string+", "false");
-    evalTest("() instanceof string?", "true");
-    evalTest("() instanceof string+", "false");
-    evalTest("() instanceof string*", "true");
-    evalTest("('2') instanceof string?", "true");
-    evalTest("('2') instanceof string+", "true");
-    evalTest("('2') instanceof string*", "true");
-    evalTest("('2','3') instanceof string?", "false");
-    evalTest("('2','3') instanceof string+", "true");
-    evalTest("('2','3') instanceof string*", "true");
+    // FIXME: evalTest("<a>xx<b/>yy</a>/node() instance of node()", "false");
+    evalTest("<a>xx<b/>yy</a>/node() instance of node()?", "false");
+    evalTest("<a>xx<b/>yy</a>/node() instance of node()+", "true");
+    evalTest("<a>xx<b/>yy</a>/node() instance of node()*", "true");
+    evalTest("<a>xx<b/>yy</a>/node() instance of item()+", "true");
+    evalTest("(3,4,5) instance of item()+", "true");
+    evalTest("('a','b') instance of string+", "true");
+    evalTest("(2,3) instance of string?", "false");
+    evalTest("(2,3) instance of string+", "false");
+    evalTest("() instance of string?", "true");
+    evalTest("() instance of string+", "false");
+    evalTest("() instance of string*", "true");
+    evalTest("('2') instance of string?", "true");
+    evalTest("('2') instance of string+", "true");
+    evalTest("('2') instance of string*", "true");
+    evalTest("('2','3') instance of string?", "false");
+    evalTest("('2','3') instance of string+", "true");
+    evalTest("('2','3') instance of string*", "true");
 
     evalTest("declare namespace Int='class:java.lang.Integer';\n"
 	     + "Int:toHexString(266)", "10a");

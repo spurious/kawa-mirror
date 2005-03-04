37e4787f97951a495189b39ad326c11c8574c383
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
2005-03-04 05:10:56 +0000

    * Future.java (initEnvironment): Removed field, for parent Environment. 	(environment): New field, for child environment. 	(run): Move allocation of child Environment from here ... 	(<init>): ... to here.  This avoids a race condition.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@4441 5e0a886f-7f45-49c5-bc19-40643649e37f

--- gnu/mapping/Future.java
+++ gnu/mapping/Future.java
@@ -4,8 +4,8 @@ public class Future extends Thread
 {
   Object result;
   CallContext context;
+  Environment environment;
 
-  private Environment initEnvironment;
   // These are only used to when we need to override the parents' in/out/err
   // in the child.  This is not needed for normal Future objects, but (say)
   // when starting a repl in a new window.  In that case we could do te
@@ -17,17 +17,21 @@ public class Future extends Thread
 
   Procedure action;
 
-  public Future (Procedure action, Environment environment)
+  public Future (Procedure action, Environment penvironment)
   {
     this.action = action;
-    this.initEnvironment = environment;
-    ((SimpleEnvironment) environment).makeShared();
+    if (penvironment instanceof SimpleEnvironment)
+      ((SimpleEnvironment) penvironment).makeShared();
+    SimpleEnvironment env = Environment.make(getName(), penvironment); 
+    env.makeShared();
+    env.flags &= ~Environment.DIRECT_INHERITED_ON_SET;
+    this.environment = env;
   }
 
-  public Future (Procedure action, Environment environment,
+  public Future (Procedure action, Environment penvironment,
 		 InPort in, OutPort out, OutPort err)
   {
-    this(action, environment);
+    this(action, penvironment);
     this.in = in;
     this.out = out;
     this.err = err;
@@ -46,10 +50,7 @@ public class Future extends Thread
     try
       {
 	context = CallContext.getInstance();
-	SimpleEnvironment env = Environment.make(getName(), initEnvironment); 
-	context.curEnvironment = env;
-	env.makeShared();
-	env.flags &= ~Environment.DIRECT_INHERITED_ON_SET;
+	context.curEnvironment = environment;
 	if (in != null)
 	  InPort.setInDefault(in);
 	if (out != null)

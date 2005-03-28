a26635ba020832d093d0f369ed986e71dc263ee2
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
2005-03-28 02:16:56 +0000

    * ClassExp.java (compile): Don't declareThis is STATIC_SPECIFIED.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@4523 5e0a886f-7f45-49c5-bc19-40643649e37f

--- gnu/expr/ClassExp.java
+++ gnu/expr/ClassExp.java
@@ -391,7 +391,8 @@ public class ClassExp extends LambdaExp
 	    LambdaExp save_lambda = comp.curLambda;
 	    comp.method = child.getMainMethod();
 	    //comp.curClass = comp.method.getDeclaringClass();
-	    child.declareThis(comp.curClass);
+            if (! child.nameDecl.getFlag(Declaration.STATIC_SPECIFIED))
+              child.declareThis(comp.curClass);
 	    comp.curClass = instanceType;
 	    comp.curLambda = child;
 	    comp.method.initCode();

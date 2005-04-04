580deb7164d95300a5d7272f98978950bc236845
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
2005-04-04 01:50:49 +0000

    * package-info.java: New file, used by javadoc.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@4555 5e0a886f-7f45-49c5-bc19-40643649e37f

---
+++ kawa/lib/package-info.java
@@ -0,0 +1,5 @@
+/** The Kawa Scheme run-time libraries, written in Scheme.
+ * The functions and macros are written in Scheme and compiled
+ * using Kawa to Java class files.
+ */
+package kawa.lib;

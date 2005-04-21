a7ca7f3f89b2927b13280661e6206e9dc42bc9f1
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
2005-04-21 06:17:13 +0000

    * BRL.java: Initialize krl_instance and brlEnvironment in the 	static initializer.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@4589 5e0a886f-7f45-49c5-bc19-40643649e37f

--- gnu/kawa/brl/BRL.java
+++ gnu/kawa/brl/BRL.java
@@ -8,18 +8,39 @@ import gnu.kawa.lispexpr.ReadTable;
 
 public class BRL extends Scheme
 {
-  static BRL krl_instance;
+  static final BRL krl_instance;
   static BRL brl_instance;
   static final Object emptyForm = new FString();
 
   protected static final SimpleEnvironment brlEnvironment
     = Environment.make("brl-environment", Scheme.kawaEnvironment);
 
+  static BRLReaderString brlReader =  new BRLReaderString();
+
+  static
+  {
+    krl_instance = new BRL();
+    krl_instance.environ = brlEnvironment;
+    CallContext ctx = CallContext.getInstance();
+    Environment saveEnv = ctx.getEnvironmentRaw();
+    try
+      {
+        ctx.setEnvironmentRaw(brlEnvironment);
+        krl_instance.initBRL();
+      }
+    finally
+      {
+        ctx.setEnvironmentRaw(saveEnv);
+      }
+  }
+
   public BRL ()
   {
+  }
+
+  void initBRL ()
+  {
     ModuleBody.setMainPrintValues(true);
-    environ = brlEnvironment;
-    Environment.setCurrent(environ);
     try
       {
 	loadClass("gnu.brl.stringfun");
@@ -39,11 +60,6 @@ public class BRL extends Scheme
 
   public static BRL getKrlInstance()
   {
-    if (krl_instance == null)
-      {
-        krl_instance = new BRL();
-        krl_instance.setBrlCompatible(false);
-      }
     return krl_instance;
   }
 
@@ -52,6 +68,7 @@ public class BRL extends Scheme
     if (brl_instance == null)
       {
         brl_instance = new BRL ();
+        brl_instance.environ = brlEnvironment;
         brl_instance.setBrlCompatible(true);
       }
     return brl_instance;
@@ -80,7 +97,7 @@ public class BRL extends Scheme
   /** The compiler insert calls to this method for applications and applets. */
   public static void registerEnvironment()
   {
-    Language.setDefaults(new BRL());
+    Language.setDefaults(getKrlInstance());
   }
 
   public Expression makeBody(Expression[] exps)
@@ -89,8 +106,6 @@ public class BRL extends Scheme
       return super.makeBody(exps);
     return new ApplyExp(gnu.kawa.functions.AppendValues.appendValues, exps);
   }
-
-  static BRLReaderString brlReader =  new BRLReaderString();
 
   public ReadTable createReadTable ()
   {

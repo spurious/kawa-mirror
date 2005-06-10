66e7a0549827c6b1fd1eac563d80a76931f7ace0
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
2005-06-10 01:24:36 +0000

    * StringValue.java (stringValue): null or #!void append nothing. 	(lowerCase, upperCase, substring, stringLength, substringBefore, 	substringAfter, translate, stringPad, contains, startsWith, endWith, 	stringJoin): Don't return empty on empty input string; instead return 	the empty string, usually taken care of by stringValue. 	(substring, stringLength, substringBefore, substringAfter, translate, 	startsWith, stringJoin): Convert to String using stringvalue, not 	toString. 	(contains, startsWith, endsWith, substringAfter): Remove redundant 	comparisons. 	(substringBefore): Fix off-by-one error. 	(translate): Re-do StringBuffer construction to append each character, 	rather than modify/delete characters. 	(stringPad): Throw exception on error instead of printing. 	Also, improve StringBuffer allocation.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@4671 5e0a886f-7f45-49c5-bc19-40643649e37f

--- gnu/xquery/util/StringValue.java
+++ gnu/xquery/util/StringValue.java
@@ -35,7 +35,8 @@ public class StringValue extends Procedure1
 	tlist.stringValue(tlist.posToDataIndex(pos.ipos), sbuf);
 	return;
       }
-    sbuf.append(node);
+    if (node != null && node != Values.empty)
+      sbuf.append(node);
   }
 
   public Object apply1 (Object node)
@@ -64,99 +65,83 @@ public class StringValue extends Procedure1
 
   public static Object lowerCase (Object node)
   {
-    if (node == Values.empty || node == null)
-      return Values.empty;
     return stringValue(node).toLowerCase();
   }
 
   public static Object upperCase (Object node)
   {
-    if (node == Values.empty)
-      return Values.empty;
     return stringValue(node).toUpperCase();
   }
 
   public static Object substring (Object str, Object start)
   {
-    if (str == Values.empty || start == Values.empty)
-      return Values.empty;
-
     int i = ((Number) NumberValue.numberValue(start)).intValue() - 1;
 
-    return str.toString().substring(i);
+    return stringValue(str).substring(i);
   }
 
   public static Object substring (Object str, Object start, Object length)
   {
-    if (str == Values.empty || start == Values.empty
-	|| length == Values.empty)
-      return Values.empty;
-
     int i = ((Number) NumberValue.numberValue(start)).intValue() - 1;
     int len = ((Number) NumberValue.numberValue(length)).intValue();
 
-    return str.toString().substring(i, i + len);
+    return stringValue(str).substring(i, i + len);
   }
 
   public static Object stringLength (Object str)
   {
-    if (str == Values.empty)
-      return Values.empty;
-
-    return IntNum.make(str.toString().length());
+    return IntNum.make(stringValue(str).length());
   }
 
   public static Object substringBefore (Object str, Object find)
   {
-    if (str == Values.empty || find == Values.empty)
-      return Values.empty;
+    String s = stringValue(str);
+    String f = stringValue(find);
+    int flen = f.length();
 
-    if (find.toString().length()==0)
-      return str.toString();
-
-    int start = str.toString().indexOf(find.toString());
-    return (start>0)?str.toString().substring(0,start):"";
+    if (flen==0)
+      return "";
+    int start = s.indexOf(f);
+    return start >= 0 ? s.substring(0,start) : "";
   }
 
   public static Object substringAfter (Object str, Object find)
   {
-    if (str == Values.empty || find == Values.empty)
-      return Values.empty;
+    String s = stringValue(str);
+    String f = stringValue(find);
+    int flen = f.length();
 
-    if (find.toString().length()==0)
-      return str.toString();
+    if (flen==0)
+      return s;
 
-    int start = str.toString().indexOf(find.toString());
-    int end = find.toString().length()+start;
-    return (start!=-1 && end<str.toString().length())?str.toString().substring(end):"";
+    int start = s.indexOf(f);
+    return start >= 0 ? s.substring(start+flen) : "";
   }
 
   public static Object translate (Object str, Object map, Object trans)
   {
-    if (str == Values.empty || map == Values.empty || trans == Values.empty)
-      return Values.empty;
-
-    String m = map.toString();
+    String m = stringValue(map);
     int mlen = m.length();
+    String sv = stringValue(str);
 
-    if (mlen==0) return str.toString();
+    if (mlen==0) return sv;
 
-    String t = trans.toString();
-    StringBuffer s = new StringBuffer(str.toString());
-    int slen = s.length();
+    int slen = sv.length();
+    StringBuffer s = new StringBuffer(slen);
+    String t = stringValue(trans);
     int tlen = t.length();
 
     for (int i=0;i < slen;i++)
       {
-	for (int j=0;j<mlen;j++)
-	  {
-	    if (s.charAt(i)==m.charAt(j))
-	      {
-		if (j<tlen) { s.setCharAt(i,t.charAt(j)); }
-		else { s.deleteCharAt(i--); slen--; }
-		continue;
-	      }
+        char c = sv.charAt(i);
+        int j = m.indexOf(c);
+        if (j >= 0)
+          {
+            if (j>=tlen)
+              continue;
+            c = t.charAt(j);
 	  }
+        s.append(c);
       }
 
     return s.toString();
@@ -164,73 +149,49 @@ public class StringValue extends Procedure1
 
   public static Object stringPad (Object str, Object padcount)
   {
-    if (str == Values.empty)
-      return Values.empty;
-
-    int count =  ((Number) NumberValue.numberValue(padcount)).intValue();
-    if (count==0) return "";
-    if (count<0)
+    int count = ((Number) NumberValue.numberValue(padcount)).intValue();
+    if (count <= 0)
       {
-	OutPort err = OutPort.errDefault();
-	err.println("Invalid string-pad count");
-	return "";
+        if (count == 0)
+          return "";
+	throw new IndexOutOfBoundsException("Invalid string-pad count");
       }
 
-    StringBuffer s = new StringBuffer(str.toString());
-    for (int i=1; i<count; i++) s.append(str.toString());
+    String sv = stringValue(str);
+    int slen = sv.length();
+    StringBuffer s = new StringBuffer(count*slen);
+    for (int i=0; i<count; i++) s.append(sv);
 
     return s.toString();
   }
 
   public static Object contains (Object str, Object contain)
   {
-    if (str == Values.empty || contain == Values.empty)
-      return Values.empty;
-
     String s = stringValue(str);
     String c = stringValue(contain);
-
-    if (c.length()==0) return Boolean.TRUE;
-    if (s.length()==0) return Boolean.FALSE;
 
     return s.indexOf(c) <0 ? Boolean.FALSE : Boolean.TRUE;
   }
 
   public static Object startsWith (Object str, Object with)
   {
-    if (str == Values.empty || with == Values.empty)
-      return Values.empty;
-
     String s = stringValue(str);
     String w = stringValue(with);
-
-    if (w.length()==0) return Boolean.TRUE;
-    if (s.length()==0) return Boolean.FALSE;
 
     return s.startsWith(w) ? Boolean.TRUE : Boolean.FALSE;
   }
 
   public static Object endsWith (Object str, Object with)
   {
-    if (str == Values.empty || with == Values.empty)
-      return Values.empty;
-
     String s = stringValue(str);
     String w = stringValue(with);
-
-    if (w.length()==0) return Boolean.TRUE;
-    if (s.length()==0) return Boolean.FALSE;
-
     return s.endsWith(w) ? Boolean.TRUE : Boolean.FALSE;
   }
 
   public static Object stringJoin (Object strseq, Object join)
   {
-    if (strseq == Values.empty)
-      return Values.empty;
-
     StringBuffer s = new StringBuffer();
-    String glue = join.toString();
+    String glue = stringValue(join);
     int glen = glue.length();
     int index=0;
     boolean started = false;
@@ -240,8 +201,9 @@ public class StringValue extends Procedure1
 	Object obj = Values.nextValue( ((Values)strseq) ,index-1);
 	if (obj == Values.empty) continue;
 
-	if (started && glen > 0) s.append(glue);
-	if (obj.toString().length() > 0) s.append(obj.toString());
+	if (started && glen > 0)
+          s.append(glue);
+        s.append(stringValue(obj));
 	started=true;
       }
 

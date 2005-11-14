8a39ca4f407beca9f3ae3670745d6a1078d850a4
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
2005-11-14 23:56:02 +0000

    * kawapath.c (get_classpath): Move declaration so we can compile 	with old (pre-C99) compilers
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@4910 5e0a886f-7f45-49c5-bc19-40643649e37f

--- bin/kawapath.c
+++ bin/kawapath.c
@@ -15,8 +15,8 @@ get_classpath(const char *progname)
   const char *path = getenv("KAWALIB");
   if (path == NULL)
     {
-      path = kawalib;
       const char *rp = progname == NULL ? NULL : strrchr(progname, '/');
+      path = kawalib;
       if (rp != NULL)
 	{
 	  /* If kawa is executed "in place" as $builddir/bin/kawa

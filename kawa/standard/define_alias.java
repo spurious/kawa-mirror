3fae2c7bcda0937c4141de72620f2c4f4bcde210
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
2005-11-30 04:46:35 +0000

    * kawa/standard/define_alias.java (scanForDefinitions): Handle 	SyntaxForm unwrapping.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@4950 5e0a886f-7f45-49c5-bc19-40643649e37f

--- kawa/standard/define_alias.java
+++ kawa/standard/define_alias.java
@@ -13,35 +13,55 @@ public class define_alias extends Syntax implements Printable
   public boolean scanForDefinitions (Pair st, java.util.Vector forms,
                                      ScopeExp defs, Translator tr)
   {
-    Pair p;
-    Object name;
-    if (! (st.cdr instanceof Pair)
-        || (! ((name = (p = (Pair) st.cdr).car) instanceof String)
-	    && ! (name instanceof Symbol))
-	|| ! (p.cdr instanceof Pair)
-	|| (p = (Pair) p.cdr).cdr != LList.Empty)
+    Object formCdr = st.cdr;
+    SyntaxForm formSyntax = null;
+    while (formCdr instanceof SyntaxForm)
       {
-	tr.error('e', "invalid syntax for define-alias");
-	return false;
+	formSyntax = (SyntaxForm) formCdr;
+	formCdr = formSyntax.form;
       }
-    Declaration decl = defs.addDeclaration(name);
-    decl.setIndirectBinding(true);
-    decl.setAlias(true);
-    Expression arg = tr.rewrite_car(p, false);
-    if (arg instanceof ReferenceExp)
-      ((ReferenceExp) arg).setDontDereference(true);
-    else
-      arg = location.rewrite(arg, tr);
-    tr.mustCompileHere(); // For simplicity.
-    tr.push(decl);
-    SetExp sexp = new SetExp(decl, arg);
-    tr.setLineOf(sexp);
-    decl.noteValue(arg);
-    sexp.setDefining (true);
-    if (! (arg instanceof ReferenceExp))
-      decl.setType(ClassType.make("gnu.mapping.Location"));
-    forms.addElement(sexp);
-    return true;
+    if (formCdr instanceof Pair)
+      {
+        Pair p = (Pair) formCdr;
+        SyntaxForm nameSyntax = formSyntax;
+        Object name = p.car;
+        while (name instanceof SyntaxForm)
+          {
+            nameSyntax = (SyntaxForm) name;
+            name = nameSyntax.form;
+          }
+        formCdr = p.cdr;
+        while (formCdr instanceof SyntaxForm)
+          {
+            formSyntax = (SyntaxForm) formCdr;
+            formCdr = formSyntax.form;
+          }
+        if ((name instanceof String || name instanceof Symbol)
+            && formCdr instanceof Pair
+            && (p = (Pair) formCdr).cdr == LList.Empty)
+          {
+            Declaration decl = tr.define(name, nameSyntax, defs);
+            decl.setIndirectBinding(true);
+            decl.setAlias(true);
+            Expression arg = tr.rewrite_car(p, formSyntax);
+            if (arg instanceof ReferenceExp)
+              ((ReferenceExp) arg).setDontDereference(true);
+            else
+              arg = location.rewrite(arg, tr);
+            tr.mustCompileHere(); // For simplicity.
+            tr.push(decl);
+            SetExp sexp = new SetExp(decl, arg);
+            tr.setLineOf(sexp);
+            decl.noteValue(arg);
+            sexp.setDefining (true);
+            if (! (arg instanceof ReferenceExp))
+              decl.setType(ClassType.make("gnu.mapping.Location"));
+            forms.addElement(sexp);
+            return true;
+          }
+      }
+    tr.error('e', "invalid syntax for define-alias");
+    return false;
   }
 
   public Expression rewrite (Object obj, Translator tr)

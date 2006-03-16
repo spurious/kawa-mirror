cc968928bd36443b1616361c5aacdb64d23eb7d7
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
2006-03-16 05:51:12 +0000

    * FilterConsumer.java (beforeContent): New sub-class hook. 	Call it before the valious witrXxx methods. 	(inAttribute, attributeName, attributeType): New convience 	fields for sub-class use.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@5197 5e0a886f-7f45-49c5-bc19-40643649e37f

--- gnu/lists/FilterConsumer.java
+++ gnu/lists/FilterConsumer.java
@@ -10,44 +10,60 @@ public class FilterConsumer
 {
   protected Consumer base;
   protected boolean skipping;
+  /** We seen a beginAttribute but not the closing endAttribute. */
+  protected boolean inAttribute;
+  /** The 'attribute name' from the most recent beginAttribute. */
+  protected String attributeName;
+  /** The 'attribute type' from the most recent beginAttribute. */
+  protected Object attributeType;
 
   public FilterConsumer (Consumer base)
   {
     this.base = base;
   }
 
+  protected void beforeContent ()
+  {
+  }
+
   public void writeChar(int v)
   {
+    beforeContent();
     if (! skipping)
       base.writeChar(v);
   }
 
   public void writeBoolean(boolean v)
   {
+    beforeContent();
     if (! skipping)
       base.writeBoolean(v);
   }
 
   public void writeFloat(float v)
   {
+    beforeContent();
     if (! skipping)
       base.writeFloat(v);
   }
 
   public void writeDouble(double v)
   {
+    beforeContent();
     if (! skipping)
       base.writeDouble(v);
   }
 
   public void writeInt(int v)
   {
+    beforeContent();
     if (! skipping)
       base.writeInt(v);
   }
 
   public void writeLong(long v)
   {
+    beforeContent();
     if (! skipping)
       base.writeLong(v);
   }
@@ -78,6 +94,9 @@ public class FilterConsumer
 
   public void beginAttribute(String attrName, Object attrType)
   {
+    attributeName = attrName;
+    attributeType = attrType;
+    inAttribute = true;
     if (! skipping)
       base.beginAttribute(attrName, attrType);
   }
@@ -86,10 +105,12 @@ public class FilterConsumer
   {
     if (! skipping)
       base.endAttribute();
+    inAttribute = false;
   }
 
   public void writeObject(Object v)
   {
+    beforeContent();
     if (! skipping)
       base.writeObject(v);
   }
@@ -101,12 +122,14 @@ public class FilterConsumer
 
   public void writeChars(String str)
   {
+    beforeContent();
     if (! skipping)
       base.writeChars(str);
   }
 
   public void write(char[] buf, int off, int len)
   {
+    beforeContent();
     if (! skipping)
       base.write(buf, off, len);
   }
@@ -114,6 +137,7 @@ public class FilterConsumer
   /* #ifdef JAVA5 */
   // public Consumer append (char c)
   // {
+  //   beforeContent();
   //   if (! skipping)
   //     base.append(c);
   //   return this;
@@ -121,6 +145,7 @@ public class FilterConsumer
 
   // public Consumer append (CharSequence csq)
   // {
+  //   beforeContent();
   //   if (! skipping)
   //     base.append(csq);
   //   return this;
@@ -128,6 +153,7 @@ public class FilterConsumer
 
   // public Consumer append (CharSequence csq, int start, int end)
   // {
+  //   beforeContent();
   //   if (! skipping)
   //     base.append(csq, start, end);
   //   return this;

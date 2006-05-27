83592ad6c772e9e15ad55a3128c78e9b7ec08fed
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
2006-05-27 00:34:32 +0000

    Expand 4-character-wide tabs and remove Emacs comments.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@5321 5e0a886f-7f45-49c5-bc19-40643649e37f

--- gnu/lists/Consumer.java
+++ gnu/lists/Consumer.java
@@ -18,47 +18,39 @@ public interface Consumer
   // extends Appendable
   /* #endif */
 {
-	public void writeChar(int v);
-	public void writeBoolean(boolean v);
+  public void writeChar(int v);
+  public void writeBoolean(boolean v);
 
-	public void writeFloat(float v);
-	public void writeDouble(double v);
-	public void writeInt(int v);
-	public void writeLong(long v);
+  public void writeFloat(float v);
+  public void writeDouble(double v);
+  public void writeInt(int v);
+  public void writeLong(long v);
 
-	public void beginDocument();
-	public void endDocument();
+  public void beginDocument();
+  public void endDocument();
 
-	public void beginGroup(String typeName, Object type);
-	public void endGroup(String typeName);
+  public void beginGroup(String typeName, Object type);
+  public void endGroup(String typeName);
 
-	/** Write a attribute for the current group.
-	 * This is only allowed immediately after a beginGroup. */
-	public void beginAttribute(String attrName, Object attrType);
+  /** Write a attribute for the current group.
+   * This is only allowed immediately after a beginGroup. */
+  public void beginAttribute(String attrName, Object attrType);
 
-	public void endAttribute();
+  public void endAttribute();
 
-	public void writeObject(Object v);
+  public void writeObject(Object v);
 
-	/** True if consumer is ignoring rest of group.
-	 * The producer can use this information to skip ahead. */
-	public boolean ignoring();
+  /** True if consumer is ignoring rest of group.
+   * The producer can use this information to skip ahead. */
+  public boolean ignoring();
 
-	public void writeChars(String str);
-	// public void writeChars(AbstractString str);
-	public void write(char[] buf, int off, int len);
+  public void writeChars(String str);
+  // public void writeChars(AbstractString str);
+  public void write(char[] buf, int off, int len);
 
-	/* #ifdef JAVA5 */
-	// public Consumer append (char c);
-	// public Consumer append (CharSequence csq);
-	// public Consumer append (CharSequence csq, int start, int end);
-	/* #endif */
+  /* #ifdef JAVA5 */
+  // public Consumer append (char c);
+  // public Consumer append (CharSequence csq);
+  // public Consumer append (CharSequence csq, int start, int end);
+  /* #endif */
 }
-
-// This is for people using the Emacs editor:
-// Local Variables:
-// c-file-style: "java"
-// c-file-offsets: ((substatement-open . 0))
-// tab-width: 4
-// indent-tabs-mode: t
-// End:

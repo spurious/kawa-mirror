65718f83d98b2081721cfd1b5956b50baaba5c32
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
2006-09-18 21:22:27 +0000

    * Debug.java: Parameterize output file and options using static. 	(tracePrefix, tracePort, traaceFilename, traceShouldFlush, 	traceShouldAppend): New static fields. 	(trace): Make synchronized.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@5443 5e0a886f-7f45-49c5-bc19-40643649e37f

--- gnu/xquery/util/Debug.java
+++ gnu/xquery/util/Debug.java
@@ -3,13 +3,35 @@ import gnu.mapping.*;
 
 public class Debug
 {
-  public static Object trace (Object value, Object message)
+  public static String tracePrefix = "XQuery-trace: ";
+  public static OutPort tracePort = null;
+  public static String traceFilename = "XQuery-trace.log";
+  public static boolean traceShouldFlush = true;
+  public static boolean traceShouldAppend = false;
+
+  public static synchronized Object trace (Object value, Object message)
   {
-    OutPort err = OutPort.errDefault();
-    err.print("XQuery-trace: ");
-    err.print(message);
-    err.print(' ');
-    err.println(value);
+    OutPort out = tracePort;
+    if (out == null)
+      {
+        try
+          {
+            out = new OutPort(new java.io.FileOutputStream(traceFilename,
+                                                           traceShouldAppend));
+          }
+        catch (Throwable ex)
+          {
+            new WrappedException("Could not open '"+traceFilename
+                                 +"' for fn:trace output", ex);
+          }
+        tracePort = out;
+      }
+    out.print(tracePrefix);
+    out.print(message);
+    out.print(' ');
+    out.println(value);
+    if (traceShouldFlush)
+      out.flush();
     return value;
   }
 }

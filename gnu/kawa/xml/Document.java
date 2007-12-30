79b386738936c091cd26856fb24e15477fd7a9d1
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
2007-12-30 23:09:46 +0000

    * Document.java: Split the uri-to-document cache into a strong 	thread-local map, and a soft global map, so the former can be cleared 	when done with a module.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@6022 5e0a886f-7f45-49c5-bc19-40643649e37f

--- gnu/kawa/xml/Document.java
+++ gnu/kawa/xml/Document.java
@@ -6,6 +6,8 @@ import gnu.mapping.*;
 import gnu.lists.*;
 import gnu.xml.*;
 import java.net.URL;
+import java.lang.ref.*;
+import java.util.*;
 import gnu.text.*;
 
 /** Implement the XQuery function 'document'. */
@@ -34,23 +36,87 @@ public class Document
     return new KDocument(tree, TreeList.BEGIN_ENTITY_SIZE << 1);
   }
 
-  /** Internal namespace used to mange cached documents. */
-  static String docNamespace = "http://gnu.org/kawa/cached-documents";
+  private static ThreadLocation docMapLocation
+    = new ThreadLocation("document-map");
+
+  /* #ifdef JAVA2 */
+  private static HashMap cache
+    = new HashMap();
+
+  private static class DocReference extends SoftReference
+  { 
+    static ReferenceQueue queue = new ReferenceQueue();
+    Path key;
+    public DocReference (Path key, KDocument doc)
+      {
+        super(doc, queue);
+        this.key = key;
+      }
+    }
+  /* #endif */
+
+  /** Clear the thread-local uri-to-document map. */
+  public static void clearLocalCache ()
+  {
+    Location loc = docMapLocation.getLocation();
+    loc.set(null);
+  }
+
+  /** Clear the global uri-to-document "soft" cache. */
+  public static void clearSoftCache ()
+  {
+    /* #ifdef JAVA2 */
+    cache = new HashMap();
+    /* #endif */
+  }
 
   public static KDocument parseCached (Object uri)
     throws Throwable
   {
-    Symbol sym = Symbol.make(docNamespace, uri.toString());
-    Environment env = Environment.getCurrent();
-    synchronized (sym)
+    return parseCached(Path.valueOf(uri));
+  }
+
+  public static synchronized KDocument parseCached (Path uri)
+    throws Throwable
+  {
+    /* #ifdef JAVA2 */
+    for (;;)
       {
-        NamedLocation loc = env.getLocation(sym, null, true);
-        Object val = loc.get(null);
-        if (val != null)
-          return (KDocument) val;
-        KDocument doc = parse(uri);
-        loc.set(doc);
-        return doc;
+        DocReference oldref = (DocReference) DocReference.queue.poll();
+        if (oldref == null)
+          break;
+        cache.remove(oldref.key);
       }
+    /* #endif */
+    Location loc = docMapLocation.getLocation();
+    Hashtable map = (Hashtable) loc.get(null);
+    if (map == null)
+      {
+        map = new Hashtable();
+        loc.set(map);
+      }
+    KDocument doc = (KDocument) map.get(uri);
+    if (doc != null)
+      return doc;
+    /* #ifdef JAVA2 */
+    DocReference ref = (DocReference) cache.get(uri);
+    if (ref != null)
+      {
+        doc = (KDocument) ref.get();
+        if (doc == null)
+          cache.remove(uri);
+        else
+          {
+            map.put(uri, doc);
+            return doc;
+          }
+      }
+    /* #endif */
+    doc = parse(uri);
+    map.put(uri, doc);
+    /* #ifdef JAVA2 */
+    cache.put(uri, new DocReference(uri, doc));
+    /* #endif */
+    return doc;
   }
 }

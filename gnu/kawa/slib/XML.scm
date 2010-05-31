23d163b3655ca12c39d5c8a2e1f45cc111492948
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
2010-05-31 16:47:09 +0000

    * XML.scm (element-name, attribute-name): New functions.
    	* kawa.texi (Creating XML nodes): Document new functions element-name
    	and attribute-name.
    
    
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@6567 5e0a886f-7f45-49c5-bc19-40643649e37f

--- gnu/kawa/slib/XML.scm
+++ gnu/kawa/slib/XML.scm
@@ -2,11 +2,26 @@
 
 (define-alias comment gnu.kawa.xml.KComment)
 
+; http://download.plt-scheme.org/doc/html/xml/index.html#%28def._%28%28lib._xml/main..ss%29._element-name%29%29 has these:
+; element-name
+; element-attributes
+; element-content
+; atribute-name
+; atribute-value
+; p-i-target-name
+; p-i-instruction
+
 (define-alias processing-instruction gnu.kawa.xml.KProcessingInstruction)
 
 ;;; Parse an XML file (specified by a URL or url string), giving a <document>.
 (define (parse-xml-from-url url) :: document
   (gnu.kawa.xml.Document:parse url))
+
+(define (element-name element::gnu.kawa.xml.KElement) ::symbol
+  (element:getNodeSymbol))
+
+(define (attribute-name attr::gnu.kawa.xml.KAttr) ::symbol
+  (attr:getNodeSymbol))
 
 #|
 (define (parse-nsxml-from-url url) :: <document>

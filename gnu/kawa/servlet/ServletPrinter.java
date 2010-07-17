8be370fa15739f1619ed5935191654dcfb487bf2
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
2010-07-17 21:18:07 +0000

    * HttpRequestContext.java (class HttpOutputStream): Move class ... 	* ServletPrinter.java: ... to here.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@6610 5e0a886f-7f45-49c5-bc19-40643649e37f

--- gnu/kawa/servlet/ServletPrinter.java
+++ gnu/kawa/servlet/ServletPrinter.java
@@ -62,3 +62,83 @@ public class ServletPrinter extends HttpPrinter
   {
   }
 }
+
+class HttpOutputStream extends OutputStream
+{
+  HttpRequestContext context;
+  byte[] buffer;
+  OutputStream out;
+  int count;
+
+  public HttpOutputStream(HttpRequestContext context, int bufSize)
+  {
+    this.context = context;
+    buffer = new byte[bufSize];
+  }
+
+  public void write(int b)
+    throws IOException
+  {
+    if (count >= buffer.length)
+      flush();
+    buffer[count++] = (byte) b;
+  }
+
+  public void write(byte[] data, int offset, int length)
+           throws IOException
+  {
+    int avail = buffer.length - count;
+    while (length > avail)
+      {
+        System.arraycopy(data, offset, buffer, count, avail);
+        count += avail;
+        flush();
+        offset += avail;
+        length -= avail;
+        avail = buffer.length;
+      }
+    if (length > 0)
+      {
+        System.arraycopy(data, offset, buffer, count, length);
+        count += length;
+      }
+  }
+
+  public void flush()
+           throws IOException
+  {
+    if (out == null)
+      {
+        maybeSendResponseHeaders(-1);
+        out = context.getResponseStream();
+      }
+    if (count > 0)
+      {
+        out.write(buffer, 0, count);
+        count = 0;
+      }
+  }
+
+  void maybeSendResponseHeaders (int count)
+    throws IOException
+  {
+    int statusCode = context.statusCode;
+    if (statusCode != HttpRequestContext.STATUS_SENT)
+      {
+        context.sendResponseHeaders(statusCode, context.statusReasonPhrase, count);
+        context.statusCode = HttpRequestContext.STATUS_SENT;
+      }
+  }
+
+  public void close() 
+    throws IOException
+  {
+    if (out == null)
+      {
+        maybeSendResponseHeaders(count);
+        out = context.getResponseStream();
+      }
+    flush();
+    out.close();
+  }
+}

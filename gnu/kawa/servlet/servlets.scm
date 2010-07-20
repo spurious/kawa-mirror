ad88cd4690406b72521ece9cbeb2768c699f1d70
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
2010-07-20 08:12:01 +0000

    * servlets.scm (servlet-context-realpath): Syntax tweaks. 	Change default value from symbol to string.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@6620 5e0a886f-7f45-49c5-bc19-40643649e37f

--- gnu/kawa/servlet/servlets.scm
+++ gnu/kawa/servlet/servlets.scm
@@ -27,14 +27,14 @@
 (define (current-servlet-config) ::javax.servlet.ServletConfig
   ((gnu.kawa.servlet.KawaServlet$Context:getInstance "current-servlet-config"):getServletConfig))
 
-(define (servlet-context-realpath #!optional (path :: <String> '||)) :: <String>
-  (((gnu.kawa.servlet.KawaServlet$Context:getInstance "servlet-context-realpath"):getServletContext):getRealPath))
-
 (define (get-response) ::javax.servlet.http.HttpServletResponse
   ((gnu.kawa.servlet.KawaServlet$Context:getInstance "get-response"):getResponse))
 
 (define (get-request) ::javax.servlet.http.HttpServletRequest
   ((gnu.kawa.servlet.KawaServlet$Context:getInstance "get-request"):getRequest))
+
+(define (servlet-context-realpath #!optional (path ::String "")) ::String
+  (((gnu.kawa.servlet.KawaServlet$Context:getInstance "servlet-context-realpath"):getServletContext):getRealPath))
 
 (define (request-servlet-path) ::String
   ((gnu.kawa.servlet.KawaServlet$Context:getInstance "request-servlet-path"):getServletPath))

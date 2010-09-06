3d6017f1750430a5a48919db5a95c1e170cada7a
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
2010-09-06 03:21:25 +0000

    * cycle1.scm (double-c2x): Move before require so it also works when 	running with --no-inline.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@6691 5e0a886f-7f45-49c5-bc19-40643649e37f

--- testsuite/cycle1.scm
+++ testsuite/cycle1.scm
@@ -1,6 +1,9 @@
 (module-name <cycle1>)
 (module-export is-even? cycle1-name1 cycle1-name2 cycle1-name3 c1x double-c2x)
 
+(define (double-c2x)
+  (set! c2x (* 2 c2x)))
+
 (require "cycle2.scm")
 
 ;; Check that c1, c2, and <cycle1> all evaluate to java.lang.Class objects.
@@ -19,5 +22,3 @@
 
 (define c1x 2) 
 (double-c1x) 
-(define (double-c2x)
-  (set! c2x (* 2 c2x)))

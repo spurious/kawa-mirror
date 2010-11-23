9dbbd1add7744e9ecc01a682b0e192c61c249640
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
2010-11-23 06:26:16 +0000

    * StackMapTableAttr.java (encodeVerificationType): Fix handling of 	Type.nullType - it's imlementation-type is objectType.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@6825 5e0a886f-7f45-49c5-bc19-40643649e37f

--- gnu/bytecode/StackMapTableAttr.java
+++ gnu/bytecode/StackMapTableAttr.java
@@ -65,6 +65,8 @@ public class StackMapTableAttr extends MiscAttr
         else
           return (label.position << 8) | 8; // ITEM_uninitialized
       }
+    else if (type == Type.nullType)
+      return 5; // ITEM_Null
     else
       {
         type = type.getImplementationType();
@@ -84,8 +86,6 @@ public class StackMapTableAttr extends MiscAttr
                 return 0; // ITEM_Top
               }
           }
-        else if (type == Type.nullType)
-          return 5; // ITEM_Null
         else // ITEM_Object
           return ((code.getConstants().addClass((ObjectType) type).index) << 8) | 7;
       }

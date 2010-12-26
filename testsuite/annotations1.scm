151ae215e50c7c3fad400287e22fee3af1aff0c8
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
2010-12-26 18:47:34 +0000

    * annotations1.scm: Add some method anc class annotations. 	* annotations1-bytecode.expected: Update accordingly.
    	* bad-annotations2.scm: New test for catching errors.
    	* bad-annotations2.expected: Expected error messages from above.
    	* Makefile.am: Update acordingly.
    
    
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@6846 5e0a886f-7f45-49c5-bc19-40643649e37f

--- testsuite/annotations1.scm
+++ testsuite/annotations1.scm
@@ -5,6 +5,7 @@
 ;(define-constant XX 20)
 
 (define-class MyClass ()
+  (@MyAnnotType name: "MyClassName")
   interface: #f
   (x #|(@java.lang.Deprecated)|#
    ;;     (@javax.jws.soap.InitParam name: (make-string 4)  value: 0)
@@ -19,20 +20,21 @@
     blvalue: (> 3 4)
     chvalue: #\B
     etype: ElementType:PACKAGE
-    ) ::integer)
+    )
+   ::integer)
   (y
-   (@MyAnnotType
-    names: (string[] "name1" "name2")
-    clvalue: java.io.InputStream)
    (@javax.annotation.Resource
     authenticationType: Resource:AuthenticationType:CONTAINER
-    ;;authenticationType: AuthenticationType:CONTAINER
-    ;;authenticationType: javax.annotation.Resource:AuthenticationType:CONTAINER
-    ;;shareable: (> 3 4)
     type: java.util.ArrayList)
    :: integer)
+  ((toString)
+   (@java.lang.Override)
+   (@MyAnnotType
+    names: (string[] "x" "y")
+    clvalue: java.io.InputStream)
+   (format "MyClass[x:~s y:~s]" x y))
 )
-#|    
-;     (@java.beans.ConstructorProperties value: #("abc" "def"))
+#| 
+     (@java.beans.ConstructorProperties value: #("abc" "def"))
      ::integer))
 |#

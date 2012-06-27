35ec2b49176f0bec843848204306f28411fd2a4e
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
2012-06-27 07:11:07 +0000

    * define1.scm: Add a test for define-alias of constant field.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@7258 5e0a886f-7f45-49c5-bc19-40643649e37f

--- testsuite/define1.scm
+++ testsuite/define1.scm
@@ -2,3 +2,7 @@
        javax.swing.ScrollPaneConstants:VERTICAL_SCROLLBAR_ALWAYS)
 (display vert-sb-always) (newline)
 ;; Output: 22
+(define-alias vert-hb-always
+       javax.swing.ScrollPaneConstants:HORIZONTAL_SCROLLBAR_ALWAYS)
+(display vert-hb-always) (newline)
+;; Output: 32

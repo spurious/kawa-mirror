61d3239024e83b381cb5b767cfdc726509fe5b37
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
2012-09-20 18:38:52 +0000

    Fix javadoc comment bugs.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@7295 5e0a886f-7f45-49c5-bc19-40643649e37f

--- gnu/kawa/lispexpr/ReaderDispatchSyntaxQuote.java
+++ gnu/kawa/lispexpr/ReaderDispatchSyntaxQuote.java
@@ -7,8 +7,8 @@ import gnu.mapping.*;
 import gnu.lists.*;
 import gnu.bytecode.Type;
 
-/** Handle special Scheme forms {code #`}, {code #'}, and {coded {#,}.
- * This is because {code #,} has two meanings:
+/** Handle special Scheme forms {@code #`}, {@code #'}, and {@code #,}.
+ * This is because {@code #,} has two meanings:
  * (1) equivalent to unsyntax when in the context of a quasisyntax form;
  * (2) otherwise a SRFI10 named constructor.
  */

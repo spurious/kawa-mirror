adcf613e115ecf44ffd37f35bf2ed871b496cafc
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
2013-01-23 08:39:34 +0000

    * xml-test.scm (xmltest): New macro.  Use it. 	Add some more tests.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@7437 5e0a886f-7f45-49c5-bc19-40643649e37f

--- testsuite/xml-test.scm
+++ testsuite/xml-test.scm
@@ -1,21 +1,36 @@
 (test-begin "xml")
-(test-equal
- '($xml-element$ () ($resolve-qname$ a)
-                 ($xml-attribute$ 'id "mine"))
- '#<a id="mine"/>)
-(test-equal
- '($xml-element$ (("x" "X")) ($resolve-qname$ a x)
-                 ($xml-attribute$ ($resolve-qname$ id x) "mine"))
- '#<x:a xmlns:x="X" x:id="mine"/>)
-(test-equal
- '($xml-element$ (("" "X")) ($resolve-qname$ a)
-                 ($xml-attribute$ 'id "mine"))
- '#<a xmlns="X" id="mine"/>)
 
-(test-equal
- "<a id=\"mine\" />"
- (format #f "~a" #<a id="mine"/>))
-(test-equal
- "<x:a xmlns:x=\"X\" x:id=\"mine\">text</x:a>"
- (format #f #<x:a xmlns:x="X" x:id="mine">text</>))
+(define-syntax xmltest
+  (syntax-rules ()
+    ((xmltest value quoted evaluated)
+     (begin
+       (test-equal quoted (quote value))
+       (test-equal evaluated (format "~a" value))))))
+
+(xmltest #<a id="mine"/>
+         '($xml-element$ () ($resolve-qname$ a)
+                 ($xml-attribute$ 'id "mine"))
+         &{<a id="mine" />})
+
+(xmltest #<x:a xmlns:x="X" x:id="mine">text</x:a>
+         '($xml-element$ (("x" "X")) ($resolve-qname$ a x)
+                         ($xml-attribute$ ($resolve-qname$ id x) "mine")
+                         "text")
+         &{<x:a xmlns:x="X" x:id="mine">text</x:a>})
+
+(xmltest #<a xmlns="X" id="mine"/>
+         '($xml-element$ (("" "X")) ($resolve-qname$ a)
+                         ($xml-attribute$ 'id "mine"))
+         &{<a xmlns="X" id="mine" />})
+
+(let ((xid 'id)
+      (xa 'a)
+      (xine "ine")
+      (xtex "tex"))
+(xmltest #<[xa] [xid]="m&[xine]">&[xtex]t</>
+         '($xml-element$ () xa
+                 ($xml-attribute$ xid "m" xine)
+                 xtex "t")
+         &{<a id="mine">text</a>}))
+
 (test-end)

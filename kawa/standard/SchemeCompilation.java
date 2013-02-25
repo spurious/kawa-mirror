6419f5b0572d7284700ea38a7d5a24f0d03fddd3
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
2013-02-25 02:35:16 +0000

    * standard/SchemeCompilation.java (checkDefaultBinding): Add 	handling of '->TYPE' as a conversion function.
    	* Curry1.java: New class, extends Procedure.
    	* CurryExp.java: New class, extends ApplyExp.
    	* Makefile.am: Update accordingly.
    
    
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@7471 5e0a886f-7f45-49c5-bc19-40643649e37f

--- kawa/standard/SchemeCompilation.java
+++ kawa/standard/SchemeCompilation.java
@@ -1,5 +1,8 @@
 package kawa.standard;
 import gnu.expr.*;
+import gnu.kawa.functions.Convert;
+import gnu.kawa.functions.Curry1;
+import gnu.kawa.functions.CurryExp;
 import gnu.mapping.*;
 import gnu.text.*;
 import kawa.lang.*;
@@ -55,50 +58,61 @@ public class SchemeCompilation extends Translator
     lambda.setKeywords(Special.optional, Special.rest, Special.key); 
   }
 
-  /** If a symbol is lexically unbound, look for a default binding.
-   * Recognizes {@code typename?} as a type predicate,
-   * plus whatever the overrideen method handles.
-   * @return null if no binding, otherwise an Expression.
-   */
-  @Override
-  public Expression checkDefaultBinding (Symbol symbol, Translator tr)
-  {
-    Namespace namespace = symbol.getNamespace();
-    String local = symbol.getLocalPart();
+    /** If a symbol is lexically unbound, look for a default binding.
+     * Recognizes {@code typename?} as a type predicate,
+     * {@code ->typename} as a conversion function,
+     * plus whatever the overridden method handles.
+     * @return null if no binding, otherwise an Expression.
+     */
+    @Override
+    public Expression checkDefaultBinding(Symbol symbol, Translator tr) {
+        Namespace namespace = symbol.getNamespace();
+        String local = symbol.getLocalPart();
 
-    String name = symbol.toString();
-    int len = name.length();
-    if (len == 0)
-      return null;
-    if (len > 1 && name.charAt(len-1) == '?')
-      {
+        String name = symbol.toString();
+        int len = name.length();
+        if (len == 0)
+            return null;
         int llen = local.length();
-        if (llen > 1)
-          {
+        // Map 'TYPE?' to '(lambda (obj) (instance? obj TYPE))'.
+        if (len > 1 && llen > 1 && name.charAt(len-1) == '?') {
             String tlocal = local.substring(0, llen-1).intern();
             Symbol tsymbol = namespace.getSymbol(tlocal);
             Expression texp = tr.rewrite(tsymbol, false);
-            if (texp instanceof ReferenceExp)
-              {
+            if (texp instanceof ReferenceExp) {
                 Declaration decl = ((ReferenceExp) texp).getBinding();
                 if (decl == null || decl.getFlag(Declaration.IS_UNKNOWN))
                   texp = null;
-              }
+            }
             else if (! (texp instanceof QuoteExp))
-              texp = null;
-            if (texp != null)
-              {
+                texp = null;
+            if (texp != null) {
                 LambdaExp lexp = new LambdaExp(1);
                 lexp.setSymbol(symbol);
                 Declaration param = lexp.addDeclaration((Object) null);
                 param.noteValueUnknown();
                 lexp.body = new ApplyExp(Scheme.instanceOf,
-                                         new Expression[] {
-                                           new ReferenceExp(param), texp, });
+                                         new ReferenceExp(param), texp);
                 return lexp;
-              }
-          }
-      }    
-    return super.checkDefaultBinding(symbol, tr);
-  }
+            }
+        }
+        // Map '->TYPE' to '(lambda (obj) (as TYPE obj))'.
+        if (len > 2 && llen > 2
+            && name.charAt(0) == '-' && name.charAt(1) == '>') {
+            String tlocal = local.substring(2).intern();
+            Symbol tsymbol = namespace.getSymbol(tlocal);
+
+            Expression texp = tr.rewrite(tsymbol, false);
+            if (texp instanceof ReferenceExp) {
+                Declaration decl = ((ReferenceExp) texp).getBinding();
+                if (decl == null || decl.getFlag(Declaration.IS_UNKNOWN))
+                  texp = null;
+            }
+            else if (! (texp instanceof QuoteExp))
+                texp = null;
+            if (texp != null)
+                return new CurryExp(Curry1.makeConverter, Convert.as, texp);
+        }
+        return super.checkDefaultBinding(symbol, tr);
+    }
 }

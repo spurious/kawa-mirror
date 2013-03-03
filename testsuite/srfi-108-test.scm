2d0b1ad69de6584fdc274019603e76f5fbae862a
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
2013-03-03 04:05:56 +0000

    * srfi-108-test.scm (xtest): Move macro to ... 	* test-utils.scm: ... new file. 	* Makefile.am: update accordingly. 	* xml-test.scm: Use xtest. Rename xmltest->xtest.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@7474 5e0a886f-7f45-49c5-bc19-40643649e37f

--- testsuite/srfi-108-test.scm
+++ testsuite/srfi-108-test.scm
@@ -2,32 +2,7 @@
 
 ;; Some tests are based on Racket/Scribble documentation.
 
-(cond-expand
- (kawa
-  ;; Using 3-operand datum->syntax enables line numbers in reporting.
-  (define-syntax xtest
-    (lambda (form)
-      (syntax-case form ()
-        ;; We need to use the rest1 and rest2 variables since the Kawa reader
-        ;; currently only attaches line-numbers to pairs, and the quoted and
-        ;; evaluated sub-forms aren't guaranteed to be lists.
-        ((strtest value . rest1)
-         (syntax-case #'rest1 ()
-           ((quoted . rest2)
-            (syntax-case #'rest2 ()
-              ((evaluated)
-               #`(begin
-             #,(datum->syntax form #'(test-equal quoted (quote value))
-                              #'rest1)
-             #,(datum->syntax form #'(test-equal evaluated (format "~a" value))
-                              #'rest2)))))))))))
- (else
-  (define-syntax xtest
-    (syntax-rules ()
-      ((xtest value quoted evaluated)
-       (begin
-         (test-equal quoted (quote value))
-         (test-equal evaluated (format "~a" value))))))))
+(require "test-utils.scm")
 
 (define-syntax $construct$:doc
   (syntax-rules ()
--- testsuite/xml-test.scm
+++ testsuite/xml-test.scm
@@ -1,24 +1,27 @@
 (test-begin "xml")
 
+(require "test-utils.scm")
+#|
 (define-syntax xmltest
   (syntax-rules ()
     ((xmltest value quoted evaluated)
      (begin
        (test-equal quoted (quote value))
        (test-equal evaluated (format "~a" value))))))
+|#
 
-(xmltest #<a id="mine"/>
+(xtest #<a id="mine"/>
          '($xml-element$ () ($resolve-qname$ a)
                  ($xml-attribute$ 'id "mine"))
          &{<a id="mine" />})
 
-(xmltest #<x:a xmlns:x="X" x:id="mine">text</x:a>
+(xtest #<x:a xmlns:x="X" x:id="mine">text</x:a>
          '($xml-element$ (("x" "X")) ($resolve-qname$ a x)
                          ($xml-attribute$ ($resolve-qname$ id x) "mine")
                          "text")
          &{<x:a xmlns:x="X" x:id="mine">text</x:a>})
 
-(xmltest #<a xmlns="X" id="mine"/>
+(xtest #<a xmlns="X" id="mine"/>
          '($xml-element$ (("" "X")) ($resolve-qname$ a)
                          ($xml-attribute$ 'id "mine"))
          &{<a xmlns="X" id="mine" />})
@@ -27,7 +30,7 @@
       (xa 'a)
       (xine "ine")
       (xtex "tex"))
-(xmltest #<[xa] [xid]="m&[xine]">&[xtex]t</>
+(xtest #<[xa] [xid]="m&[xine]">&[xtex]t</>
          '($xml-element$ () xa
                  ($xml-attribute$ xid "m" xine)
                  xtex "t")
@@ -35,7 +38,7 @@
 
 ;; Computed attribute value.
 (let ((idatr ($xml-attribute$ 'id "myId")))
-  (xmltest #<ab [idatr] x="name"/>
+  (xtest #<ab [idatr] x="name"/>
            '($xml-element$ () ($resolve-qname$ ab)
                            idatr ($xml-attribute$ 'x "name"))
            &{<ab id="myId" x="name" />}))

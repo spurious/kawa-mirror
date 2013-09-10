5e7fccb887d4a7ba954b66cdbaf4165b1ff2847f
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
2013-09-10 06:24:04 +0000

    Added JavaDoc comment.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@7583 5e0a886f-7f45-49c5-bc19-40643649e37f

--- gnu/expr/ObjectExp.java
+++ gnu/expr/ObjectExp.java
@@ -1,6 +1,11 @@
 package gnu.expr;
 import gnu.bytecode.*;
 
+/** An expression that evaluated to an instance of an anonymous class.
+ * It's conceptually questionable that this inherits from ClassExp
+ * - it should perhaps inherit from ApplyExp.
+ */
+
 public class ObjectExp extends ClassExp
 {
   public ObjectExp ()

9f0a4ce35fe246fcfa27c1ba240cff1c94775591
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
2013-09-18 18:20:20 +0000

    * rnrs/programs.scm (command-line): More accurate/informative 	first argument.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@7598 5e0a886f-7f45-49c5-bc19-40643649e37f

--- kawa/lib/rnrs/programs.scm
+++ kawa/lib/rnrs/programs.scm
@@ -3,8 +3,32 @@
 (require <kawa.lib.prim_syntax>)
 
 (define (command-line) :: list
-  (let ((arg0 "kawa")) ;; FIXME
-    (cons arg0 (gnu.lists.LList:makeList gnu.expr.ApplicationMainSupport:commandLineArgArray 0))))
+  (let* ((rest
+          (gnu.lists.LList:makeList
+           gnu.expr.ApplicationMainSupport:commandLineArgArray 0))
+         (command ::java.lang.String
+                  (try-catch
+                   (let ((raw (java.lang.System:getProperty
+                               "sun.java.command")))
+                     (if (eq? raw #!null) #!null
+                         ;; Strip off the tail of the property value that
+                         ;; duplicates the rest value.
+                         (let* ((frest (format #f "~{ ~a~}" rest))
+                                (rlen (raw:length))
+                                (flen (frest:length))
+                                (alen (- rlen flen)))
+                           (cond ((= flen 0)
+                                  raw)
+                                 ;; Sanity check
+                                 ((and (>= alen 0)
+                                       ((raw:substring alen):equals frest))
+                                  (raw:substring 0 alen))
+                                 (else
+                                  #!null)))))
+                   (exp java.lang.Throwable #!null)))
+         (arg0 (if (eq? command #!null) "kawa"
+                   ("java ":concat command))))
+    (cons arg0 rest)))
 
 (define (exit #!optional (code 0)) :: #!void
   (invoke-static <output-port> 'runCleanups)

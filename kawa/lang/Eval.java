38fc9cfb24d85a945c4c5c894f54df1967a1cd5c
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
2013-11-11 20:44:42 +0000

    * lang/Eval.java (evalBody): Even simpler, just remove the 	save/restore of the formStack.  Not sure why it would be needed, 	since eval creates a fresh Translator and thus FormStack each time.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@7683 5e0a886f-7f45-49c5-bc19-40643649e37f

--- kawa/lang/Eval.java
+++ kawa/lang/Eval.java
@@ -82,16 +82,12 @@ public class Eval
         Compilation saveComp = Compilation.setSaveCurrent(tr);
         try
           {
-            LList savedForms
-                = (LList) tr.formStack.popTail(tr.formStack.getHead());
-            Pair savedLast = tr.formStack.lastPair();
             tr.scanBody(body, mod, false);
             tr.finishModule(mod);
             if (body instanceof PairWithPosition)
                 mod.setFile(((PairWithPosition) body).getFileName());
             tr.setEvalName();
             tr.process(Compilation.RESOLVED);
-            tr.formStack.pushAll(savedForms, savedLast);
           }
         finally
           {

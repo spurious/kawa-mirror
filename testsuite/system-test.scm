879a4a478ac84cb62ccaf368105ebb06678f67e5
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
2013-12-25 21:27:11 +0000

    * system-test.scm: More tests, some re-factoring.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@7738 5e0a886f-7f45-49c5-bc19-40643649e37f

--- testsuite/system-test.scm
+++ testsuite/system-test.scm
@@ -1,5 +1,13 @@
 (test-begin "system")
 
+(define split-with-kawa
+  ["../bin/kawa" "-e" "(format #t \"|~{~a|~}~%\" command-line-arguments)"])
+
+(define-syntax test-str-equal
+  (syntax-rules ()
+    ((_ expected value)
+     (test-equal expected (->string value)))))
+
 (define p1 &`{echo foo bar})
 (test-equal "[foo bar\n]" (format #f "[~a]" p1))
 
@@ -12,13 +20,35 @@
 (define p4 &`{sh -c 'for x in $*; do echo $x; sleep 1; done; echo Done' "-" "α" "β" "γ"})
 (test-equal "[α\nβ\nγ\nDone\n]" (format #f "[~a]" p4))
 
-(test-equal "#(\"abc\" \"def ghi\")\n"  (->string &`{../bin/kawa -e '(format #t "~w~%" command-line-arguments)' abc "def ghi"}))
+(test-str-equal "|abc|def ghi|\n" &`{&[split-with-kawa] abc "def ghi"})
 
-(test-equal "|abc|def ghi|\n" (->string &`{../bin/kawa -e '(format #t "|~{~a|~}~%" command-line-arguments)' abc "def ghi"}))
 (let ((v1 " x yz"))
-  (test-equal "|x|yzabc|x|yz|def x yzghi|\n" (->string &`{../bin/kawa -e '(format #t "|~{~a|~}~%" command-line-arguments)' &[v1]abc &[v1] "def&[v1]ghi"})))
+  (test-str-equal "|x|yzabc|x|yz|def x yzghi|\n"
+                  &`{&[split-with-kawa] &[v1]abc &[v1] "def&[v1]ghi"}))
+
 (let ((v1 " x yz"))
-  (test-equal "|x|yzabc|x|yz|def x yzghi|\n" (->string &sh{../bin/kawa -e '(format #t "|~{~a|~}~%" command-line-arguments)' &[v1]abc &[v1] "def&[v1]ghi"})))
+  (test-str-equal "|x|yzabc|x|yz|def x yzghi|\n"
+                  &sh{&[split-with-kawa] &[v1]abc &[v1] "def&[v1]ghi"}))
+
+(let ((v2 ["a b" "c\"d"]))
+  (test-str-equal "|cmd|a b|c\"d|\n" &`{&[split-with-kawa] cmd &[v2]}))
+
+(let ((v2 ["a b" "c\"d"]))
+  (test-str-equal "|cmd|a b c\"d|\n" &`{&[split-with-kawa] cmd  "&[v2]"}))
+
+(let ((vq (unescaped-data "' bc '")))
+  (test-str-equal "|cmd|a|bc|z|\n"
+                  &`{&[split-with-kawa] cmd 'a&[vq]z'}))
+(let ((vq (unescaped-data "' bc '")))
+  (test-str-equal "|shcmd|a|bc|z|\n"
+                  &sh{&[split-with-kawa] shcmd 'a&[vq]z'}))
+;; In contrast with:
+(let ((vq "' bc '"))
+  (test-str-equal "|cmd|a' bc 'z|\n"
+                  &`{&[split-with-kawa] cmd 'a&[vq]z'}))
+(let ((vu (unescaped-data "b ' c d '")))
+  (test-str-equal "|cmd|a b |c|d|z|\n"
+                  &`{&[split-with-kawa] cmd 'a &[vu]z'}))
 
 (let ((tmp1 (java.io.File:createTempFile "kawa-test" #!null)))
   &`[out-to: tmp1]{echo ab cd}

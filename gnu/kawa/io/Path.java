b6630e7be8800be4466b5eba6b63a95f2b7e06cb
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
2014-09-28 05:48:02 +0000

    Re-formatted.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@8066 5e0a886f-7f45-49c5-bc19-40643649e37f

--- gnu/kawa/io/Path.java
+++ gnu/kawa/io/Path.java
@@ -45,9 +45,8 @@ implements javax.tools.FileObject
         }
     };
 
-  protected Path ()
-  {
-  }
+    protected Path() {
+    }
 
     public static Path currentPath() {
         return pathLocation.get();
@@ -57,112 +56,99 @@ implements javax.tools.FileObject
         pathLocation.set(path);
     }
 
-  public static Path coerceToPathOrNull (Object path)
-  {
-    if (path instanceof Path)
-      return (Path) path;
-    if (path instanceof URL)
-      return URLPath.valueOf((URL) path);
-    /* #ifdef use:java.net.URI */
-    if (path instanceof URI)
-      return URIPath.valueOf((URI) path);
-    /* #endif */
-    if (path instanceof File)
-      return FilePath.valueOf((File) path);
-    String str;
-    if (path instanceof CharSequence) // FIXME: || UntypedAtomic
-      str = path.toString();
-    else
-      return null;
-    if (Path.uriSchemeSpecified(str))
-      return URIPath.valueOf(str);
-    else
-      return FilePath.valueOf(str);
-  }
-
-  public static Path valueOf (Object arg)
-  {
-    Path path = coerceToPathOrNull(arg);
-    if (path == null)
-      throw new WrongType((String) null, WrongType.ARG_CAST, arg, "path");
-    return path;
-  }
-
-  public static URL toURL (String str)
-  {
-    try
-      {
-        if (! Path.uriSchemeSpecified(str))
-          {
-            Path cur = currentPath();
-            Path path = cur.resolve(str);
-            if (path.isAbsolute())
-              return path.toURL();
+    public static Path coerceToPathOrNull(Object path) {
+        if (path instanceof Path)
+            return (Path) path;
+        if (path instanceof URL)
+            return URLPath.valueOf((URL) path);
+        /* #ifdef use:java.net.URI */
+        if (path instanceof URI)
+            return URIPath.valueOf((URI) path);
+        /* #endif */
+        if (path instanceof File)
+            return FilePath.valueOf((File) path);
+        String str;
+        if (path instanceof CharSequence) // FIXME: || UntypedAtomic
             str = path.toString();
-          }
-        return new URL(str);
-      }
-    catch (Exception ex)
-      {
-        throw WrappedException.wrapIfNeeded(ex);
-      }
- }
+        else
+            return null;
+        if (Path.uriSchemeSpecified(str))
+            return URIPath.valueOf(str);
+        else
+            return FilePath.valueOf(str);
+    }
 
-  /** Helper routine to get the scheme part of a URI.
-   * The scheme part is "http:" or "file:" or "ftp:" most commonly.
-   * This functions searches for the first ':' that doesn't follow a '/'.
-   * @return The length of the scheme component, not counting the colon,
-   * (or alternatively the index of the colon), or -1 if the is no scheme.
-   */
-  public static int uriSchemeLength (String uri)
-  {
-    int len = uri.length();
-    for (int i = 0;  i < len;  i++)
-      {
-	char ch = uri.charAt(i);
-	if (ch == ':')
-	  return i;
-        if (i == 0 ? ! Character.isLetter(ch)
-            : (! Character.isLetterOrDigit(ch)
-               && ch != '+' && ch != '-' && ch != '.'))
-	  return -1;
-      }
-    return -1;
-  }
+    public static Path valueOf(Object arg) {
+        Path path = coerceToPathOrNull(arg);
+        if (path == null)
+            throw new WrongType((String) null, WrongType.ARG_CAST, arg, "path");
+        return path;
+    }
 
-  /** Tests if a URL has a scheme.
-   * For convenience, we treat a 1-character "scheme" as an
-   * MS-DOS-style "drive letter" - i.e. not a scheme. */
-  public static boolean uriSchemeSpecified (String name)
-  {
-    int ulen = uriSchemeLength(name);
-    if (ulen == 1 && File.separatorChar == '\\')
-      {
-        char drive = name.charAt(0);
-        return ! ((drive >= 'a' && drive <= 'z')
-                  || (drive >= 'A' && drive <= 'Z'));
-      }
-    return ulen > 0;
-  }
+    public static URL toURL(String str) {
+        try {
+            if (! Path.uriSchemeSpecified(str)) {
+                Path cur = currentPath();
+                Path path = cur.resolve(str);
+                if (path.isAbsolute())
+                    return path.toURL();
+                str = path.toString();
+            }
+            return new URL(str);
+        } catch (Exception ex) {
+            throw WrappedException.wrapIfNeeded(ex);
+        }
+    }
 
-  public abstract boolean isAbsolute ();
+    /** Helper routine to get the scheme part of a URI.
+     * The scheme part is "http:" or "file:" or "ftp:" most commonly.
+     * This functions searches for the first ':' that doesn't follow a '/'.
+     * @return The length of the scheme component, not counting the colon,
+     * (or alternatively the index of the colon), or -1 if the is no scheme.
+     */
+    public static int uriSchemeLength(String uri) {
+        int len = uri.length();
+        for (int i = 0;  i < len;  i++) {
+            char ch = uri.charAt(i);
+            if (ch == ':')
+                return i;
+            if (i == 0 ? ! Character.isLetter(ch)
+                : (! Character.isLetterOrDigit(ch)
+                   && ch != '+' && ch != '-' && ch != '.'))
+                return -1;
+        }
+        return -1;
+    }
 
-  /** Does this path name a directory?
-   * The default implementation returns true only if the path ends
-   * with '/' or the separatorChar.
-   */
-  public boolean isDirectory ()
-  {
-    String str = toString(); 
-    int len = str.length();
-    if (len > 0)
-      {
-        char last = str.charAt(len - 1);
-        if (last == '/' || last == File.separatorChar)
-          return true;
-      }
-    return false;
-  }
+    /** Tests if a URL has a scheme.
+     * For convenience, we treat a 1-character "scheme" as an
+     * MS-DOS-style "drive letter" - i.e. not a scheme. */
+    public static boolean uriSchemeSpecified(String name) {
+        int ulen = uriSchemeLength(name);
+        if (ulen == 1 && File.separatorChar == '\\') {
+            char drive = name.charAt(0);
+            return ! ((drive >= 'a' && drive <= 'z')
+                      || (drive >= 'A' && drive <= 'Z'));
+        }
+        return ulen > 0;
+    }
+
+    public abstract boolean isAbsolute();
+
+    /** Does this path name a directory?
+     * The default implementation returns true only if the path ends
+     * with '/' or the separatorChar.
+     */
+    public boolean isDirectory() {
+        String str = toString(); 
+        int len = str.length();
+        if (len > 0) {
+            char last = str.charAt(len - 1);
+            if (last == '/' || last == File.separatorChar)
+                return true;
+        }
+        return false;
+    }
 
     /** Delete file - for compatibility with FileObject. */
     public boolean delete() {
@@ -179,154 +165,134 @@ implements javax.tools.FileObject
         throw new UnsupportedOperationException();
     }
 
-  public boolean exists ()
-  {
-    return getLastModified() != 0;
-  }
+    public boolean exists() {
+        return getLastModified() != 0;
+    }
 
-  public abstract long getLastModified ();
+    public abstract long getLastModified();
 
-  public long getContentLength ()
-  {
-    return -1;
-  }
+    public long getContentLength() {
+        return -1;
+    }
  
-  public abstract String getScheme ();
+    public abstract String getScheme();
 
-  public String getAuthority ()
-  {
-    return null;
-  }
+    public String getAuthority() {
+        return null;
+    }
 
-  public String getUserInfo ()
-  {
-    return null;
-  }
+    public String getUserInfo() {
+        return null;
+    }
 
-  public String getHost ()
-  {
-    return null;
-  }
+    public String getHost() {
+        return null;
+    }
 
-  public abstract String getPath ();
+    public abstract String getPath();
 
-  public Path getDirectory ()
-  {
-    if (isDirectory())
-      return this;
-    else
-      return resolve("");
-  }
+    public Path getDirectory() {
+        if (isDirectory())
+            return this;
+        else
+            return resolve("");
+    }
 
-  public Path getParent ()
-  {
-    return resolve(isDirectory() ? ".." : "");
-  }
+    public Path getParent() {
+        return resolve(isDirectory() ? ".." : "");
+    }
 
-  public String getLast ()
-  {
-    String p = getPath();
-    if (p == null)
-      return null;
-    int len = p.length();
-    int end = len;
-    for (int i = len; ; )
-      {
-        if (--i <= 0)
-          return "";
-        char c = p.charAt(i);
-        if (c == '/'
-            || (this instanceof FilePath
-                && c == File.separatorChar))
-          {
-            if (i+1 == len)
-              end = i;
-            else
-              return p.substring(i+1, end);
-          }
-      }
-  }
+    public String getLast() {
+        String p = getPath();
+        if (p == null)
+            return null;
+        int len = p.length();
+        int end = len;
+        for (int i = len; ; ) {
+            if (--i <= 0)
+                return "";
+            char c = p.charAt(i);
+            if (c == '/'
+                || (this instanceof FilePath
+                    && c == File.separatorChar)) {
+                if (i+1 == len)
+                    end = i;
+                else
+                    return p.substring(i+1, end);
+            }
+        }
+    }
 
-  public String getExtension ()
-  {
-    String p = getPath();
-    if (p == null)
-      return null;
-    int len = p.length();
-    for (int i = len; ; )
-      {
-        if (--i <= 0)
-          return null;
-        char c = p.charAt(i);
-        boolean sawDot = false;
-        if (c == '.')
-          {
-            c = p.charAt(i-1);
-            sawDot = true;
-          }
-        if (c == '/'
-            || (this instanceof FilePath
-                && c == File.separatorChar))
-          return null;
-        if (sawDot)
-          return p.substring(i+1);
-      }
-  }
+    public String getExtension() {
+        String p = getPath();
+        if (p == null)
+            return null;
+        int len = p.length();
+        for (int i = len; ; ) {
+            if (--i <= 0)
+                return null;
+            char c = p.charAt(i);
+            boolean sawDot = false;
+            if (c == '.') {
+                c = p.charAt(i-1);
+                sawDot = true;
+            }
+            if (c == '/'
+                || (this instanceof FilePath
+                    && c == File.separatorChar))
+                return null;
+            if (sawDot)
+                return p.substring(i+1);
+        }
+    }
 
-  public int getPort ()
-  {
-    return -1;
-  }
+    public int getPort() {
+        return -1;
+    }
 
-  public String getQuery ()
-  {
-    return null;
-  }
+    public String getQuery() {
+        return null;
+    }
 
-  public String getFragment ()
-  {
-    return null;
-  }
+    public String getFragment() {
+        return null;
+    }
 
-  public abstract URL toURL ();
+    public abstract URL toURL();
 
-  /* #ifdef use:java.net.URI */
-  public abstract URI toUri ();
-  /* @deprecated */
-  public final URI toURI () { return toUri(); }
-  /* #else */
-  // public String toUri () { return toURIString(); }
-  // /* @deprecated */
-  // public final String toURI () { return toUri(); }
-  /* #endif */
-  public String toURIString () { return toUri().toString(); }
+    /* #ifdef use:java.net.URI */
+    public abstract URI toUri();
+    /* @deprecated */
+    public final URI toURI() { return toUri(); }
+    /* #else */
+    // public String toUri() { return toURIString(); }
+    // /* @deprecated */
+    // public final String toURI() { return toUri(); }
+    /* #endif */
+    public String toURIString() { return toUri().toString(); }
 
-  public Path resolve (Path relative)
-  {
-    if (relative.isAbsolute())
-      return relative;
-    return resolve(relative.toString());
-  }
+    public Path resolve(Path relative) {
+        if (relative.isAbsolute())
+            return relative;
+        return resolve(relative.toString());
+    }
 
-  public abstract Path resolve (String relative);
+    public abstract Path resolve(String relative);
 
-  public static InputStream openInputStream (Object uri) throws IOException
-  {
-    return Path.valueOf(uri).openInputStream();
-  }
+    public static InputStream openInputStream(Object uri) throws IOException {
+        return Path.valueOf(uri).openInputStream();
+    }
 
-  public abstract InputStream openInputStream () throws IOException;
-  public abstract OutputStream openOutputStream () throws IOException;
+    public abstract InputStream openInputStream() throws IOException;
+    public abstract OutputStream openOutputStream() throws IOException;
 
-  public Reader openReader (boolean ignoreEncodingErrors) throws IOException
-  {
-    throw new UnsupportedOperationException(); // FIXME
-  }
+    public Reader openReader(boolean ignoreEncodingErrors) throws IOException {
+        throw new UnsupportedOperationException(); // FIXME
+    }
 
-  public Writer openWriter () throws IOException
-  {
-    return new OutputStreamWriter(openOutputStream());
-  }
+    public Writer openWriter() throws IOException {
+        return new OutputStreamWriter(openOutputStream());
+    }
 
     public CharSequence getCharContent(boolean ignoreEncodingErrors)
         throws IOException {
@@ -359,79 +325,73 @@ implements javax.tools.FileObject
         return buffer;
     }
 
-  /** Convert an absolute URI to one relatve to a given base.
-   * This goes beyond java.net.URI.relativize in that if the arguments
-   * have a common prefix, it can create a relative URI using "../" steps.
-   */
-  public static String relativize (String in, String base)
-    throws java.net.URISyntaxException, java.io.IOException
-  {
-    String baseStr = base;
-    String inStr = in;
-    /* #ifdef use:java.net.URI */
-    baseStr = new URI(baseStr).normalize().toString();
-    inStr = URLPath.valueOf(in).toURI().normalize().toString();
-    /* #endif */
-    int baseLen = baseStr.length();
-    int inLen = inStr.length();
-    int i = 0;
-    int sl = 0;
-    int colon = 0;
-    for (; i < baseLen && i < inLen;  i++)
-      {
-        char cb = baseStr.charAt(i);
-        char ci = inStr.charAt(i);
-        if (cb != ci)
-          break;
-        if (cb == '/')
-          sl = i;
-        if (cb == ':')
-          colon = i;
-      }
-    if (colon > 0
-        && (sl > colon + 2 || baseLen <= colon+2 || baseStr.charAt(colon+2) != '/')
-        /*
-        && (colon + 2 != CLASS_RESOURCE_URI_PREFIX_LENGTH
-            || ! inStr.substring(0, colon + 2).equals(CLASS_RESOURCE_URI_PREFIX)
-            || getClassLoaderForURI(base) == getClassLoaderForURI(in))
-        */
-        )
-      {
-        baseStr = baseStr.substring(sl+1);
-        inStr = inStr.substring(sl+1);
-      }
-    else
-      return in;
-    /* #ifdef JAVA5 */
-    StringBuilder sbuf = new StringBuilder();
-    /* #else */
-    // StringBuffer sbuf = new StringBuffer();
-    /* #endif */
-    sl = 0;
-    for (i = baseLen = baseStr.length(); --i >= 0; )
-      if (baseStr.charAt(i) == '/') // sep?
-        sbuf.append("../");
-    sbuf.append(inStr);
-    return sbuf.toString();
-  }
+    /** Convert an absolute URI to one relatve to a given base.
+     * This goes beyond java.net.URI.relativize in that if the arguments
+     * have a common prefix, it can create a relative URI using "../" steps.
+     */
+    public static String relativize(String in, String base)
+            throws java.net.URISyntaxException, java.io.IOException {
+        String baseStr = base;
+        String inStr = in;
+        /* #ifdef use:java.net.URI */
+        baseStr = new URI(baseStr).normalize().toString();
+        inStr = URLPath.valueOf(in).toURI().normalize().toString();
+        /* #endif */
+        int baseLen = baseStr.length();
+        int inLen = inStr.length();
+        int i = 0;
+        int sl = 0;
+        int colon = 0;
+        for (; i < baseLen && i < inLen;  i++) {
+            char cb = baseStr.charAt(i);
+            char ci = inStr.charAt(i);
+            if (cb != ci)
+                break;
+            if (cb == '/')
+                sl = i;
+            if (cb == ':')
+                colon = i;
+        }
+        if (colon > 0
+            && (sl > colon + 2 || baseLen <= colon+2 || baseStr.charAt(colon+2) != '/')
+            /*
+            && (colon + 2 != CLASS_RESOURCE_URI_PREFIX_LENGTH
+                || ! inStr.substring(0, colon + 2).equals(CLASS_RESOURCE_URI_PREFIX)
+                || getClassLoaderForURI(base) == getClassLoaderForURI(in))
+            */
+            ) {
+            baseStr = baseStr.substring(sl+1);
+            inStr = inStr.substring(sl+1);
+        }
+        else
+            return in;
+        /* #ifdef JAVA5 */
+        StringBuilder sbuf = new StringBuilder();
+        /* #else */
+        // StringBuffer sbuf = new StringBuffer();
+        /* #endif */
+        sl = 0;
+        for (i = baseLen = baseStr.length(); --i >= 0; )
+            if (baseStr.charAt(i) == '/') // sep?
+                sbuf.append("../");
+        sbuf.append(inStr);
+        return sbuf.toString();
+    }
 
-  public String getName ()
-  {
-    return toString();
-  }
+    public String getName() {
+        return toString();
+    }
 
-  public Path getAbsolute ()
-  {
-    if (this == Path.userDirPath)
-      return resolve("");
-    else
-      return currentPath().resolve(this);
-  }
+    public Path getAbsolute() {
+        if (this == Path.userDirPath)
+            return resolve("");
+        else
+            return currentPath().resolve(this);
+    }
 
-  public Path getCanonical ()
-  {
-    return getAbsolute ();
-  }
+    public Path getCanonical() {
+        return getAbsolute();
+    }
 
     /* #ifdef JAVA7 */
     /** Convert if possible to a {@code java.nio.file.Path} instance.

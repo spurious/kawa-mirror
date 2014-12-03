4455e21a715bec78198d03618e5fb010e5ead1d3
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
2014-12-03 22:25:57 +0000

    Re-formatted.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@8204 5e0a886f-7f45-49c5-bc19-40643649e37f

--- gnu/kawa/reflect/TypeSwitch.java
+++ gnu/kawa/reflect/TypeSwitch.java
@@ -15,95 +15,83 @@ import gnu.mapping.*;
  * must be LambdaExps, and the call must be inlined.
  */
 
-public class TypeSwitch extends MethodProc implements Inlineable
-{
-  public static final TypeSwitch typeSwitch = new TypeSwitch("typeswitch");
+public class TypeSwitch extends MethodProc implements Inlineable {
 
-  public TypeSwitch(String name)
-  {
-    setName(name);
-    setProperty(Procedure.validateApplyKey,
-                   "gnu.kawa.reflect.CompileReflect:validateApplyTypeSwitch");
-  }
+    public static final TypeSwitch typeSwitch = new TypeSwitch("typeswitch");
 
-  public int numArgs() { return 0xfffff002; }
+    public TypeSwitch(String name) {
+        setName(name);
+        setProperty(Procedure.validateApplyKey,
+                    "gnu.kawa.reflect.CompileReflect:validateApplyTypeSwitch");
+    }
 
-  public void apply (CallContext ctx) throws Throwable
-  {
-    Object[] args = ctx.getArgs();
-    Object selector = args[0];
-    int n = args.length-1;
-    for (int i = 1;  i < n;  i++)
-      {
-	MethodProc caseProc = (MethodProc) args[i];
-        int m = caseProc.match1(selector, ctx);
-	if (m >= 0)
-	  return;
-      }
-    Procedure defaultProc = (Procedure) args[n];
-    defaultProc.check1(selector, ctx);
-  }
+    public int numArgs() { return 0xfffff002; }
 
-  public void compile (ApplyExp exp, Compilation comp, Target target)
-  {
-    Expression[] args = exp.getArgs();
+    public void apply(CallContext ctx) throws Throwable {
+        Object[] args = ctx.getArgs();
+        Object selector = args[0];
+        int n = args.length-1;
+        for (int i = 1;  i < n;  i++) {
+            MethodProc caseProc = (MethodProc) args[i];
+            int m = caseProc.match1(selector, ctx);
+            if (m >= 0)
+                return;
+        }
+        Procedure defaultProc = (Procedure) args[n];
+        defaultProc.check1(selector, ctx);
+    }
 
-    CodeAttr code = comp.getCode();
-    code.pushScope();
-    Variable selector = code.addLocal(Type.pointer_type);
-    args[0].compile(comp, Target.pushObject);
-    code.emitStore(selector);
+    public void compile(ApplyExp exp, Compilation comp, Target target) {
+        Expression[] args = exp.getArgs();
 
-    for (int i = 1;  i < args.length;  )
-      {
-	if (i > 1)
-	  code.emitElse();
+        CodeAttr code = comp.getCode();
+        code.pushScope();
+        Variable selector = code.addLocal(Type.pointer_type);
+        args[0].compile(comp, Target.pushObject);
+        code.emitStore(selector);
 
-        Expression arg = args[i++];
+        for (int i = 1;  i < args.length;  ) {
+            if (i > 1)
+                code.emitElse();
 
-	if (arg instanceof LambdaExp)
-	  {
-	    LambdaExp lambda = (LambdaExp) arg;
-	    Declaration param = lambda.firstDecl();
-	    Type type = param.getType();
-            if (! param.getCanRead())
-              param = null;
-            else
-              param.allocateVariable(code);
+            Expression arg = args[i++];
 
-	    if (type instanceof TypeValue)
-	      ((TypeValue) type).emitTestIf(selector, param, comp);
-	    else
-	      {
-                if (i < args.length)
-                  {
-                    code.emitLoad(selector);
-                    type.emitIsInstance(code);
-                    code.emitIfIntNotZero();
-                  }
-                if (param != null)
-                  {
-                    code.emitLoad(selector);
-                    param.compileStore(comp);
-                  }
-	      }
-	    lambda.allocChildClasses(comp);
-	    lambda.body.compileWithPosition(comp, target);
-	  }
-	else
-	  {
-	    throw new Error("not implemented: typeswitch arg not LambdaExp");
-	  }
-      }
-    for (int i = args.length - 2; --i >= 0; )
-      code.emitFi();
+            if (arg instanceof LambdaExp) {
+                LambdaExp lambda = (LambdaExp) arg;
+                Declaration param = lambda.firstDecl();
+                Type type = param.getType();
+                if (! param.getCanRead())
+                    param = null;
+                else
+                    param.allocateVariable(code);
+
+                if (type instanceof TypeValue)
+                    ((TypeValue) type).emitTestIf(selector, param, comp);
+                else {
+                    if (i < args.length) {
+                        code.emitLoad(selector);
+                        type.emitIsInstance(code);
+                        code.emitIfIntNotZero();
+                    }
+                    if (param != null) {
+                        code.emitLoad(selector);
+                        param.compileStore(comp);
+                    }
+                }
+                lambda.allocChildClasses(comp);
+                lambda.body.compileWithPosition(comp, target);
+            } else {
+                throw new Error("not implemented: typeswitch arg not LambdaExp");
+            }
+        }
+        for (int i = args.length - 2; --i >= 0; )
+            code.emitFi();
     
-    code.popScope();
-  }
+        code.popScope();
+    }
 
 
-  public Type getReturnType (Expression[] args)
-  {
-    return Type.pointer_type;
-  }
+    public Type getReturnType(Expression[] args) {
+        return Type.pointer_type;
+    }
 }

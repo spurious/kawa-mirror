53581414b33c60cafb83098c58b8712572ca31ef
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
2015-10-07 21:00:09 +0000

    Re-formatted.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@8632 5e0a886f-7f45-49c5-bc19-40643649e37f

--- gnu/expr/ErrorExp.java
+++ gnu/expr/ErrorExp.java
@@ -9,38 +9,35 @@ import gnu.text.SourceMessages;
 
 public class ErrorExp extends Expression
 {
-  String message;
-  public ErrorExp (String message)
-  {
-    this.message = message;
-  }
+    String message;
 
-  public ErrorExp (String message, SourceMessages messages)
-  {
-    messages.error('e', message);
-    this.message = message;
-  }
+    public ErrorExp(String message) {
+        this.message = message;
+    }
 
-  public ErrorExp (String message, Compilation comp)
-  {
-    comp.getMessages().error('e', message);
-    this.message = message;
-  }
+    public ErrorExp(String message, SourceMessages messages) {
+        this(message);
+        messages.error('e', message);
+    }
 
-  protected boolean mustCompile () { return false; }
+    public ErrorExp(String message, Compilation comp) {
+        this(message, comp.getMessages());
+    }
 
-  public void print (OutPort out)
-  {
-    out.startLogicalBlock("(Error", false, ")");
-    out.writeSpaceLinear();
-    out.print(message);
-    out.endLogicalBlock(")");
-  }
+    protected boolean mustCompile() { return false; }
 
-  public void compile (Compilation comp, Target target)
-  {
-    // Should never happen!
-    throw new Error(comp.getFileName()+":"+comp.getLineNumber()
-                    +": internal error: compiling error expression: "+message);
-  }
+    public String toString() { return "ErrorExp["+message+"]"; }
+
+    public void print(OutPort out) {
+        out.startLogicalBlock("(Error", false, ")");
+        out.writeSpaceLinear();
+        out.print(message);
+        out.endLogicalBlock(")");
+    }
+
+    public void compile(Compilation comp, Target target) {
+        // Should never happen!
+        throw new Error(comp.getFileName()+":"+comp.getLineNumber()
+                        +": internal error: compiling error expression: "+message);
+    }
 }

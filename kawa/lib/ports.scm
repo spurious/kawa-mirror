c9ce6e06e8f709ba748f76d2430385962105c266
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
2016-01-28 02:52:20 +0000

    Revert premature check-in.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@8683 5e0a886f-7f45-49c5-bc19-40643649e37f

--- kawa/lib/ports.scm
+++ kawa/lib/ports.scm
@@ -320,11 +320,6 @@
   ((primitive-virtual-method <input-port> "getReadState" <char> ())
    port))
 
-(define (input-port-is-domterm port)
-  ;; (and (? tport ::gnu.kawa.io.TtyPort port) (port:isDomTerm))
-  (and (gnu.kawa.io.TtyInPort? port)
-       ((->gnu.kawa.io.TtyInPort port):isDomTerm)))
-
 (define (set-port-line! (port ::input-port) (line ::int)) ::void
   (port:setLineNumber line))
 
@@ -354,15 +349,12 @@
 (define (default-prompter port)
   (let ((state (input-port-read-state port)))
     (if (char=? state #\Newline)
-        ""
-        (let* ((initial (char=? state #\Space))
-               (line (number->string (input-port-line-number port)))
-               (pr (format (if initial "#|kawa:~a|# " "#|----:~a|# ") line)))
-          (if (input-port-is-domterm port)
-              ;; Unicode 25bc is ▼ "black down-pointing triangle"
-              (if initial (format "\e[19u\e[16u\x25bc;\e[17u\e[14u~a\e[15u\e]75; #|%P.:%N|# \a" pr)
-                  (format " \e[24u~a\e[18u" pr))
-              pr)))))
+	""
+	(string-append (if (char=? state #\Space)
+			   "#|kawa:"
+			   (string-append "#|" (make-string 1 state) "---:"))
+		       (number->string (input-port-line-number port))
+		       "|# "))))
 
 (define (set-input-port-prompter!
 	 (port :: <gnu.kawa.io.TtyInPort>) (prompter :: <procedure>))

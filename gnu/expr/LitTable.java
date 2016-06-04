f0a6ac513a3a8f03a1367c94f2e2244c01ff3d6a
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
2016-06-04 19:31:23 +0000

    * LitTable.java (findLiteral): Use 'matches' rather than '==' 	to see if value matches a static field. 	Also, do some caching of hash codes.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@8817 5e0a886f-7f45-49c5-bc19-40643649e37f

--- gnu/expr/LitTable.java
+++ gnu/expr/LitTable.java
@@ -45,11 +45,18 @@ public class LitTable extends GeneralHashTable<Object,Object>
     this.mainClass = comp.mainClass;
   }
 
+    private Object hashKeyCache = null;
+    private int hashCodeCache;
+
     @Override
     public int hash(Object key) {
-        if (comp.immediate)
-            return System.identityHashCode(key);
-        return HashUtils.boundedHash(key);
+        if (key == hashKeyCache)
+            return hashCodeCache;
+        int h = comp.immediate ? System.identityHashCode(key)
+            : HashUtils.boundedHash(key);
+        hashKeyCache = key;
+        hashCodeCache = h;
+        return h;
     }
 
     @Override
@@ -297,6 +304,7 @@ public class LitTable extends GeneralHashTable<Object,Object>
     if (value == null)
       return Literal.nullLiteral;
     Literal literal = (Literal) get(value);
+    int valueHash = hash(value); // gets cached value
     if (literal != null)
       return literal;
     if (comp.immediate)
@@ -332,7 +340,9 @@ public class LitTable extends GeneralHashTable<Object,Object>
 			      continue;
 			    Literal lit = new Literal (litValue, fld, this);
 			    staticTable.put(litValue, null, lit);
-			    if (value == litValue)
+                            int litHash = hash(litValue); // gets cached value
+                            if (valueHash == litHash
+                                && matches(litValue, value))
 			      literal = lit;
 			  }
 			catch (Exception ex)

3a8e1182a0c63fae206ea73ae68e21f802863e5b
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
2016-06-26 05:59:21 +0000

    * MakeScene.scm:  Don't use deprecated javafx.scene.SceneBuilder. 	Also if child is already a single Parent, we don't want 	to wrap it in a Group.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@8853 5e0a886f-7f45-49c5-bc19-40643649e37f

--- gnu/kawa/javafx/MakeScene.scm
+++ gnu/kawa/javafx/MakeScene.scm
@@ -2,19 +2,31 @@
 
 (define-simple-class MakeScene ()
   (title ::java.lang.String)
-  (builder access: 'private ::javafx.scene.SceneBuilder)
-  (group access: 'private ::javafx.scene.Group)
-  (root access: 'private ::javafx.scene.Parent)
-  ((*init*)
-   (set! builder (javafx.scene.SceneBuilder:create)))
-  ((setTitle x::java.lang.String) (set! title x))
-  ((setCamera x::javafx.scene.Camera)::void (builder:camera x))
-  ((setDepthBuffer x::boolean)::void (builder:depthBuffer x))
-  ((setCursor x::javafx.scene.Cursor)::void (builder:cursor x))
-  ((setEventDispatcher x::javafx.event.EventDispatcher)::void (builder:eventDispatcher x))
-  ((setFill x::javafx.scene.paint.Paint)::void (builder:fill x))
-  ((setWidth x::double)::void (builder:width x))
-  ((setHeight x::double)::void (builder:height x))
+  (group ::javafx.scene.Group access: 'private)
+  (root ::javafx.scene.Parent access: 'private)
+  (camera ::javafx.scene.Camera access: 'private)
+  (camera-assigned ::boolean access: 'private)
+  (cursor ::javafx.scene.Cursor access: 'private)
+  (cursor-assigned ::boolean access: 'private)
+  (eventDispatcher ::javafx.event.EventDispatcher access: 'private)
+  (eventDispatcher-assigned ::boolean access: 'private)
+  (width ::double init: -1 access: 'private)
+  (height ::double init: -1 access: 'private)
+  (depthBuffer ::boolean access: 'private)
+  (fill ::javafx.scene.paint.Paint access: 'private)
+  (fill-assigned ::boolean access: 'private)
+  ((setCamera x::javafx.scene.Camera)::void
+   (set! camera x) (set! camera-assigned #t))
+  ((setDepthBuffer x::boolean)::void
+   (set! depthBuffer x))
+  ((setCursor x::javafx.scene.Cursor)::void
+   (set! cursor x) (set! cursor-assigned #t))
+  ((setEventDispatcher x::javafx.event.EventDispatcher)::void
+   (set! eventDispatcher x) (set! eventDispatcher-assigned #t))
+  ((setFill x::javafx.scene.paint.Paint)::void
+   (set! fill x) (set! fill-assigned #t))
+  ((setWidth x::double)::void (set! width x))
+  ((setHeight x::double)::void (set! height x))
   ((setRoot r::javafx.scene.Parent)::void
    (if group
        (primitive-throw (java.lang.IllegalArgumentException "setting root after adding children")))
@@ -26,7 +38,11 @@
           (set! group (javafx.scene.Group))))
    group)
   ((add n::javafx.scene.Node)
-   ((getGroup):children:add n))
+   (if (and group root)
+       (set! root #!null))
+   ((getGroup):children:add n)
+   (if (and (javafx.scene.Parent? n) (not group))
+       (setRoot n)))
   ((setNodes n::java.util.List)::void
    (let ((ch (getGroup):children))
      ;;(ch:addAll n))
@@ -34,5 +50,15 @@
        (do ((i ::int 0 (+ i 1))) ((= i k))
          (ch:add (n:get i))))))
   ((build)::javafx.scene.Scene
-   (builder:root (if root root (getGroup)))
-   (builder:build)))
+   (! r (if root root (getGroup)))
+   (! sc (javafx.scene.Scene r width height depthBuffer))
+   (if camera-assigned
+       (sc:setCamera camera))
+   (if fill-assigned
+       (sc:setFill fill))
+   (if cursor-assigned
+       (sc:setCursor cursor))
+   (if eventDispatcher-assigned
+       (sc:setEventDispatcher eventDispatcher))
+   sc))
+

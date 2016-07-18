09d496f40ac7781bf93a634393c595665f761ba0
bothner <bothner@5e0a886f-7f45-49c5-bc19-40643649e37f>
2016-07-18 20:12:09 +0000

    * SVGUtils.java: Clean up and fix PreProcess problems.
    git-svn-id: svn://sourceware.org/svn/kawa/trunk@8882 5e0a886f-7f45-49c5-bc19-40643649e37f

--- gnu/kawa/models/SVGUtils.java
+++ gnu/kawa/models/SVGUtils.java
@@ -2,56 +2,54 @@ package gnu.kawa.models;
 import java.awt.*;
 import java.awt.geom.*;
 /* #ifdef with:VectorGraphics2D */
-import de.erichseifert.vectorgraphics2d.Document;
-import de.erichseifert.vectorgraphics2d.VectorGraphics2D;
-import de.erichseifert.vectorgraphics2d.svg.SVGProcessor;
-import de.erichseifert.vectorgraphics2d.util.PageSize;
-import java.io.ByteArrayOutputStream;
+// import de.erichseifert.vectorgraphics2d.Document;
+// import de.erichseifert.vectorgraphics2d.VectorGraphics2D;
+// import de.erichseifert.vectorgraphics2d.svg.SVGProcessor;
+// import de.erichseifert.vectorgraphics2d.util.PageSize;
+// import java.io.ByteArrayOutputStream;
 /* #endif */
 
 public class SVGUtils {
     /* #ifdef with:VectorGraphics2D */
-    public static boolean haveToSvg = true;
+    // public static boolean haveToSvg = true;
     /* #else */
-    // public static boolean haveToSvg = false;
+    public static boolean haveToSvg = false;
     /* #endif */
-    public static final String JFREESVG_UNAVAILABLE
-        = new String("[JFreeSVG is unavailable]");
+    public static final String UNSUPPORTED =
+        "[converting to SVG not supported]";
     public static String toSVG(Paintable p) {
         /* #ifdef with:VectorGraphics2D */
-        try {
-            Rectangle2D bounds = p.getBounds2D();
-            VectorGraphics2D g2 = new VectorGraphics2D();
-            /*(int)Math.ceil(bounds.getWidth()), (int)Math.ceil(bounds.getHeight()));*/
-            g2.setPaint(Color.BLACK);
-            p.paint(g2);
-            SVGProcessor svgProcessor = new SVGProcessor();
-            Document document = svgProcessor.getDocument(g2.getCommands(), new PageSize(bounds));
-            ByteArrayOutputStream byteOutput = new ByteArrayOutputStream();
-            document.writeTo(byteOutput);
-            String str = new String(byteOutput.toByteArray()); // FIXME
-            str = str.trim();
-            int doctype = str.indexOf("<!DOCTYPE svg");
-            if (doctype >= 0) {
-                int close = str.indexOf(">\n", doctype);
-                if (close > 0)
-                    str = str.substring(close+2);
-            }
-            return str;
-        } catch (Throwable ex) {
-            ex.printStackTrace();
-            return "[Caught "+ex+" while generating SVG]";
-        }
-        /*
-       public InputStream getSVG() throws IOException {
-PageSize pageSize = new PageSize(0.0, 0.0, width, height);
-                return new ByteArrayInputStream(byteOutput.toByteArray());
-        }
-
-
-         */
+        // SVGProcessor svgProcessor;
+        // VectorGraphics2D g2;
+        // try {
+        //     svgProcessor = new SVGProcessor();
+        //     g2 = new VectorGraphics2D();
+        // } catch (Throwable ex) {
+        //     return UNSUPPORTED;
+        // }
+        // try {
+        //     Rectangle2D bounds = p.getBounds2D();
+        //     /*(int)Math.ceil(bounds.getWidth()), (int)Math.ceil(bounds.getHeight()));*/
+        //     g2.setPaint(Color.BLACK);
+        //     p.paint(g2);
+        //     Document document = svgProcessor.getDocument(g2.getCommands(), new PageSize(bounds));
+        //     ByteArrayOutputStream byteOutput = new ByteArrayOutputStream();
+        //     document.writeTo(byteOutput);
+        //     String str = new String(byteOutput.toByteArray()); // FIXME
+        //     str = str.trim();
+        //     int doctype = str.indexOf("<!DOCTYPE svg");
+        //     if (doctype >= 0) {
+        //         int close = str.indexOf(">\n", doctype);
+        //         if (close > 0)
+        //             str = str.substring(close+2);
+        //     }
+        //     return str;
+        // } catch (Throwable ex) {
+        //     ex.printStackTrace();
+        //     return "[Caught "+ex+" while generating SVG]";
+        // }
         /* #else */
-        // return JFREESVG_UNAVAILABLE;
+        return UNSUPPORTED;
         /* #endif */
     }
 }
